9afb6769a666bb29474b1566629747022b25684d
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-03-29 19:06:48 +0200

    household distribution ready to be tested

--- covid/distributor.py
+++ covid/distributor.py
@@ -18,6 +18,38 @@ def populate_world(world:World):
         populate_postcode(postcode)
     print("\n")
 
+def populate_household(household, configuration, adults, kids, old):
+    counter = 0
+    n_kids, n_adults, n_old, n_oldadult = configuration.split(" ")
+    n_kids = int(n_kids)
+    n_adults = int(n_adults)
+    n_old = int(n_old)
+    n_oldadult = int(n_oldadult)
+    n_adults = n_adults + n_oldadult
+    if n_kids > len(kids):
+        print("Warning, too few kids to fill household")
+        n_kids = len(kids)
+    if n_old > len(old):
+        print("Warning, too few old people to fill household")
+        n_old = len(old)
+    if n_adults > len(adults):
+        print("Warning, too few adults to fill household")
+        n_adults = len(adults)
+    for i in range(0, n_kids):
+        kid = kids.pop()
+        kid.household = household
+        household.residents[counter] = kid
+        counter += 1
+    for i in range(0, n_adults):
+        adult = adults.pop()
+        adult.household = household
+        household.residents[counter] = adult 
+        counter += 1
+    for i in range(0, n_old):
+        oldperson = old.pop()
+        oldperson.household = household
+        household.residents[counter] = oldperson
+        counter += 1
 
 def populate_postcode(postcode:Postcode):
     """
@@ -36,12 +68,12 @@ def populate_postcode(postcode:Postcode):
     age_random_variable = stats.rv_discrete(values=(np.arange(0,len(age_freq)),
                                                     age_freq.values))
     household_random_variable = stats.rv_discrete(values=(np.arange(0,len(household_freq)),
-                                                    houesehold_freq.values))
+                                                    household_freq.values))
 
     # sample from random variables
     sex_sampling = sex_random_variable.rvs(size = postcode.n_residents)
     age_sampling = age_random_variable.rvs(size = postcode.n_residents)
-    household_sampling = age_random_variable.rvs(size = postcode.n_residents)
+    household_sampling = household_random_variable.rvs(size = postcode.n_residents)
     people_ids = np.arange(postcode.world.total_people+1,
                            postcode.world.total_people+postcode.n_residents+1)
     # create postcode population
@@ -66,39 +98,7 @@ def populate_postcode(postcode:Postcode):
         configuration = household_sampling[house_id]
         household = Household(house_id, configuration, postcode)
         house_id += 1
-        populate_household(household, configuration, adults, kids, old)
-
-    def populate_household(household, configuration, adults, kids, old):
-        counter = 0
-        n_kids, n_adults, n_old = configuration.split(" ")
-        if n_kids > len(kids):
-            print("Warning, too few kids to fill household")
-            n_kids = len(kids)
-        if n_old > len(old):
-            print("Warning, too few old people to fill household")
-            n_old = len(old)
-        if n_adults > len(adults):
-            print("Warning, too few adults to fill household")
-            n_adults = len(adults)
-        for i in range(0, len(n_kids)):
-            kid = kids.pop()
-            kid.household = household
-            household.residents[counter] = kid
-            counter += 1
-        for i in range(0, len(n_adults)):
-            adult = adults.pop()
-            adult.household = household
-            household.residents[counter] = adult 
-            counter += 1
-        for i in range(0, len(n_old)):
-            oldperson = old.pop()
-            oldperson.household = household
-            household.residents[counter] = oldperson
-            counter += 1
-
-
-
-
-
-
+        configuration_decoded = postcode.world.decoder_household[configuration]
+        populate_household(household, configuration_decoded, adults, kids, old)
+        postcode.households[house_id] = household
 
