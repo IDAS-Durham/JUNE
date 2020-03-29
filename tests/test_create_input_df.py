f25580bebcbff1e9b4568e8be48d41ac3f8f320c
florpi <florpisoteadadejardin@gmail.com>
2020-03-29 13:07:18 +0100

    changing inputs type to dict

--- tests/test_create_input_df.py
+++ tests/test_create_input_df.py
@@ -8,3 +8,5 @@ def test_populate_postcode():
     df = create_input_df()
     # Test frequencies sum up to one
     np.testing.assert_equal((df["males"] + df["females"]).values, np.ones(len(df)))
+
+    # add test for ages as well
