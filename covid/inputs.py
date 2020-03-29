177ac73e6adf9d3308b29bbe1c6f87a7309cd71c
florpi <florpisoteadadejardin@gmail.com>
2020-03-29 16:32:54 +0100

    added inputs docstrings

--- covid/inputs.py
+++ covid/inputs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 
+
 def read_df(
     DATA_DIR: str, filename: str, column_names: list, usecols: list, index: str
 ) -> pd.DataFrame:
@@ -24,7 +25,7 @@ def read_df(
     return df
 
 
-def read_population_df(DATA_DIR: str, freq : bool = True) -> pd.DataFrame:
+def read_population_df(DATA_DIR: str, freq: bool = True) -> pd.DataFrame:
     """Read population dataset downloaded from https://www.nomisweb.co.uk/census/2011/ks101ew        
 
     Args:
@@ -88,6 +89,15 @@ def read_household_df(DATA_DIR: str, freq: bool = True) -> pd.DataFrame:
 
 
 def read_ages_df(DATA_DIR: str, freq: bool = True) -> pd.DataFrame:
+    """Read ages dataset downloaded from https://www.nomisweb.co.uk/census/2011/ks102ew
+
+    Args:
+        DATA_DIR: path to dataset folder (default should be postcode_sector folder) 
+
+    Returns:
+        pandas dataframe with age profiles per postcode sector
+
+    """
     ages = "age_structure.csv"
     ages_names = [
         "postcode_sector",
@@ -117,7 +127,7 @@ def read_ages_df(DATA_DIR: str, freq: bool = True) -> pd.DataFrame:
     return ages_df
 
 
-def read_bedrooms_df(DATA_DIR: str, freq: bool = True)-> pd.DataFrame:
+def read_bedrooms_df(DATA_DIR: str, freq: bool = True) -> pd.DataFrame:
     """Read household dataset downloaded from https://www.nomisweb.co.uk/census/2011/lc1402ew
     Args:
         DATA_DIR: path to dataset folder (default should be postcode_sector folder) 
@@ -126,50 +136,73 @@ def read_bedrooms_df(DATA_DIR: str, freq: bool = True)-> pd.DataFrame:
         pandas dataframe with number of bedrooms per household type per postcode sector
 
     """
-    bedrooms = 'household_compositon_by_bedrooms.csv'
-    bedrooms_usecols = [2, 8, 18] + list(range(24, 28)) \
-                        + list(range(29, 33)) + list(range(34, 38))
+    bedrooms = "household_compositon_by_bedrooms.csv"
+    bedrooms_usecols = (
+        [2, 8, 18] + list(range(24, 28)) + list(range(29, 33)) + list(range(34, 38))
+    )
 
-    bedrooms_names = ['postcode_sector',
-                    'Person', # regardless of bedrooms (only one person)
-                    'Old_Family', # assumed to be 2 always
-                    'Young_Family_1B',
-                    'Young_Family_2B',
-                    'Young_Family_3B',
-                    'Young_Family_4B',
-                    'Lone_Family_1B',
-                    'Lone_Family_2B',
-                    'Lone_Family_3B',
-                    'Lone_Family_4B',
-                    'Other_Family_1B',
-                    'Other_Family_2B',
-                    'Other_Family_3B',
-                    'Other_Family_4B',
-                    ]
+    bedrooms_names = [
+        "postcode_sector",
+        "Person",  # regardless of bedrooms (only one person)
+        "Old_Family",  # assumed to be 2 always
+        "Young_Family_1B",
+        "Young_Family_2B",
+        "Young_Family_3B",
+        "Young_Family_4B",
+        "Lone_Family_1B",
+        "Lone_Family_2B",
+        "Lone_Family_3B",
+        "Lone_Family_4B",
+        "Other_Family_1B",
+        "Other_Family_2B",
+        "Other_Family_3B",
+        "Other_Family_4B",
+    ]
 
-    bedrooms_df = read_df(DATA_DIR, bedrooms, bedrooms_names, bedrooms_usecols, "postcode_sector")
+    bedrooms_df = read_df(
+        DATA_DIR, bedrooms, bedrooms_names, bedrooms_usecols, "postcode_sector"
+    )
     if freq:
         bedrooms_df = bedrooms_df.div(bedrooms_df.sum(axis=1), axis=0)
     return bedrooms_df
 
-def bedrooms2households(bedrooms_df):
+
+def bedrooms2households(bedrooms_df: pd.DataFrame) -> pd.DataFrame:
+    """Converts bedroom data into assumptions of how households are populated by different age groups
+    
+    Args: 
+        bedrooms_df: number of bedrooms per different family types
+    
+    Returns:
+        households_df: how households are populated by age group
+    """
+
     households_df = pd.DataFrame()
-    households_df['0 0 0 1'] = bedrooms_df['Person']
+    households_df["0 0 0 1"] = bedrooms_df["Person"]
     # ASSUMPTIONS
-    # i) Old families are composed of couples only 
-    households_df['0 0 2 0'] = bedrooms_df['Old_Family'] 
+    # i) Old families are composed of couples only
+    households_df["0 0 2 0"] = bedrooms_df["Old_Family"]
     # ii) Lone parents with one or two bedrooms only have one child
-    households_df['1 1 0 0'] = bedrooms_df['Lone_Family_1B'] + bedrooms_df['Lone_Family_2B']
+    households_df["1 1 0 0"] = (
+        bedrooms_df["Lone_Family_1B"] + bedrooms_df["Lone_Family_2B"]
+    )
     # iii) Lone parents with three or more bedrooms only have two children
-    households_df['2 1 0 0'] = bedrooms_df['Lone_Family_3B'] + bedrooms_df['Lone_Family_4B']
+    households_df["2 1 0 0"] = (
+        bedrooms_df["Lone_Family_3B"] + bedrooms_df["Lone_Family_4B"]
+    )
     # iv) Families classified as others count as young adults with no children
-    households_df['0 2 0 0'] = bedrooms_df['Young_Family_1B'] + bedrooms_df['Other_Family_1B'] + bedrooms_df['Other_Family_2B']
-    households_df['1 2 0 0'] = bedrooms_df['Young_Family_2B']
-    households_df['2 2 0 0'] = bedrooms_df['Young_Family_3B'] 
-    households_df['3 2 0 0'] = bedrooms_df['Young_Family_4B']
-    households_df['0 3 0 0'] = bedrooms_df['Other_Family_3B']
-    households_df['0 4 0 0'] = bedrooms_df['Other_Family_4B']
+    households_df["0 2 0 0"] = (
+        bedrooms_df["Young_Family_1B"]
+        + bedrooms_df["Other_Family_1B"]
+        + bedrooms_df["Other_Family_2B"]
+    )
+    households_df["1 2 0 0"] = bedrooms_df["Young_Family_2B"]
+    households_df["2 2 0 0"] = bedrooms_df["Young_Family_3B"]
+    households_df["3 2 0 0"] = bedrooms_df["Young_Family_4B"]
+    households_df["0 3 0 0"] = bedrooms_df["Other_Family_3B"]
+    households_df["0 4 0 0"] = bedrooms_df["Other_Family_4B"]
     return households_df
+
 
 def create_input_dict(
     DATA_DIR: str = os.path.join("..", "data", "census_data", "postcode_sector")
