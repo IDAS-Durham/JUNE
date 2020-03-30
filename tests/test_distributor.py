8f52c724c3e304fd509f9594eb177088944d45d7
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-03-30 19:05:48 +0200

    passes basic tests, freq test fail, probably cause areas are very small for stats

--- tests/test_distributor.py
+++ tests/test_distributor.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 from covid.inputs import create_input_dict
-from covid.classes import World, Person, Postcode, Household
+from covid.classes import World, Person, Area, Household
 from covid.distributor import populate_world
 
 
@@ -15,32 +15,32 @@ def test_global():
     populate_world(world)
 
     n_residents_est = sum(
-        [world.postcodes[i].n_residents for i in range(len(world.postcodes))]
+        [world.areas[i].n_residents for i in range(len(world.areas))]
     )
     n_residents = census_dict_safe["n_residents"].sum()
 
     assert n_residents == n_residents_est
 
     n_residents_est = sum(
-        [len(world.postcodes[i].people) for i in range(len(world.postcodes))]
+        [len(world.areas[i].people) for i in range(len(world.areas))]
     )
 
     assert n_residents == n_residents_est
 
     n_households_est = sum(
-        [world.postcodes[i].n_households for i in range(len(world.postcodes))]
+        [world.areas[i].n_households for i in range(len(world.areas))]
     )
     n_households = census_dict_safe["n_households"].sum()
 
     assert n_households == n_households_est
 
     # n_households_est = sum(
-    #    [len(world.postcodes[i].households) for i in range(len(world.postcodes))]
+    #    [len(world.areas[i].households) for i in range(len(world.areas))]
     # )
     # assert n_households == n_households_est
 
 
-def test_per_postcode():
+def test_per_area():
 
     census_dict = create_input_dict()
     for key, value in census_dict.items():
@@ -51,13 +51,13 @@ def test_per_postcode():
     populate_world(world)
 
     n_residents_est = [
-        world.postcodes[i].n_residents for i in range(len(world.postcodes))
+        world.areas[i].n_residents for i in range(len(world.areas))
     ]
 
     np.testing.assert_equal(n_residents_est, census_dict_safe["n_residents"].values)
 
     n_households_est = [
-        world.postcodes[i].n_households for i in range(len(world.postcodes))
+        world.areas[i].n_households for i in range(len(world.areas))
     ]
 
     np.testing.assert_equal(n_households_est, census_dict_safe["n_households"].values)
@@ -67,20 +67,20 @@ def compute_frequency(world, attribute):
     print(attribute)
     frequencies = []
     decoder = getattr(world, "decoder_" + attribute)
-    for i in world.postcodes.keys():
+    for i in world.areas.keys():
         freq = np.zeros(len(decoder))
         if 'house' not in attribute:
-            for j in world.postcodes[i].people.keys():
-                freq[getattr(world.postcodes[i].people[j], attribute)] += 1
-            freq /= world.postcodes[i].n_residents
+            for j in world.areas[i].people.keys():
+                freq[getattr(world.areas[i].people[j], attribute)] += 1
+            freq /= world.areas[i].n_residents
         else:
-            for j in world.postcodes[i].households.keys():
-                freq[getattr(world.postcodes[i].households[j], attribute)] += 1
-            freq /= world.postcodes[i].n_households
+            for j in world.areas[i].households.keys():
+                freq[getattr(world.areas[i].households[j], attribute)] += 1
+            freq /= world.areas[i].n_households
 
         frequencies.append(freq)
     frequencies = np.asarray(frequencies)
-    assert frequencies.shape == (len(world.postcodes), len(decoder))
+    assert frequencies.shape == (len(world.areas), len(decoder))
     return frequencies
 
 
@@ -116,11 +116,11 @@ def test_lonely_children():
     attribute = 'age'
     decoder = getattr(world, "decoder_" + attribute)
     only_children = 0
-    for i in world.postcodes.keys():
-        for j in world.postcodes[i].households.keys():
+    for i in world.areas.keys():
+        for j in world.areas[i].households.keys():
             freq = np.zeros(len(decoder))
-            for k in world.postcodes[i].households[j].residents.keys():
-                freq[getattr(world.postcodes[i].households[j].residents[k], attribute)] += 1
+            for k in world.areas[i].households[j].residents.keys():
+                freq[getattr(world.areas[i].households[j].residents[k], attribute)] += 1
                 # if no adults, but at least one child
                 if (np.sum(freq[5:]) == 0.) & (np.sum(freq[:5]) > 0.):
                     only_children += 1
