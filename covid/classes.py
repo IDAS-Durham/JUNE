b12703dd08481c86384e5be7b3c18b9e05b68a11
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-03-31 21:19:17 +0200

    all tests green

--- covid/classes.py
+++ covid/classes.py
@@ -72,10 +72,10 @@ class Household:
     its residents.
     """
 
-    def __init__(self, house_id, configuration, area):
+    def __init__(self, house_id, composition, area):
         self.id = house_id
-        self.household_composition = configuration
         self.residents = {}
         #self.residents = group(self.id,"household")
         self.area = area
+        self.household_composition = composition 
 
