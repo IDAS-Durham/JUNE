e19e8a27658934efe8f9684734b4aebd9cce7de5
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-03-31 18:08:36 +0200

    now removes data.zip

--- get_data.sh
+++ get_data.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# https://drive.google.com/file/d/1NM1UDdZnwHwiNDxhcP-nndaWj24m-90L/view
+#https://drive.google.com/file/d/1Bj2OvX-tCAAhw3bxMPfkP8XCQMctDTfs/view
 fileId=1Bj2OvX-tCAAhw3bxMPfkP8XCQMctDTfs
 fileName=data.zip
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
@@ -8,3 +8,4 @@ code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
 curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName} 
 
 unzip data.zip
+rm data.zip
