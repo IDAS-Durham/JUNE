2023344a09e96fcd3c27a4a64ecd3933bf7f78bd
Henry Truong <47297893+htruong0@users.noreply.github.com>
2020-04-02 20:27:50 +0100

    set symptom params in infection class + symp test

--- covid/symptoms.py
+++ covid/symptoms.py
@@ -20,9 +20,9 @@ class Symptoms:
     TODO: we should try and map this onto the Flute/Imperial models, as far
     as possible, to have a baseline and to facilitate validation.
     """
-    def __init__(self,params={},time=-1.):
+    def __init__(self,params,time):
         self.starttime = time
-        self.severity      = 0.
+        self.severity  = 0.
         self.Init(params)
 
     def SetInterval(self,interval):
@@ -30,23 +30,12 @@ class Symptoms:
 
     def ResetParameters(self):
         pass
-    
-    def SetParameter(self,param,limits):
-        delta    = -1.
-        paramnew = -1.
-        while delta < 0. or delta>2. or paramnew > limits[1] or paramnew < limits[0]:
-            delta = random.gauss(0.,1.)
-            if random.random() < param[1]/(param[1]+param[2]):
-                paramnew = param[0] + delta * param[1]
-            else:
-                paramnew = param[0] - delta * param[2]
-        return paramnew
             
     def Init(self,params):
         pass
     
     def Severity(self,time):
-        if time > self.starttime:
+        if time >= self.starttime:
             self.Calculate(time)
         else:
             self.severity = 0.
@@ -70,37 +59,11 @@ class NoSymptoms(Symptoms):
 
 class SymptomsGaussian(Symptoms):
     def Init(self,params):
-        self.maxseverities = []
-        self.Tmeans        = []
-        self.sigmaTs       = []
-        self.maxseverities.append(params["Symptoms:MaxSeverity"])
-        self.Tmeans.append(params["Symptoms:Tmean"])
-        self.sigmaTs.append(params["Symptoms:SigmaT"])
-        self.ResetParameters()
-
-    def ResetParameters(self):
-        self.maxseverity = self.maxseverities[0]
-        self.Tmean       = self.Tmeans[0]
-        self.sigmaT      = self.sigmaTs[0]
-        
-    def SetInterval(self,params):
-        self.maxseverities.append(params["Symptoms:MaxSeverityUpper"])
-        self.maxseverities.append(params["Symptoms:MaxSeverityLower"])
-        self.Tmeans.append(params["Symptoms:TmeanUpper"])
-        self.Tmeans.append(params["Symptoms:TmeanLower"])
-        self.sigmaTs.append(params["Symptoms:SigmaTUpper"])
-        self.sigmaTs.append(params["Symptoms:SigmaTLower"])
-
-    def SetParameters(self):
-        limits = [0,1]
-        self.maxseverity = Symptoms.SetParameter(self,self.maxseverities,limits)
-        limits = [0,2*self.Tmeans[0]]
-        self.Tmean = Symptoms.SetParameter(self,self.Tmeans,limits)
-        limits = [0,2*self.sigmaTs[0]]
-        self.sigmaT = Symptoms.SetParameter(self,self.sigmaTs,limits)
+        self.maxseverity = min(1., max(0., params["Symptoms:MaximalSeverity"]["Value"]))
+        self.Tmean = max(0., params["Symptoms:MeanTime"]["Value"])
+        self.sigmaT = max(0.001, params["Symptoms:SigmaTime"]["Value"])
         
     def Calculate(self,time):
         dt = time - (self.starttime + self.Tmean)
         self.severity = (self.maxseverity * np.exp(-dt**2/self.sigmaT**2))
             
-
