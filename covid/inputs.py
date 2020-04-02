a3605e5830da7e6bf39a6984c07e65610f904fe1
florpi <forpisoteadadejardin@gmail.com>
2020-04-02 16:27:19 +0100

    cosma pulling

--- covid/inputs.py
+++ covid/inputs.py
@@ -193,15 +193,17 @@ def bedrooms2households(bedrooms_df: pd.DataFrame) -> pd.DataFrame:
     # iv) Families classified as others count as young adults with no children
     households_df["0 2 0 0"] = (
         bedrooms_df["Young_Family_1B"]
-        + 0.2*bedrooms_df["Young_Family_2B"]
+        + 0.5*bedrooms_df["Young_Family_2B"]
+        + 0.2*bedrooms_df["Young_Family_3B"]
         + bedrooms_df["Other_Family_1B"]
         + bedrooms_df["Other_Family_2B"]
     )
-    households_df["1 2 0 0"] = 0.8*bedrooms_df["Young_Family_2B"] 
-    households_df["2 2 0 0"] = bedrooms_df["Young_Family_3B"] 
+    households_df["1 2 0 0"] = 0.5*bedrooms_df["Young_Family_2B"] 
+    households_df["2 2 0 0"] = 0.8*bedrooms_df["Young_Family_3B"] 
     households_df["3 2 0 0"] = bedrooms_df["Young_Family_4B"]
     households_df["0 3 0 0"] = bedrooms_df["Other_Family_3B"]
-    households_df["0 4 0 0"] = bedrooms_df["Other_Family_4B"]
+    households_df["0 4 0 0"] = 0.5*bedrooms_df["Other_Family_4B"]
+    households_df["0 5 0 0"] = 0.5*bedrooms_df["Other_Family_4B"]
     return households_df
 
 
