e21d8b7bd120112641d9400fd33508e90d979b34
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-03 20:44:34 +0200

    new fixed postcode spacing data

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# https://drive.google.com/open?id=1EvXmYe048n8VIsmdMog_lEh0K5nNtfea
-fileId=1EvXmYe048n8VIsmdMog_lEh0K5nNtfea
+# https://drive.google.com/open?id=1TbrKPNCar_4h0rldnJnddT67qVl0-7_9
+fileId=1TbrKPNCar_4h0rldnJnddT67qVl0-7_9
 fileName=data.zip
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
