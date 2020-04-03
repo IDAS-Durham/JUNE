b1e7638c33c3bf2ddc34317280e555958652f8da
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-03 19:00:41 +0200

    free education for all

--- covid/school.py
+++ covid/school.py
@@ -4,17 +4,19 @@ class School:
     its pupils (6 - 14 years old).
     """
 
-    def __init__(self, school_id, loc, n_pupils):
+    def __init__(self, school_id, coordinates, n_pupils, urn):
         self.id = school_id
         self.pupils= {}
+        self.urn = urn
+        self.coordinates = coordinates
         #self.residents = group(self.id,"household")
-        self.loc = loc 
-        self.n_pupils = n_pupils 
+        self.n_pupils_max = n_pupils
+        self.n_pupils = 0
 
 class PrimarySchool(School):
-    def __init__(self, school_id, loc, n_pupils):
-        super.__init__(school_id, loc, n_pupils)
+    def __init__(self, school_id, coordinates, n_pupils, urn):
+        super.__init__(school_id, coordinates, n_pupils, urn)
 
 class SecondarySchool(School):
-    def __init__(self, school_id, loc, n_pupils):
-        super.__init__(school_id, loc, n_pupils)
+    def __init__(self, school_id, coordinates, n_pupils, urn):
+        super.__init__(school_id, coordinates, n_pupils, urn)
