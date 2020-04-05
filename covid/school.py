659eaefa2aa95686e78e7aacf89a40b8840363e9
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-05 18:09:36 +0200

    school distribution working, need to think about number of schools

--- covid/school.py
+++ covid/school.py
@@ -4,12 +4,13 @@ class School:
     its pupils (6 - 14 years old).
     """
 
-    def __init__(self, school_id, coordinates, n_pupils, urn):
+    def __init__(self, school_id, coordinates, n_pupils, age_min, age_max):
         self.id = school_id
         self.pupils= {}
-        self.urn = urn
         self.coordinates = coordinates
         #self.residents = group(self.id,"household")
         self.n_pupils_max = n_pupils
         self.n_pupils = 0
+        self.age_min = age_min
+        self.age_max = age_max
 
