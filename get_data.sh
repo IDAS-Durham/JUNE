727ad888c000af409b3f71d865cc05b95a0ff7f1
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-06 21:56:22 +0200

    added welsh schools data

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#https://drive.google.com/open?id=1yssNsQl15LqHYRFqIN_5bPyBAxS8zOvf
-fileId=1yssNsQl15LqHYRFqIN_5bPyBAxS8zOvf
+#https://drive.google.com/open?id=1v82HGQsiN-2763rf6T-sNx22Vpq_GLfB
+fileId=1v82HGQsiN-2763rf6T-sNx22Vpq_GLfB
 fileName=data.zip
 curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
