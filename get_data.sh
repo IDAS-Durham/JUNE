1cdd0723bfebf9f3abea69b96454d70a9e8ef1e6
arnauq <arnau.quera@gmail.com>
2020-04-06 21:03:35 +0100

    changed get_data.sh to store cookie locally so it works in cosma

--- get_data.sh
+++ get_data.sh
@@ -3,9 +3,10 @@
 #https://drive.google.com/open?id=1v82HGQsiN-2763rf6T-sNx22Vpq_GLfB
 fileId=1v82HGQsiN-2763rf6T-sNx22Vpq_GLfB
 fileName=data.zip
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
-code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"  
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName} 
+curl -sc cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
+code="$(awk '/_warning_/ {print $NF}' cookie)"  
+curl -Lb cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName} 
 
 unzip data.zip
 rm data.zip
+rm cookie
