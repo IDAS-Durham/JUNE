9eacdae6bc6fd94113e80bb7b18fe37ff4994b45
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-11 15:48:24 +0200

    fixed group dynamics, transmission failes

--- covid/interaction.py
+++ covid/interaction.py
@@ -1,71 +1,85 @@
 from covid.infection import Infection
-from covid.groups import Group 
+from covid.groups import Group
 import numpy as np
 import sys
 import random
 
+
 class Interaction:
-    def __init__(self,infection_selector,mode="Superposition"):
+    def __init__(self, infection_selector, mode="Superposition"):
         self.selector = infection_selector
-        self.mode     = mode
-        self.groups   = []
-        self.time     = 0.
-        self.oldtime  = 0.
-        self.delta_t  = 0.
-        self.transmission_probability = 0.
-        
-    def set_groups(self,groups):
+        self.mode = mode
+        self.groups = []
+        self.time = 0.0
+        self.oldtime = 0.0
+        self.delta_t = 0.0
+        self.transmission_probability = 0.0
+
+    def set_groups(self, groups):
         self.groups = groups
 
-    def set_time(self,time):
+    def set_time(self, time):
         self.oldtime = self.time
-        self.time    = time
-        self.delta_t = self.time-self.oldtime
-        
+        self.time = time
+        self.delta_t = self.time - self.oldtime
+
     def time_step(self):
-        print ("================ BEFORE ====================")
-        print ("=== iterate over ",len(groups)," groups ====")
-        for group in self.groups:
-            group.update_status_lists(self.time)
-            group.output()
-        for group in self.groups:
-            self.single_time_step_for_group(group)
-        print ("================ AFTER =====================")
-        for group in self.groups:
-            group.update_status_lists(self.time)
-            group.output()
-            
-    def single_time_step_for_group(self,group):
+        print("================ BEFORE ====================")
+        print("=== iterate over ", len(self.groups), " groups ====")
+        for grouptype in self.groups:
+            for group in grouptype.members:
+                if group.size() == 0:
+                    continue
+                group.update_status_lists(self.time)
+                group.output()
+        for grouptype in self.groups:
+            for group in grouptype.members:
+                if group.size() == 0:
+                    continue
+                self.single_time_step_for_group(group)
+        print("================ AFTER =====================")
+        for grouptype in self.groups:
+            for group in grouptype.members:
+                if group.size() == 0:
+                    continue
+                group.update_status_lists(self.time)
+                group.output()
+
+    def single_time_step_for_group(self, group):
         pass
 
-    
-class CollectiveInteraction(Interaction):        
-    def single_time_step_for_group(self,group):
-        if (group.size()<=1 or
-            group.size_infected()==0 or
-            group.size_susceptible()==0):
+
+class CollectiveInteraction(Interaction):
+    def single_time_step_for_group(self, group):
+        if (
+            group.size() <= 1
+            or group.size_infected() == 0
+            or group.size_susceptible() == 0
+        ):
             return
         transmission_probability = self.calculate_transmission_probability(group)
-        if transmission_probability<=0.:
+        if transmission_probability <= 0.0:
             return
         for recipient in group.get_susceptible():
-            self.single_time_step_for_recipient(recipient,transmission_probability)
+            self.single_time_step_for_recipient(recipient, transmission_probability)
 
-    def single_time_step_for_recipient(self,recipient,transmission_probability):
+    def single_time_step_for_recipient(self, recipient, transmission_probability):
         recipient_probability = recipient.get_susceptibility()
-        if recipient_probability>0.:
+        if recipient_probability > 0.0:
             if random.random() <= transmission_probability * recipient_probability:
-                recipient.set_infection(self.selector.make_infection(recipient,self.time))
-                            
-    def calculate_transmission_probability(self,group):
-        transmission_probability = 0.
-        if self.mode=="Superposition":
+                recipient.set_infection(
+                    self.selector.make_infection(recipient, self.time)
+                )
+
+    def calculate_transmission_probability(self, group):
+        transmission_probability = 0.0
+        if self.mode == "Superposition":
             transmission_probability = self.added_transmission_probability(group)
-        elif self.mode=="Probabilistic":
+        elif self.mode == "Probabilistic":
             transmission_probability = self.combined_transmission_probability(group)
         return transmission_probability
-            
-    def combined_transmission_probability(self,group):
+
+    def combined_transmission_probability(self, group):
         """
         multiplicative probability from product of non-infection probabilities.
         for each time step, the infection probabilities per infected person are given
@@ -74,11 +88,13 @@ class CollectiveInteraction(Interaction):
         logic of the SI/SIR models --- and normalised to the time interval, given in
         units of full days.
         """
-        interaction_intensity = group.get_intensity()/group.size()*self.delta_t
-        prob_notransmission   = 1.
+        interaction_intensity = group.get_intensity() / group.size() * self.delta_t
+        prob_notransmission = 1.0
         for person in group.get_infected():
-            prob_notransmission *= (1.-person.transmission_probability(self.time)*interaction_intensity)
-        return 1.-prob_notransmission
+            prob_notransmission *= (
+                1.0 - person.transmission_probability(self.time) * interaction_intensity
+            )
+        return 1.0 - prob_notransmission
 
     def added_transmission_probability(self):
         """
@@ -89,8 +105,8 @@ class CollectiveInteraction(Interaction):
         logic of the SI/SIR models --- and normalised to the time interval, given in
         units of full days.
         """
-        interaction_intensity = group.get_intensity()/group.size()*self.delta_t
-        prob_transmission     = 0.
+        interaction_intensity = group.get_intensity() / group.size() * self.delta_t
+        prob_transmission = 0.0
         for person in group.get_infected():
             prob_transmission += person.transmission_probability(self.time)
-        return prob_transmission*interaction_intensity
+        return prob_transmission * interaction_intensity
--- covid/transmission.py
+++ covid/transmission.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 
+
 class Transmission:
     """
     The probability for an individual to transmit the infection.
@@ -24,108 +25,110 @@ class Transmission:
     TODO: we should try and map this onto the Flute/Imperial models, as far
     as possible, to have a baseline and to facilitate validation.
     """
-    def __init__(self,person,params={},time=-1.):
-        self.person    = person
+
+    def __init__(self, person, params={}, time=-1.0):
+        self.person = person
         self.starttime = time
-        self.value     = 0.
+        self.value = 0.0
         self.init(params)
 
-    def init(self,params):
+    def init(self, params):
         pass
-    
-    def probability(self,time):
+
+    def probability(self, time):
         if time >= self.starttime:
             self.calculate(time)
         else:
-            self.value = 0.
-        return max(0.,self.value)
+            self.value = 0.0
+        return max(0.0, self.value)
 
-    def calculate(self,time):
+    def calculate(self, time):
         pass
 
+
 #################################################################################
 #################################################################################
 #################################################################################
+
 
 class TransmissionSI(Transmission):
-    def init(self,params):
-        self.prob = min(1.,max(0.,params["probability"]["value"]))
+    def init(self, params):
+        self.prob = min(1.0, max(0.0, params["probability"]["value"]))
 
-    def calculate(self,time):
+    def calculate(self, time):
         self.value = self.prob
-        
+
+
 #################################################################################
 #################################################################################
 #################################################################################
+
 
 class TransmissionSIR(Transmission):
-    def init(self,params):
-        self.probT = min(1.,max(0.,params["probability"]["value"]))
-        self.probR = min(1.,max(0.,params["recovery"]["value"]))
+    def init(self, params):
+        self.probT = min(1.0, max(0.0, params["probability"]["value"]))
+        self.probR = min(1.0, max(0.0, params["recovery"]["value"]))
         self.RecoverCutoff = params["recovery_cutoff"]["value"]
-        self.lasttime = self.starttime # last time they had a chance to recover
+        self.lasttime = self.starttime  # last time they had a chance to recover
 
-    def calculate(self,time):
+    def calculate(self, time):
         if self.probR != 0:
-            if self.probT>0 and time>self.lasttime:
+            if self.probT > 0 and time > self.lasttime:
                 for t in range(self.lasttime, time, 1):
                     if random.random() < self.probR:
-                        self.person.set_susceptibility(0) # immune
+                        self.person.set_susceptibility(0)  # immune
                         self.person.set_recovered(True)
-                        break                             # can only recover once
+                        break  # can only recover once
         else:
-            if self.probT>0 and time>self.lasttime:
+            if self.probT > 0 and time > self.lasttime:
                 for t in range(self.lasttime, time, 1):
                     if time > self.starttime + self.RecoverCutoff:
-                        self.person.set_susceptibility(0) # immune
+                        self.person.set_susceptibility(0)  # immune
                         self.person.set_recovered(True)
-                        break                             # can only recover once
-        self.lasttime = time                              # update last time
+                        break  # can only recover once
+        self.lasttime = time  # update last time
         self.value = self.probT
 
 
-
-
-        
 #################################################################################
 #################################################################################
 #################################################################################
 
-    
+
 class TransmissionConstantInterval(Transmission):
-    def init(self,params):
-        self.prob    = min(1.,max(0.,params["probability"]["value"]))
+    def init(self, params):
+        self.prob = min(1.0, max(0.0, params["probability"]["value"]))
         self.endtime = params["end_time"]["value"]
-        self.value   = 0
-        
-    def calculate(self,time):
-        if time<=self.starttime+self.endtime:
+        self.value = 0
+
+    def calculate(self, time):
+        if time <= self.starttime + self.endtime:
             self.value = self.prob
         else:
-            self.value = 0.
+            self.value = 0.0
+
 
 #################################################################################
 #################################################################################
 #################################################################################
+
 
 class TransmissionXNExp(Transmission):
-    def init(self,params):
-        self.prob       = min(1.,max(0.,params["probability"]["value"]))
-        self.exponent   = max(0.,params["exponent"]["value"])
+    def init(self, params):
+        self.prob = min(1.0, max(0.0, params["probability"]["value"]))
+        self.exponent = max(0.0, params["exponent"]["value"])
         self.tailfactor = params["norm"]["value"]
-        if self.tailfactor  < 0.001:
+        if self.tailfactor < 0.001:
             self.tailfactor = 0.001
         self.init_norm
 
     def init_norm(self):
-        x0        = self.exponent * self.tailfactor
-        self.norm = x0**self.exponent * np.exp(-x0/self.tailfactor)  
-        self.norm = 1./self.norm
-                               
-    def calculate(self,time):
-        dt = time - self.starttime
-        self.value = (self.norm * self.prob *
-                     dt**self.exponent *
-                     np.exp(-dt/self.tailfactor))
-    
+        x0 = self.exponent * self.tailfactor
+        self.norm = x0 ** self.exponent * np.exp(-x0 / self.tailfactor)
+        self.norm = 1.0 / self.norm
 
+    def calculate(self, time):
+        dt = time - self.starttime
+        self.value = (
+            self.norm * self.prob * dt ** self.exponent * np.exp(-dt / self.tailfactor)
+        )
