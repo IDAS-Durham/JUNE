3923ab4167fa0dbdeb641acdad870058e48de5e5
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-04-13 13:38:37 +0100

    working on groups/companies/company.py

--- covid/groups/companies/company.py
+++ covid/groups/companies/company.py
@@ -13,7 +13,9 @@ class Company:
         self.people = []
         self.msoa = msoa
         self.n_employees_max = n_employees
-        self.n_pupils = 0
+        self.n_employees
+        self.size_mean
+        self.industry
 
 class Companies:
     def __init__(self, world, msoareas, companysize_dict):
@@ -22,17 +24,61 @@ class Companies:
         self.init_companies(companysize_dict)
 
 
-    def init_companies(self, companysize_dict):
+    def _compute_size_mean(self, sizegroup):
+        '''
+        Given company size group calculates mean
+        '''
+        
+        # ensure that read_companysize_census() also returns number of companies in each size category
+        size_min, size_max = sizegroup.split('-')
+        if age_2 == "XXX":
+            agemean = 1500
+        else:
+            size_min = float(size_min)
+            size_max = float(size_max)
+            size_mean = (size_max - size_min)/2.0
+
+        return size_mean
+    
+    def init_companies(self, companysize_dict, companysector_dict):
         """
         Initializes Companies.
         """
-        COMPANY_AGE_THRESHOLD = [8, 13]
+        # companysize_dict contains msoarea, and the number of companies of different sizes in that area
+        # company_sector_dict contains the number of companies by sector in each msoarea
+        # for each msoarea
+            # compute a probability distribution over company sizes
+            # for each industry
+                # for each company in industry
+                    # assign company a size_mean according to the probability distribution
+
+        
         companies = []
-        copmany_age = list(self.world.decoder_age.values())[
-            0] : COMPANY_AGE_THRESHOLD[1]
-        ]
-        copmany_trees = {}
-        copmany_agegroup_to_global_indices = (
+        # need to make sure the dict is set up correctly to do this
+        sector_columns = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U']
+        size_columns = ["0-9","10-19","20-49","50-99","100-249","250-499","500-999","1000-xxx"]
+        size_dict = {}
+        for idx, column in enumerate(size_columns):
+            size_dict[idx+1] = self._compute_size_mean(column)
+        
+        for msoarea in range(len(companysector_dict['msoarea'])):
+            company_total = int(companysector_dict['n_companies'][msoarea])
+            distribution = []
+            for column in size_columns:
+                distribution.append(int(companysize_dict[column][msoarea])/total)
+
+            numbers = np.arange(1,9)
+            # gives a discrete distribution over the company size per msoarea
+            random_variable = rv_discrete(values=(numbers,distribution))
+            for column in sector_columns:
+                for company in range(column[msoarea]):
+                    # finish this
+                                          
+                    
+                
+        
+
+        
             {}
         )  # stores for each gender-ratio group the index to the school
         # create company neighbour trees
@@ -67,25 +113,5 @@ class Companies:
         self.school_trees = school_trees
         self.school_agegroup_to_global_indices = school_agegroup_to_global_indices
         return None
-
-    def get_closest_schools(self, age, area, k):
-        """
-        Returns the k schools closest to the output area centroid.
-        """
-        school_tree = self.school_trees[age]
-        distances, neighbours = school_tree.query(
-            np.deg2rad(area.coordinates.reshape(1, -1)), k=k, sort_results=True,
-        )
-        return neighbours[0]
-
-    def _create_school_tree(self, school_df):
-        """
-        Reads school location and sizes, it initializes a KD tree on a sphere,
-        to query the closest schools to a given location.
-        """
-        school_tree = BallTree(
-            np.deg2rad(school_df[["latitude", "longitude"]].values), metric="haversine"
-        )
-        return school_tree
-
+        
 
