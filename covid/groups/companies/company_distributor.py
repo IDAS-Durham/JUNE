a3a4864cf997b0a7364df70087435667ee9f8b2d
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-04-14 21:02:40 +0100

    assigning people to companies in CompanyDistributor

--- covid/groups/companies/company_distributor.py
+++ covid/groups/companies/company_distributor.py
@@ -15,40 +15,35 @@ class CompanyDistributor:
     """
 
     def __init__(self, msoarea):
+        self.area = msoareadef __init__(self, msoarea, companies):
         self.area = msoarea
-        self.MAX_COMPANIES = msoarea.world.config["schools"]["neighbour_schools"]
+        self.companies_all = companies
+        # gather call companies in a given msoarea
+        self.companies_msoarea = []
+        for company in self.companies.members:
+            if company.msoa == msoarea:
+                self.companies_msoarea.append(company)
+
+    def _randomly_sample_company(self):
+        index = np.random.randint(len(self.companies_msoarea))
+        return self.companies_msoarea[index]
 
     def distribute_adults_to_companies(self):
+        # this assumes that self.msoarea.people.values() gives the people who WORK in that area
         for person in self.msoarea.people.values():
             if (
-                person.age <= self.WORK_AGE_RANGE[1]
+                    person.age <= self.WORK_AGE_RANGE[1] # if we already assume the first comment, this seems redundant
                 and person.age >= self.WORK_AGE_RANGE[0]
             ):  # person age from 20 up to 74 yo
-                agegroup = self.area.world.decoder_age[person.age]
-                agemean = self.compute_age_group_mean(agegroup)
-                if self.is_agemean_full[
-                    agegroup
-                ]:  # if all schools at that age are full, assign one randomly
-                    if person.age == 6:  # if it is 18-19 yo, then do not fill
-                        continue
-                    random_number = np.random.randint(0, self.MAX_SCHOOLS, size=1)[0]
-                    school = self.closest_schools_by_age[agegroup][random_number]
-                else:
-                    schools_full = 0
-                    for i in range(0, self.MAX_SCHOOLS):  # look for non full school
-                        school = self.closest_schools_by_age[agegroup][i]
-                        if school.n_pupils >= school.n_pupils_max:
-                            schools_full += 1
-                        else:
-                            break
-                    if schools_full == self.MAX_SCHOOLS:  # all schools are full
-                        self.is_agemean_full[agegroup] = True
-                        random_number = np.random.randint(0, self.MAX_SCHOOLS, size=1)[
-                            0
-                        ]
-                        school = self.closest_schools_by_age[agegroup][random_number]
-                    else:  # just keep the school saved in the previous for loop
-                        pass
-                company.employees[company.n_employees] = person
-                person.company = company
-                company.n_pupils += 1
+                person_industry = person.industry
+                assigned = False
+                while assigned == False:
+                    # randomly sample from companies in msoarea rather than filling from the start
+                    # as not all companies will be filled and there may be a mismatch in the number of people
+                    # and the number of companies
+                    company = self._randomly_sample_company()
+                    if company.n_employees == company.n_employees_max:
+                            pass
+                    else:
+                        company.n_employees +=1
+                        assigned = True
--- covid/groups/companies/distribute_to_company.py
+++ covid/groups/companies/distribute_to_company.py
@@ -9,8 +9,14 @@ def assign_adults_to_company(person,live_oarea, work_msoarea, industry):
         # company = randomly choose company based on size distribution(?)
         # assign person to company
 
-    # gather companies to be sampled from - this should be included in the __init__ function as per the SchoolDistirbutor
-    def __init__(self, msoarea, companies):
+
+class CompanyDistributor:
+    """
+    Distributes students to different schools
+    """
+
+    def __init__(self, msoarea):
+        self.area = msoareadef __init__(self, msoarea, companies):
         self.area = msoarea
         self.companies_all = companies
         # gather call companies in a given msoarea
@@ -18,17 +24,28 @@ def assign_adults_to_company(person,live_oarea, work_msoarea, industry):
         for company in self.companies.members:
             if company.msoa == msoarea:
                 self.companies_msoarea.append(company)
-        
 
-    ########## PROCEED WITH FUNCTION ##############
+    def _randomly_sample_company(self):
+        index = np.random.randint(len(self.companies_msoarea))
+        return self.companies_msoarea[index]
 
-    person_industry = person.industry
-    for company in self.companies_msoarea:
-        if company.industry == person_industry:
-            if company.n_employees == company.n_employees_max:
-                pass
-            else:
-                company.n_employees +=1
-                break
-    
+    def distribute_adults_to_companies(self):
+        # this assumes that self.msoarea.people.values() gives the people who WORK in that area
+        for person in self.msoarea.people.values():
+            if (
+                    person.age <= self.WORK_AGE_RANGE[1] # if we already assume the first comment, this seems redundant
+                and person.age >= self.WORK_AGE_RANGE[0]
+            ):  # person age from 20 up to 74 yo
+                person_industry = person.industry
+                assigned = False
+                while assigned == False:
+                    # randomly sample from companies in msoarea rather than filling from the start
+                    # as not all companies will be filled and there may be a mismatch in the number of people
+                    # and the number of companies
+                    company = self._randomly_sample_company()
+                    if company.n_employees == company.n_employees_max:
+                            pass
+                    else:
+                        company.n_employees +=1
+                        assigned = True
     
