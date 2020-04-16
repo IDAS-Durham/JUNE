4e739a8cdda36ec2ff972e1e4e83bac93a1f0bf2
florpi <florpisoteadadejardin@gmail.com>
2020-04-16 18:43:08 +0100

    fixed in dev dynamics

--- get_data.sh
+++ get_data.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-#https://drive.google.com/open?id=1ths-W5DevesmiMoQ1WPunXNwGVI8CV1B
-fileId=1ths-W5DevesmiMoQ1WPunXNwGVI8CV1B
+#https://drive.google.com/open?id=1vNMaEYmPgvDvG6HTFrX_hhV_zH3HgALM
+fileId=1vNMaEYmPgvDvG6HTFrX_hhV_zH3HgALM
 fileName=data.zip
 curl -sc cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' cookie)"  
-curl -Lb cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName} 
+curl -Lb cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
 
-unzip data.zip
-rm data.zip
+unzip ${fileName} || tar -zxvf ${fileName}
 rm cookie
+rm data.zip
