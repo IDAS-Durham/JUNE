ec185221010544a7fd0c577eb1c3b567933079f1
Christoph Becker <christoph.becker@durham.ac.uk>
2020-04-16 17:46:31 +0100

    allow yourself to be distracted

--- get_data.sh
+++ get_data.sh
@@ -2,11 +2,10 @@
 
 #https://drive.google.com/open?id=1ths-W5DevesmiMoQ1WPunXNwGVI8CV1B
 fileId=1ths-W5DevesmiMoQ1WPunXNwGVI8CV1B
-fileName=data.zip
+fileName=data
 curl -sc cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' cookie)"  
-curl -Lb cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName} 
+curl -Lb cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
 
-unzip data.zip
-rm data.zip
+unzip ${fileName} || tar -zxvf ${fileName}
 rm cookie
