085a764ff96f9175076feb8005e435c14b2be23d
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-16 19:14:37 +0200

    fixed

--- get_data.sh
+++ get_data.sh
@@ -2,10 +2,11 @@
 
 #https://drive.google.com/open?id=1pEUYQ0X8mctP7q5pWtOOxaqf4W-IxaLf
 fileId=1pEUYQ0X8mctP7q5pWtOOxaqf4W-IxaLf
-fileName=data
+fileName=data.zip
 curl -sc cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' cookie)"  
 curl -Lb cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
 
 unzip ${fileName} || tar -zxvf ${fileName}
 rm cookie
+rm data.zip
