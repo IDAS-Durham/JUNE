80b269213581f9fe5512435a6e5c150ca5557eda
florpi <florpisoteadadejardin@gmail.com>
2020-04-16 16:50:34 +0100

    updating to new inputs tests

--- tests/test_schools.py
+++ tests/test_schools.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from covid.world import World
 import pandas as pd
 from covid.inputs import Inputs 
 import seaborn as sns
@@ -7,21 +8,17 @@ import pickle
 sns.set_context('paper')
 
 
-def load_world(path2world):
-
-    with open(path2world, 'rb') as f:
-        world = pickle.load(f)
-    return world
-
 def test_number_schools():
 
-    world = load_world('/cosma7/data/dp004/dc-quer1/world.pkl')
+    #world = load_world('/cosma7/data/dp004/dc-quer1/world.pkl')
+    world = World()
     inputs = Inputs()
     assert len(world.schools) ==  len(inputs.school_df)
 
 def test_all_kids_school():
     
-    world = load_world('/cosma7/data/dp004/dc-quer1/world.pkl')
+    #world = load_world('/cosma7/data/dp004/dc-quer1/world.pkl')
+    world = World()
     KIDS_LOW = 1
     KIDS_UP = 5
     lost_kids = 0
