c7b20f5ca134efc9cb154502be8ce549ad8e8ce0
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-18 19:38:15 +0200

    trying a rework

---
+++ covid/transmission_new.py
@@ -0,0 +1,10 @@
+import numpy as np
+import random
+
+class Transmission:
+    def __init__(self, config_dict):
+        self.init_from_config(config_dict)
+
+    def init_from_config(self, config_dict):
+        pass
+
