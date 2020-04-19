cde42dddc06bddac0ce5f5271c87ec05423b8e7c
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-19 13:07:47 +0200

    example default file

--- covid/transmission/constant.py
+++ covid/transmission/constant.py
@@ -1,5 +1,8 @@
+from covid.parameter_distributions import parameter_initializer
+
 class TransmissionConstant:
     def __init__(self, transmission_parameters):
+        self.prob_transmission = parameter_initializer(transmission_parameters)
 
     @property
     def probability(self):
