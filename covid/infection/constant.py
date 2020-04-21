99f6af597720431f6d019355f2ffa2087191b965
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-21 21:40:45 +0200

    basic working

--- covid/infection/constant.py
+++ covid/infection/constant.py
@@ -6,7 +6,19 @@ class InfectionConstant(Infection):
         super().__init__(person, timer, user_config, required_parameters)
 
     def update_infection_probability(self):
-        trans_probability = self.transmission.update_probability()
-        return trans_probability 
+        self.transmission.update_probability()
+        trans_prob = self.transmission.transmission_probability
+        self.infection_probability = trans_prob
 
+    @property
+    def still_infected(self):
+        #transmission_bool = (
+        #    self.transmission != None
+        #    and self.transmission.probability > self.threshold_transmission
+        #)
+        #symptoms_bool = (
+        #    self.symptoms != None and self.symptoms.severity > self.threshold_symptoms
+        #)
+        #is_infected = transmission_bool or symptoms_bool
+        return True
 
--- covid/interaction/base.py
+++ covid/interaction/base.py
@@ -8,8 +8,7 @@ import random
 class Interaction(ParameterInitializer):
 
     def __init__(self, user_parameters, required_parameters, world):
-        super().__init__("interaction", required_parameters)
-        self.initialize_parameters(user_parameters)
+        super().__init__("interaction", user_parameters, required_parameters)
         self.groups   = []
         self.world = world
 
