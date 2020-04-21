22ea9bc299fe5af0f97a70162f59059f916e157e
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-21 22:42:57 +0200

    fixed a bug where transmission probabiltiy was not read from user config

--- covid/time.py
+++ covid/time.py
@@ -29,7 +29,7 @@ class Timer():
         self.previous_day = self.day
         self.day += self.duration
         self.shift += 1
-        if int(self.day) - self.day < 1.e-6 :
+        if abs(int(self.day) - self.day) < 1.e-6 :
             self.shift = 0
         self.weekend = self.is_weekend()
         self.duration = self.get_shifts_duration(self.weekend)
