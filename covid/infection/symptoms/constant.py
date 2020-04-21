90db60ebf837fbc6f68ebbf4dd9989603107485a
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-21 16:19:52 +0200

    fixed logger error, infection folder

---
+++ covid/infection/symptoms/constant.py
@@ -0,0 +1,50 @@
+from covid.infection.symptoms import Symptoms 
+
+class SymptomsConstant(Symptoms):
+    def __init__(self, timer, health_index, user_parameters={}):
+        required_parameters = ["time_offset", "end_time"]
+        super().__init__(timer, health_index, user_parameters, required_parameters)
+        self.Toffset = max(0.0, self.time_offset)
+        self.Tend = max(0.0, self.end_time)
+
+    def _calculate_severity(self, time):
+        if time > self.starttime + self.Toffset and time < self.starttime + self.Tend:
+            severity = self.maxseverity
+        else:
+            severity = 0.
+        return severity
+
+
+
+if __name__=='__main__':
+
+    sc = SymptomsConstant(1)
+    print(sc.time_offset)
+    print(sc.end_time)
+
+    user_config = {
+                'time_offset': 
+                {
+                    'distribution': 'gaussian',
+                    'parameters': 
+                    {
+                        'mean': 5,
+                        'width_minus': 3,
+                    }
+                },
+                'end_time':
+                {
+                    'distribution': 'gaussian',
+                    'parameters': 
+                    {
+                        'mean': 15,
+                        'width_minus': 3,
+                    }
+                }
+
+            }
+
+
+    sc = SymptomsConstant(None, user_config)
+    print(sc.time_offset)
+    print(sc.end_time)
