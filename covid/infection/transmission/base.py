c4cb191a3344ff444271a4d6eaa46b091f503c3b
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-21 20:01:26 +0200

    changing transmission probabiltiy works

--- covid/infection/transmission/base.py
+++ covid/infection/transmission/base.py
@@ -5,10 +5,13 @@ class Transmission(ParameterInitializer):
         super().__init__("transmission", required_parameters)
         self.initialize_parameters(user_parameters)
         self.timer = timer
+        if timer != None:
+            self.infection_start_time = self.timer.now
+            self.last_time_updated = self.timer.now  # for testing
+        self.probability = 0.0
 
-    @property
-    def probability(self):
-        return None
+    def update_probability(self):
+        pass
 
 
 
