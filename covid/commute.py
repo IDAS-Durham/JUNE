aa36d39509525b867b8be8336656ef3dc87ef310
Richard <richardhayes777@gmail.com>
2020-04-22 14:31:20 +0100

    parse geography code

--- covid/commute.py
+++ covid/commute.py
@@ -1,14 +1,33 @@
 import csv
+from typing import List
+
+
+class RegionalGenerator:
+    def __init__(self, code: str):
+        self.code = code
 
 
 class CommuteGenerator:
-    def __init__(self, regional_generators):
+    def __init__(
+            self,
+            regional_generators: List[RegionalGenerator]
+    ):
         self.regional_generators = regional_generators
 
     @classmethod
     def from_file(cls, filename: str) -> "CommuteGenerator":
+        regional_generators = list()
         with open(filename) as f:
             reader = csv.reader(f)
-            next(reader)
-            rows = [row for row in reader]
-        return CommuteGenerator(rows)
+            headers = next(reader)
+            code_column = headers.index("geography code")
+            for row in reader:
+                code = row[code_column]
+                regional_generators.append(
+                    RegionalGenerator(
+                        code=code
+                    )
+                )
+        return CommuteGenerator(
+            regional_generators
+        )
