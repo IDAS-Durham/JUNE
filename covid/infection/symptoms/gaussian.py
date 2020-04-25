6cc996db1b734bf91f29979ebd710754d5359bf5
florpi <florpisoteadadejardin@gmail.com>
2020-04-25 13:02:21 +0100

    refactoring + pythonic naming

--- covid/infection/symptoms/gaussian.py
+++ covid/infection/symptoms/gaussian.py
@@ -1,5 +1,6 @@
 import numpy as np
-from covid.infection.symptoms import Symptoms 
+from covid.infection.symptoms import Symptoms
+
 
 class SymptomsGaussian(Symptoms):
     def __init__(self, timer, health_index, user_parameters={}):
@@ -15,37 +16,23 @@ class SymptomsGaussian(Symptoms):
         self.severity = self.maxseverity * np.exp(-(dt ** 2) / self.sigmaT ** 2)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
 
     sc = SymptomsGaussian(1)
     print(sc.mean_time)
     print(sc.sigma_time)
 
     user_config = {
-                'mean_time': 
-                {
-                    'distribution': 'gaussian',
-                    'parameters': 
-                    {
-                        'mean': 5,
-                        'width_minus': 3,
-                    }
-                },
-                'sigma_time':
-                {
-                    'distribution': 'gaussian',
-                    'parameters': 
-                    {
-                        'mean': 15,
-                        'width_minus': 3,
-                    }
-                }
-
-            }
-
+        "mean_time": {
+            "distribution": "gaussian",
+            "parameters": {"mean": 5, "width_minus": 3,},
+        },
+        "sigma_time": {
+            "distribution": "gaussian",
+            "parameters": {"mean": 15, "width_minus": 3,},
+        },
+    }
 
     sc = SymptomsGaussian(None, user_config)
     print(sc.mean_time)
     print(sc.sigma_time)
-
-
