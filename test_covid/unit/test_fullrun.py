5b0e24d2976a1b5b8f737fd4ec2f22666b5fa907
Richard <richardhayes777@gmail.com>
2020-04-26 09:25:33 +0100

    config directory relative to file

--- test_covid/unit/test_fullrun.py
+++ test_covid/unit/test_fullrun.py
@@ -3,20 +3,30 @@ This is a quick test that makes sure the box model can be run. It does not check
 but at least we can use it in the meantime to make sure the code runs before pusing it to master.
 """
 
-from covid import World
 import os
 
+from covid import World
+
+
 def test_full_run():
-    world = World(os.path.join("..", "configs", "config_example.yaml"), box_mode=False)
+    world = World(
+        os.path.join(
+            os.path.dirname(
+                os.path.realpath(__file__)
+            ),
+            "../..",
+            "configs",
+            "config_example.yaml"
+        ),
+        box_mode=False
+    )
     world.group_dynamics()
+
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+
     world = World(os.path.join("..", "configs", "config_example.yaml"), box_mode=False)
     world.group_dynamics()
     world.logger.plot_infection_curves_per_day()
     plt.show()
-
-
-
-
