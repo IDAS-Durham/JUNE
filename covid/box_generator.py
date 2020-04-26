5b2e20b3faabf5b642266dfdd16b642d2a376b54
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-26 20:23:52 +0200

    all tests working

--- covid/box_generator.py
+++ covid/box_generator.py
@@ -3,7 +3,8 @@ from covid.groups.people import HealthIndex
 from covid.groups import Person, Box
 import numpy as np
 
-def _get_age_brackets(nomis_age_bin):
+
+def get_age_brackets(nomis_age_bin):
     try:
         age_1, age_2 = nomis_age_bin.split("-")
         if age_2 == "XXX":
@@ -34,55 +35,61 @@ class BoxGenerator(Box):
         inputs = Inputs(zone=region)
         # sex numbers
         number_of_men = (
-            inputs.n_residents * np.array(inputs.sex_freq["males"]).flatten()
+            inputs.n_residents["n_residents"].values * inputs.sex_freq["males"].values
         )
-        number_of_men = number_of_men.sum()
+        number_of_men = int(number_of_men.sum())
         number_of_women = (
-            inputs.n_residents * np.array(inputs.sex_freq["females"]).flatten()
+            inputs.n_residents["n_residents"].values * inputs.sex_freq["females"].values
         )
-        number_of_women = number_of_women.sum()
+        number_of_women = int(number_of_women.sum())
         self.n_people = number_of_men + number_of_women
         # ages. since Nomis works in bins, I have to sample an age, this will be improved in the future TODO
         age_counts = inputs.age_freq
-        age_bins = age_counts.columns # get age bins from column names
+        age_bins = age_counts.columns  # get age bins from column names
         # need to add for all output areas
-        age_counts_total = np.array(age_counts.values) * np.array(inputs.n_residents.values)
+        age_counts_total = age_counts.values * inputs.n_residents.values
         # and sum over all areas
         age_counts_total = age_counts_total.sum(axis=0).astype(np.int)
-        # finally divide by total population
+        assert age_counts_total.sum() == self.n_people
         # we now create an array with as many ages as people
-        ages_expanded = []
+        ages_expanded = None
         for i, age_bin in enumerate(age_bins):
-            #for each age bin, create n ages where n is the number of people with that age.
-            age_1, age_2 = _get_age_brackets(age_bin)
+            # for each age bin, create n ages where n is the number of people with that age.
+            age_1, age_2 = get_age_brackets(age_bin)
             if age_1 == age_2:
-                age_array = age_1 * np.ones(age_counts_total[i])
+                age_array = age_1 * np.ones(age_counts_total[i], dtype=np.int)
             else:
-                age_array = np.random.randint(age_1, age_2, age_counts_total[i]) 
-            ages_expanded += age_array
-        
+                age_array = np.random.randint(age_1, age_2, age_counts_total[i])
+            if ages_expanded is None:
+                ages_expanded = age_array
+            else:
+                ages_expanded = np.concatenate((ages_expanded, age_array))
         np.random.shuffle(ages_expanded)
+        assert len(ages_expanded) == self.n_people
         age_counter = 0
         # init all men
         for _ in range(number_of_men):
             sex = 0
             age = ages_expanded[age_counter]
             health_index = self.health_index_gen.get_index_for_age(age)
-            person = Person(world=self.world, age=age, sex=sex, health_index=health_index)
-            counter += 1
+            person = Person(
+                world=self.world, age=age, sex=sex, health_index=health_index
+            )
+            age_counter += 1
             self.people.append(person)
 
         for _ in range(number_of_women):
             sex = 1
             age = ages_expanded[age_counter]
             health_index = self.health_index_gen.get_index_for_age(age)
-            person = Person(world=self.world, age=age, sex=sex, health_index=health_index)
-            counter += 1
+            person = Person(
+                world=self.world, age=age, sex=sex, health_index=health_index
+            )
+            age_counter += 1
             self.people.append(person)
-        
+
         # shuffle people just in case
         np.random.shuffle(self.people)
-
 
     def create_random_box(self, n_people):
         self.n_people = n_people
@@ -105,11 +112,15 @@ class BoxGenerator(Box):
             selected_ages = possible_ages
 
         people_per_age = n_people // len(selected_ages)
-        people_per_age_array = people_per_age * np.ones(len(selected_ages), dtype=np.int)
+        people_per_age_array = people_per_age * np.ones(
+            len(selected_ages), dtype=np.int
+        )
         # fill remaining people in random age groups
-        remaining_people = n_people % len(selected_ages) 
+        remaining_people = n_people % len(selected_ages)
         if remaining_people != 0:
-            random_ages = np.random.choice(possible_ages, remaining_people, replace=False)
+            random_ages = np.random.choice(
+                possible_ages, remaining_people, replace=False
+            )
             print(len(random_ages))
             people_per_age_array[random_ages] += 1
 
