38ec7d28ad118b6f146691a23db11269016a52dd
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-04-28 11:28:57 +0100

    Symptoms step updated

--- covid/infection/symptons.py
+++ covid/infection/symptons.py
@@ -23,8 +23,8 @@ class Symptoms:
         self.tags = ALLOWED_SYMPTOM_TAGS
         self.severity = 0.0
 
-    def update_severity(self, time):
-        self.last_time_updated = time
+    def update_severity_at_time(self, time):
+        raise NotImplementedError()
 
     def is_recovered(self):
         raise NotImplementedError()
@@ -37,15 +37,16 @@ class Symptoms:
         return self.tags[index]
 
 
-
 class SymptomsConstant(Symptoms):
-
     def __init__(self, health_index, recovery_rate=0.2):
 
         super().__init__(health_index=health_index)
 
         self.recovery_rate = recovery_rate
         self.predicted_recovery_time = stats.expon.rvs(scale=1.0 / self.recovery_rate)
+
+    def update_severity_at_time(self, time):
+        self.last_time_updated = time
 
     def is_recovered(self, deltat):
 
@@ -55,6 +56,7 @@ class SymptomsConstant(Symptoms):
         else:
             return False
 
+
 class SymptomsGaussian(Symptoms):
     def __init__(self, timer, health_index, user_parameters={}):
         required_parameters = ["mean_time", "sigma_time"]
@@ -62,7 +64,7 @@ class SymptomsGaussian(Symptoms):
         self.Tmean = max(0.0, self.mean_time)
         self.sigmaT = max(0.001, self.sigma_time)
 
-    def update_severity(self):
+    def update_severity_at_time(self):
         time = self.timer.now
         dt = time - (self.start_time + self.Tmean)
         self.last_time_updated = time
@@ -70,15 +72,17 @@ class SymptomsGaussian(Symptoms):
 
 
 class SymptomsStep(Symptoms):
-    def __init__(self, timer, health_index, user_parameters=None):
-        user_parameters = user_parameters or dict()
-        required_parameters = ["time_offset", "end_time"]
-        super().__init__(timer, health_index, user_parameters, required_parameters)
-        self.time_offset = max(0.0, self.time_offset)
-        self.end_time = max(0.0, self.end_time)
+    def __init__(self, health_index, start_time, time_offset=2.0, end_time=5.0):
 
-    def update_severity(self):
-        time = self.timer.now
+        super().__init__(health_index)
+
+        self.start_time = start_time
+        self.time_offset = max(0.0, time_offset)
+        self.end_time = max(0.0, end_time)
+        self.maxseverity = random.random()
+
+    def update_severity_at_time(self, time):
+
         if (
             time > self.start_time + self.time_offset
             and time < self.start_time + self.end_time
@@ -87,8 +91,8 @@ class SymptomsStep(Symptoms):
         else:
             severity = 0.0
 
-        self.last_time_updated = time
         self.severity = severity
+        self.last_time_updated = time
 
 
 class SymptomsTanh(Symptoms):
@@ -103,7 +107,7 @@ class SymptomsTanh(Symptoms):
         self.delta_onset = self.Tmax - self.Tonset
         self.delta_end = self.Tend - self.Tmax
 
-    def update_severity(self):
+    def update_severity_at_time(self):
         time = self.timer.now
         self.severity = 0.0
         time_since_start = time - self.start_time
--- test_covid/unit/infection/test_symptoms.py
+++ test_covid/unit/infection/test_symptoms.py
@@ -3,16 +3,15 @@ from covid.infection import symptons as sym
 import numpy as np
 import pytest
 
-class TestSymptoms:
 
+class TestSymptoms:
     def test__update_severity(self):
 
         symptom = sym.Symptoms()
 
 
 class TestSymptomsConstant:
-
-    def test__is_recovered__gives_correct_probabaility(self):
+    def test__is_recovered__correct_depedence_on_recovery_rate(self):
 
         np.random.seed(1)
 
@@ -30,3 +29,35 @@ class TestSymptomsConstant:
 
         assert symptom.is_recovered(deltat=0.0) == False
         assert symptom.is_recovered(deltat=1.0) == True
+
+
+class TestSymptomsStep:
+    def test__constructor__negatve_values_rounded_to_zero(self):
+
+        symptom = sym.SymptomsStep(
+            health_index=None, start_time=0.0, time_offset=-1.0, end_time=-2.0
+        )
+
+        assert symptom.time_offset == 0.0
+        assert symptom.end_time == 0.0
+
+    def test__update_severity__correct_dependence_on_parameters(self):
+
+        symptom = sym.SymptomsStep(
+            health_index=None, start_time=0.0, time_offset=1.0, end_time=2.0
+        )
+
+        symptom.update_severity_at_time(time=1.5)
+
+        assert symptom.severity == symptom.maxseverity
+        assert symptom.last_time_updated == 1.5
+
+        symptom.update_severity_at_time(time=0.1)
+
+        assert symptom.severity == 0.0
+        assert symptom.last_time_updated == 0.1
+
+        symptom.update_severity_at_time(time=10.0)
+
+        assert symptom.severity == 0.0
+        assert symptom.last_time_updated == 10.0
