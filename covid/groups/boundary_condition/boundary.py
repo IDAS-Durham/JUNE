cc8b7b962edec06d72662effada77204585a621a
Christoph Becker <christoph.becker@durham.ac.uk>
2020-04-28 16:18:35 +0100

    created age & sex distr. for boundary :duck:

--- covid/groups/boundary_condition/boundary.py
+++ covid/groups/boundary_condition/boundary.py
@@ -1,5 +1,6 @@
 import warnings
 import numpy as np
+import pandas as pd
 from scipy.stats import rv_discrete
 from tqdm.auto import tqdm
 
@@ -36,8 +37,13 @@ class Boundary(Group):
             #TODO companies shouldn always be completely full
             n_residents += (company.n_employees_max - company.n_employees)
 
-            sex_random_array = self.sex_rv.rvs(size=n_residents)
             nomis_bin_random_array = self.nomis_bin_rv.rvs(size=n_residents)
+            self.area.sex_rv = stats.rv_discrete(
+                values=(
+                    np.arange(0, len(sex_freq_per_compsec.columns.values)),
+                    sex_freq.values
+                )
+            )
 
             for i in n_residents:
                 
@@ -70,5 +76,39 @@ class Boundary(Group):
         simulated region, and initialize random variables following
         the discrete distribution.
         """
-        self.area.nomis_bin_rv
-        self.area.sex_rv
+
+        # sex-frequencies per company sector
+        f_col = [
+            col for col in self.world.inputs.compsec_by_sex_df.columns.values if "f " in col
+        ]
+        f_nrs_per_compsec = self.world.inputs.compsec_by_sex_df[f_col].sum(axis='rows')
+        
+        m_col = [
+            col for col in self.world.inputs.compsec_by_sex_df.columns.values if "m " in col
+        ]
+        m_nrs_per_compsec = self.world.inputs.compsec_by_sex_df[m_col].sum(axis='rows')
+
+        self.sex_freq_per_compsec = pd.DataFrame(
+            data=np.vstack((f_nrs_per_compsec.values, m_nrs_per_compsec.values)).T,
+            index=[idx.split(' ')[-1] for idx in m_nrs_per_compsec.index.values],
+            columns=["f", "m"],
+        )
+
+        # age-frequencies based on the whole simulated region
+        nomis_and_age_list = [
+            [person.nomis_bin, person.age] for person in self.world.people.members
+        ]
+        nomis_bin_arr, age_arr = np.array(nomis_and_age_list).T
+        (nomis_bin_unique, nomis_bin_counts) = np.unique(nomis_bin_arr, return_counts=True)
+        (age_unique, age_counts) = np.unique(age_arr, return_counts=True)
+
+        self.nomis_bin_freq = pd.DataFrame(
+            data=nomis_bin_counts,
+            index=list(nomis_bin_unique),
+            columns=["freq"],
+        )
+        self.age_freq = pd.DataFrame(
+            data=age_counts,
+            index=list(age_unique),
+            columns=["freq"],
+        )
