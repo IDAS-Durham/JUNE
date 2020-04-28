5e98b84464e92c4b7d1a39c3057c708580419fb2
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-04-28 11:40:13 +0100

    Symptons Gaussian

--- test_covid/unit/infection/test_symptoms.py
+++ test_covid/unit/infection/test_symptoms.py
@@ -7,7 +7,7 @@ import pytest
 class TestSymptoms:
     def test__update_severity(self):
 
-        symptom = sym.Symptoms()
+        symptom = sym.Symptoms(health_index=None)
 
 
 class TestSymptomsConstant:
@@ -29,6 +29,36 @@ class TestSymptomsConstant:
 
         assert symptom.is_recovered(deltat=0.0) == False
         assert symptom.is_recovered(deltat=1.0) == True
+
+
+class TestSymptomsGaussian:
+    def test__update_severity__correct_depedence_on_parameters(self):
+
+        np.random.seed(1)
+
+        symptom = sym.SymptomsGaussian(
+            health_index=None, start_time=0.0, mean_time=1.0, sigma_time=3.0
+        )
+
+        symptom.update_severity_at_time(time=0.0)
+
+        # dt = 1.0
+        # np.exp(-(1.0 ** 2) / 3.0 ** 2) = 0.89483
+
+        assert symptom.severity == pytest.approx(symptom.maxseverity * 0.89483, 1.0e-4)
+        assert symptom.last_time_updated == 0.0
+
+        symptom = sym.SymptomsGaussian(
+            health_index=None, start_time=1.0, mean_time=3.0, sigma_time=5.0
+        )
+
+        symptom.update_severity_at_time(time=2.0)
+
+        # dt = 2.0 - (1.0 + 3.0)
+        # np.exp(-(dt ** 2) / 5.0 ** 2)
+
+        assert symptom.severity == pytest.approx(symptom.maxseverity * 0.85214, 1.0e-4)
+        assert symptom.last_time_updated == 2.0
 
 
 class TestSymptomsStep:
