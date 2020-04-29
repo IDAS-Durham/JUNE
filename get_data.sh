ba36b235ed7646cba94d050e97512db34e2f56f2
florpi <florpisoteadadejardin@gmail.com>
2020-04-29 14:53:42 +0100

    new new data

--- get_data.sh
+++ get_data.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#https://drive.google.com/open?id=1i-HkNRbBwZojT5FuXIOJArEV2Q4lRCWa
-fileId=1i-HkNRbBwZojT5FuXIOJArEV2Q4lRCWa
+#https://drive.google.com/open?id=10xRco-yQlAzdBH6YbkbQk15CaTEuQ-wp
+fileId=10xRco-yQlAzdBH6YbkbQk15CaTEuQ-wp
 fileName=data.zip
 curl -sc cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
 code="$(awk '/_warning_/ {print $NF}' cookie)"  
