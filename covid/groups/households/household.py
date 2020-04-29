f4d111ce347e6d5ec4925bebaee3306c5e1a4946
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-04-29 16:04:52 +0200

    switching branch

--- covid/groups/households/household.py
+++ covid/groups/households/household.py
@@ -7,7 +7,10 @@ class Household(Group):
     """
 
     def __init__(self, house_id=None, composition=None, area=None):
-        super().__init__("Household_%03d"%house_id, "household") 
+        if house_id is None:
+            super().__init__(None, "household") 
+        else:
+            super().__init__("Household_%03d"%house_id, "household") 
         self.id = house_id
         self.area = area
         self.people = []
