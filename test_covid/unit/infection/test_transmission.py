a296cfc66a6fdf3a6ee4274707328b3d2bbdf88e
jammy2211 <james.w.nightingale@durham.ac.uk>
2020-04-29 10:47:31 +0100

    Refactored transmission

--- test_covid/unit/infection/test_transmission.py
+++ test_covid/unit/infection/test_transmission.py
@@ -6,12 +6,17 @@ import pytest
 
 class TestTransmission:
 
-    def test__update_probability(self):
+    def test__update_probability_at_time(self):
 
-        transmission = trans.Transmission(constant=0.3, timer=t.Timer())
+        transmission = trans.TransmissionConstant(start_time=0.0, proabability=0.3)
 
-        assert transmission.constant == 0.3
-     #   assert transmission.update_probability()
+        assert transmission.start_time == 0.0
+        assert transmission.probability == 0.3
+
+        transmission.update_probability_at_time(time=0.5)
+
+        assert transmission.last_time_updated == 0.5
+
 
 
 
