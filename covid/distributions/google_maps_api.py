4af3dfabbbb30257a653c466c986dfe6ccb0cb78
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-05-01 16:50:00 +0100

    add places to google_api and refactor

--- covid/distributions/google_maps_api.py
+++ covid/distributions/google_maps_api.py
@@ -8,11 +8,28 @@ class APICall():
 
     def __init__(key):
         self.key = key
+        self.client = googlemaps.Client(self.key)
 
+    def process_results(self, results):
+        locations = []
+        names = []
+        reviews = []
+        ratings = []
+        for i in results:
+            locations.append(i['geometry']['location'])
+            names.append(i['name'])
+            ratings.append(i['rating'])
+            reviews.append(i['user_ratings_total'])
+
+        return locations, names, reviews, ratings
+
+        
     def nearby_search(self, location, radius, location_type):
         """
-        :param location: location is a tuple of (latitude, longitude)
-        :param radius: meter radius search area around location coordinate
+        Searches nearby locations given a location and a radius for particular loction types
+
+        :param location: (tuple of ints) location is a tuple of (latitude, longitude)
+        :param radius: (int) meter radius search area around location coordinate
         :param location_type: (string) type of location being searched for
 
         Note: location types can be found here: https://developers.google.com/places/supported_types#table1
@@ -33,21 +50,52 @@ class APICall():
         resp_json_payload = response.json()
         results = resp_json_payload['results']
 
-        
-        locations = []
-        names = []
-        reviews = []
-        ratings = []
-        for i in results:
-            locations.append(i['geometry']['location'])
-            names.append(i['name'])
-            ratings.append(i['rating'])
-            reviews.append(i['user_ratings_total'])
+        locations, names, reviews, ratings = self.process_results(results)
 
         return locations, names, reviews, ratings
 
         
+    def places(self, query, location, radius, location_type = None):
+        """
+        Search places according to a certain query
 
+        :param query: (string) e.g. 'restaurant'
+        :param location: (tuple of ints) location is a tuple of (latitude, longitude)
+        :param radius: (int) meter radius search area around location coordinate
+        :param location_type: (string, optional) type of location being searched for
+        """
+
+        
+        try:
+            if location_type is not None:
+                call = self.client.places(
+                    query,
+                    location=location,
+                    radius=radius,
+                    region="UK",
+                    language-"en-UK",
+                    type=location_type
+                )
+            else:
+                call = self.client.places(
+                    query,
+                    location=location,
+                    radius=radius,
+                    region="UK",
+                    language-"en-UK"
+                )
+            pass
+
+        except:
+            raise Exception('Error: GET request failed')
+
+        results = call['results']
+
+        locations, names, reviews, ratings = process_results(results)
+
+        return locations, names, reviews, ratings
+            
+            
         
         
 
