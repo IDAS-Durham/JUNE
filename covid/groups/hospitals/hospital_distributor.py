5dd23285ef0a8ccb64fc4779456caecd87b80384
Christoph Becker <christoph.becker@durham.ac.uk>
2020-05-01 19:15:28 +0100

    last cleaning

--- covid/groups/hospitals/hospital_distributor.py
+++ covid/groups/hospitals/hospital_distributor.py
@@ -72,7 +72,7 @@ class HospitalDistributor:
             hospitals_rnd_arr = areas_rv.rvs(size=len(medics))
 
             for i,medic in enumerate(medics):
-                if medic.industry_specific != None:
+                if medic.industry_specific not None:
                     hospital = hospitals_in_msoa[hospitals_rnd_arr[i]]
                         
                     if (hospital.n_medics < hospital.n_medics_max):# and \
