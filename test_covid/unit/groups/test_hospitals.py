e1126cb6d2bccd23813ca05f89367743fb1429a7
florpi <florpisoteadadejardin@gmail.com>
2020-05-03 18:31:37 +0100

    :lipstick:

--- test_covid/unit/groups/test_hospitals.py
+++ test_covid/unit/groups/test_hospitals.py
@@ -62,16 +62,27 @@ def test__add_patient_release_patient(hospitals, health_info):
     assert len(hospitals.members[0].icu_patients) == 0 
     assert len(hospitals.members[0].patients) == 0 
 
+class MockArea:
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+
 
 @pytest.mark.parametrize("health_info", ["hospitalised", "intensive care"])
-def test__allocate_patient(hospitals, health_info):
+def test__allocate_patient_release_patient(hospitals, health_info):
     dummy_person = Person()
     dummy_person.health_information = MockHealthInformation(health_info) 
+    dummy_person.area = MockArea(hospitals.members[0].coordinates)
     assert dummy_person.in_hospital is None
     hospitals.allocate_patient(dummy_person)
     if health_info == 'hospitalised':
-        assert hospitals.members[0].patients[0] == dummy_person
+        assert dummy_person.in_hospital.patients[0] == dummy_person
     elif health_info == 'intensive care':
-        assert hospitals.members[0].icu_patients[0] == dummy_person
+        assert dummy_person.in_hospital.icu_patients[0] == dummy_person
+    selected_hospital = dummy_person.in_hospital
     assert dummy_person.in_hospital is not None
+    dummy_person.in_hospital.release_as_patient(dummy_person)
+    assert dummy_person.in_hospital is None
+    assert len(selected_hospital.icu_patients) == 0 
+    assert len(selected_hospital.patients) == 0 
+
 
