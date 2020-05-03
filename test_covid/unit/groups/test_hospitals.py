018a748b0c64664772cb0ddadbb8e1cb1ec1c823
florpi <florpisoteadadejardin@gmail.com>
2020-05-03 18:21:31 +0100

    mocking is good :alien:

--- test_covid/unit/groups/test_hospitals.py
+++ test_covid/unit/groups/test_hospitals.py
@@ -41,12 +41,14 @@ def test__given_hospital_finds_itself_as_closest(hospitals, hospitals_df, index)
 
     assert hospitals.members[closest_hospital_idx].name == hospitals.members[index].name
 
+class MockHealthInformation:
+    def __init__(self, tag):
+        self.tag = tag
 
-'''
 @pytest.mark.parametrize("health_info", ["hospitalised", "intensive care"])
-def test__add_patient(hospitals, hospitals_df, health_info):
+def test__add_patient_release_patient(hospitals, health_info):
     dummy_person = Person()
-    dummy_person.health_information.tag = health_info
+    dummy_person.health_information = MockHealthInformation(health_info) 
     assert dummy_person.in_hospital is None
     hospitals.members[0].add_as_patient(dummy_person)
     if health_info == 'hospitalised':
@@ -55,7 +57,23 @@ def test__add_patient(hospitals, hospitals_df, health_info):
         assert hospitals.members[0].icu_patients[0] == dummy_person
     assert dummy_person.in_hospital is not None
 
+    hospitals.members[0].release_as_patient(dummy_person)
+    assert dummy_person.in_hospital is None
+    assert len(hospitals.members[0].icu_patients) == 0 
+    assert len(hospitals.members[0].patients) == 0 
+
+
+'''
 @pytest.mark.parametrize("health_info", ["hospitalised", "intensive care"])
-def test__allocate_patient
+def test__allocate_patient(hospitals, health_info):
+    dummy_person = Person()
+    dummy_person.health_information = MockHealthInformation(health_info) 
+    assert dummy_person.in_hospital is None
+    hospitals.members[0].add_as_patient(dummy_person)
+    if health_info == 'hospitalised':
+        assert hospitals.members[0].patients[0] == dummy_person
+    elif health_info == 'intensive care':
+        assert hospitals.members[0].icu_patients[0] == dummy_person
+    assert dummy_person.in_hospital is not None
 '''
 
