344a35c567cd50d5a3acfcc6ee6d81d6d0f65a9c
florpi <florpisoteadadejardin@gmail.com>
2020-05-03 17:54:17 +0100

    created fixture

--- test_covid/unit/groups/test_hospitals.py
+++ test_covid/unit/groups/test_hospitals.py
@@ -6,27 +6,31 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 
-
-def test__total_number_hospitals_is_correct():
+@pytest.fixture(name="hospitals", scope="session")
+def create_hospitals():
     data_directory = Path(__file__).parent.parent.parent.parent
     hospital_path = data_directory / "data/processed/hospital_data/england_hospitals.csv"
     config_path = data_directory / "configs/defaults/hospitals.yaml"
-    hospital_df = pd.read_csv(hospital_path)
-    hospitals = Hospitals.from_file(hospital_path, config_path)
-    assert len(hospitals.members) == len(hospital_df)
+    return Hospitals.from_file(hospital_path, config_path)
+
+@pytest.fixture(name="hospitals_df", scope="session")
+def create_hospitals_df():
+    data_directory = Path(__file__).parent.parent.parent.parent
+    hospital_path = data_directory / "data/processed/hospital_data/england_hospitals.csv"
+    config_path = data_directory / "configs/defaults/hospitals.yaml"
+    return  pd.read_csv(hospital_path)
+
+
+def test__total_number_hospitals_is_correct(hospitals, hospitals_df):
+    assert len(hospitals.members) == len(hospitals_df)
 
 
 @pytest.mark.parametrize("index", [5, 500])
-def test__given_hospital_finds_itself_as_closest(index):
-    data_directory = Path(__file__).parent.parent.parent.parent
-    hospital_path = data_directory / "data/processed/hospital_data/england_hospitals.csv"
-    config_path = data_directory / "configs/defaults/hospitals.yaml"
-    hospital_df = pd.read_csv(hospital_path)
-    hospitals = Hospitals.from_file(hospital_path, config_path)
+def test__given_hospital_finds_itself_as_closest(hospitals, hospitals_df, index):
 
     r_max = 150.
     distances, closest_idx = hospitals.get_closest_hospitals(
-        hospital_df[["Latitude", "Longitude"]].iloc[index].values, 
+        hospitals_df[["Latitude", "Longitude"]].iloc[index].values, 
         r_max,
     )
 
@@ -37,3 +41,9 @@ def test__given_hospital_finds_itself_as_closest(index):
 
     assert hospitals.members[closest_hospital_idx].name == hospitals.members[index].name
 
+
+#def test__add_patient():
+
+
+
+#def test__add_icu_patient():
