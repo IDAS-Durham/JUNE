41111c87a7e86cbf6ddf5a49c990d97259dcafd2
Richard <richardhayes777@gmail.com>
2020-05-06 16:33:59 +0100

    fixtures

--- test_covid/unit/test_demography.py
+++ test_covid/unit/test_demography.py
@@ -1,23 +1,39 @@
-from covid.groups.people import demography as d
 import pytest
+
+from covid.groups.people import demography as d
 
 
 @pytest.fixture(
     name="demography"
 )
-def make_demography():
+def make_demography(super_area):
     return d.Demography.from_super_area(
-        "NorthEast"
+        super_area
     )
 
 
-def test_create_demography(demography):
-    assert demography.super_area == "NorthEast"
-    assert demography.residents_map["E00062207"] == 242
+@pytest.fixture(
+    name="super_area"
+)
+def make_super_area():
+    return "NorthEast"
 
 
-def test_population_size(demography):
+@pytest.fixture(
+    name="area"
+)
+def make_area():
+    return "E00062207"
+
+
+def test_create_demography(demography, super_area, area):
+    assert demography.super_area == super_area
+    assert demography.residents_map[area] == 242
+
+
+def test_get_population(demography, area):
     population = demography.population_for_area(
-        "E00062207"
+        area
     )
+    assert population.area == area
     assert len(population) == 242
