157335f8a9d1eb54b5a6fbdfa49a965e9941b540
florpi <florpisoteadadejardin@gmail.com>
2020-05-06 18:22:06 +0100

    some changes to make tests run

--- covid/interaction/interaction.py
+++ covid/interaction/interaction.py
@@ -4,9 +4,9 @@ import numpy as np
 class Interaction:
         
     def time_step(self, time, delta_time, group):
-       if self.group.size != 0:
+       if group.size != 0:
             self.single_time_step_for_group(group, time, delta_time)
-            self.group.update_status_lists(time=time, delta_time=delta_time)
+            group.update_status_lists(time=time, delta_time=delta_time)
                     
     def single_time_step_for_group(self):
         raise NotImplementedError()
