61d460bfe24d43af4ff5b9e59f5dda9763eb181a
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-05-07 11:27:26 +0200

    carehome tests do not need world

--- test_covid/unit/groups/test_carehomes.py
+++ test_covid/unit/groups/test_carehomes.py
@@ -1,36 +1,59 @@
 from collections import Counter
 import pickle
 import pytest
+import pandas as pd
+from pathlib import Path
+from covid.distributors import CareHomeDistributor
+from covid.distributors.carehome_distributor import CareHomeError
+from covid.groups import Person
 
-def test_number_carehomes(world_ne):
+@pytest.fixture(name="carehomes_df")
+def load_carehomes_df():
+    carehomes_path = Path(__file__).parent.parent.parent.parent / "data/processed/census_data/output_area/test/carehomes.csv" 
+    return pd.read_csv(carehomes_path)
+
+@pytest.fixture(name="carehome_distributor")
+def create_carehome_dist():
+    carehome_dist = CareHomeDistributor()
+    return carehome_dist
+
+class MockArea:
+    def __init__(self):
+        self.carehome = None
+        pass
+
+def test_assertion_no_carehome_residents(carehome_distributor):
+    area = MockArea()
+    with pytest.raises(CareHomeError) as e:
+        assert carehome_distributor.create_carehome_in_area(area, 0)
+    assert str(e.value) == "No carehome residents in this area."
+
+
+def test_number_carehomes(carehomes_df, carehome_distributor):
     """ 
     Check the number of schools is right
     """
-    inputs = world_ne.inputs
-    assert len(world_ne.carehomes.members) == len(inputs.carehomes_df[inputs.carehomes_df['N_carehome_residents'] > 0])
-
-
-def test_no_kids_carehome(world_ne):
-    """
-    Check that kids are not assigned to carehomes
-    """
-    KIDS_LOW = 0
-    KIDS_UP = 18
-    lost_kids = 0
-    for i in range(len(world_ne.areas.members)):
-        for j in range(len(world_ne.areas.members[i].people)):
-            if (world_ne.areas.members[i].people[j].age >= KIDS_LOW) and (
-                world_ne.areas.members[i].people[j].age <= KIDS_UP
-            ):
-                if world_ne.areas.members[i].people[j].carehome is not None:
-                    lost_kids += 1
-
-    assert lost_kids == 0
-
-
-def test_n_carehome_residents(world_ne):
-
-    for carehome in world_ne.carehomes.members:
-        assert world_ne.inputs.carehomes_df.loc[carehome.area.name, 'N_carehome_residents'] == carehome.n_residents
-        assert world_ne.inputs.carehomes_df.loc[carehome.area.name, 'N_carehome_residents']  == len(carehome.people) 
+    for value in carehomes_df.values[:,1]:
+        area = MockArea()
+        if value == 0:
+            try:
+                carehome_distributor.create_carehome_in_area(area, 0)
+            except:
+                assert area.carehome is None
+        else:
+            area.men_by_age = {}
+            area.women_by_age = {}
+            for age in range(50, 100):
+                area.men_by_age[age] = []
+                area.women_by_age[age] = []
+                for _ in range(max(1, value / (100-65+1))):
+                    man = Person(sex=0, age=age)
+                    woman = Person(sex=1, age=age)
+                    area.men_by_age[age].append(man)
+                    area.women_by_age[age].append(woman)
+            carehome_distributor.create_carehome_in_area(area, value)
+            assert area.carehome is not None
+            assert area.carehome.size == value
+            for person in area.carehome.people:
+                assert person.age >= 65
 
