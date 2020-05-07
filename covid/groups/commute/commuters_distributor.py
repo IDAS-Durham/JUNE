e1be4fdf8003ad6a51cfed03ee8e3d8b4bda780a
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-05-07 19:45:02 +0100

    finilising first draft of commer_distibutor

--- covid/groups/commute/commuters_distributor.py
+++ covid/groups/commute/commuters_distributor.py
@@ -1,6 +1,20 @@
 import numpy as np
 
-class CommutersDistributor:
+class CommuteUnitDistributor:
 
-    def __init__(self):
-        pass
+    def __init__(self, commuteunits, commutehubs):
+        self.commuteunits = commuteunits
+        self.commutehubs = commutehubs
+
+    def distribute_people(self):
+
+        for hub in self.commutehub:
+
+            possible_units = hub.commuteunits
+            to_commute = hub.passengers
+            
+            unit_choice = np.random.choice(
+                
+            )
+            # make people commute
+        
