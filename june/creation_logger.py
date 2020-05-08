76dcc49e55d624038b376fb92537f5e28a1725fa
Christoph Becker <christoph.becker@durham.ac.uk>
2020-05-08 01:36:45 +0100

    geography tests

--- june/creation_logger.py
+++ june/creation_logger.py
@@ -2,21 +2,26 @@ import os
 import yaml
 import logging
 import logging.config
+from pathlib import Path
+
+default_logging_config_filename = Path(__file__).parent.parent / \
+    "configs/config_world_creation_logger.yaml"
 
 
-def get_creation_logger(self, config_file: str = None):
+def get_creation_logger(config_file: str = None):
     """
     Create logger to make debugging easier
     """
     if config_file is None:
-        config_file = self.configs_dir + "config_create_world.yaml"
+        config_file = default_logging_config_filename
     if os.path.isfile(config_file):
+        print("Set up logger with default settings")
         with open(config_file, 'rt') as f:
             log_config = yaml.safe_load(f.read())
             logging.config.dictConfig(log_config)
     else:
         print("The provided logging config file does not exist.")
-        log_file = os.path.join(self.output_dir, "world_creation.log")
+        log_file = os.path.join("./", "world_creation.log")
         logging.basicConfig(
             filename=log_file, level=logging.DEBUG
         )
--- june/geography.py
+++ june/geography.py
@@ -2,36 +2,23 @@ import csv
 import logging
 from pathlib import Path
 from random import randint
-from itertools import count
+from itertools import chain, count
 from typing import List, Dict, Optional
 
 import numpy as np
+import pandas as pd
 
-from june.geography.areas import Area
-from june.geography.super_areas import SuperArea
 from june import get_creation_logger
 
-default_data_path_1 = Path(__file__).parent.parent.parent.parent / \
+default_data_path_1 = Path(__file__).parent.parent / \
         "data/census_data/area_code_translations"
-default_data_path_2 = Path(__file__).parent.parent.parent.parent / \
+default_data_path_2 = Path(__file__).parent.parent / \
         "data/processed/geographical_data"
 
-default_logging_config_filename = Path(__file__).parent.parent.parent.parent / \
+default_logging_config_filename = Path(__file__).parent.parent / \
         "configs/config_world_creation_logger.yaml"
 
-get_creation_logger(default_logging_config_filename)
 logger = logging.getLogger(__name__)
-
-
-class Areas:
-    def __init__(self, areas: List[Area]):
-        self.members = areas
-       
-    def __len__(self):
-        return len(self.members)
-
-    def __iter__(self):
-        return iter(self.members)
 
 
 class Area:
@@ -43,17 +30,18 @@ class Area:
     def __init__(
             self,
             name: str,
-            coordinates: Optional[[float, float]] = None,
-            super_area: str = None,
+            super_area: "SuperArea" = None,
+            coordinates: [float, float] = [None, None],
     ):
         """
         """
         self.id = next(self._id)
+        self.name = name
 
 
-class SuperAreas:
-    def __init__(self, super_areas: List[SuperArea]):
-        self.members = super_areas
+class Areas:
+    def __init__(self, areas: List[Area]):
+        self.members = areas
        
     def __len__(self):
         return len(self.members)
@@ -71,12 +59,23 @@ class SuperArea:
     def __init__(
             self,
             name: str,
-            coordinates: Optional[[float, float]] = None,
-            area: Optional[List[Area]] = None,
+            area: List[Area] = [None],
+            coordinates: [float, float] = [None, None],
     ):
         self.id = next(self._id)
-        self.super_area = super_area
+        self.name = name
         self.area = area
+
+
+class SuperAreas:
+    def __init__(self, super_areas: List[SuperArea]):
+        self.members = super_areas
+       
+    def __len__(self):
+        return len(self.members)
+
+    def __iter__(self):
+        return iter(self.members)
 
 
 class Geography:
@@ -100,6 +99,7 @@ class Geography:
         self.hierarchy = hierarchy
         self.hierarchy = _sorting_and_grouping(self.hierarchy)
         self.coordinates = coordinates
+        self.create_geographical_units()
 
     def create_geographical_units(self):
         """
@@ -108,31 +108,42 @@ class Geography:
         Note: This function looks a bit more complicated than need be,
         but it was created with a eye on the future.
         """
-        self.areas = Areas()
-        self.super_areas = SuperAreas()
+        areas_list = [] 
+        super_areas_list = []
 
         # loop through all but the smallest geo-unit
         for geo_unit_level in range(self.hierarchy.index.nlevels):  #Atm. only one level
-            geo_units_labels = dic.index.get_level_values(geo_unit_level).unique()
+            geo_units_labels = self.hierarchy.index.get_level_values(geo_unit_level).unique()
             
             # loop through this geo-unit
             for unit_label in geo_units_labels:
                 smaller_unit_df = self.hierarchy.loc[unit_label]
-                
+                superarea_areas_list = []
+
                 # loop over smallest geo-unit
                 for smaller_unit_label in smaller_unit_df.values[0].split(' '):
                      
-                    Area(
-                        name=smaller_unit_label,
-                        coordinates=self.get_area_coord(smaller_unit_label),
+                    superarea_areas_list.append(
+                        Area(
+                            name=smaller_unit_label,
+                            #coordinates=self.get_area_coord(smaller_unit_label),
+                        )
                     )
-                    self.areas.members.append(Area)
                 
-                SuperArea(
+                areas_list.append(superarea_areas_list)
+                super_area = SuperArea(
                     name=unit_label,
-                    area=areas,
+                    area=superarea_areas_list,
                 )
-                self.super_areas.append(SuperArea)
+                for area in superarea_areas_list:
+                    area.super_area = super_area
+                super_areas_list.append(super_area)
+       
+        self.areas = Areas(
+            list(chain.from_iterable(areas_list))
+        )
+        self.super_areas = SuperAreas(super_areas_list)
+        del superarea_areas_list, areas_list, super_areas_list
 
     def get_area_coord(self, area_name):
         """
@@ -142,19 +153,18 @@ class Geography:
         # NOTE df["X"] ~5 times faster than df[ ["Y", "X"] ]
         return [df_entry["Y"], df_entry["X"]]
     
-    def get_super_area_coord(self, area_name):
-        """
-        Read two numbers from input df, return as array.
-        """
-        return [df_entry["Y"], df_entry["X"]]
-
-oa = pd.read_csv('./../data/geographical_data/oa_coorindates.csv')
-oa_msoa = pd.read_csv(
-    './../data/census_data/area_code_translations/oa_msoa_englandwales_2011.csv'
-)
-oa_msoa_ll = pd.merge(oa_msoa, oa, on='OA11CD')
-oa_msoa_ll_mean = oa_msoa_ll.groupby('MSOA11CD', as_index=False)[['X','Y']].mean()
-oa_msoa_ll_mean.to_csv('msoa_coordinates_englandwales.csv')
+    #def get_super_area_coord(self, area_name):
+    #    """
+    #    Read two numbers from input df, return as array.
+    #    """
+    #    oa = pd.read_csv('./../data/geographical_data/oa_coorindates.csv')
+    #    oa_msoa = pd.read_csv(
+    #        './../data/census_data/area_code_translations/oa_msoa_englandwales_2011.csv'
+    #    )
+    #    oa_msoa_ll = pd.merge(oa_msoa, oa, on='OA11CD')
+    #    oa_msoa_ll_mean = oa_msoa_ll.groupby('MSOA11CD', as_index=False)[['X','Y']].mean()
+    #    oa_msoa_ll_mean.to_csv('msoa_coordinates_englandwales.csv')
+    #    return [df_entry["Y"], df_entry["X"]]
 
     @classmethod
     def from_file(
@@ -189,23 +199,23 @@ oa_msoa_ll_mean.to_csv('msoa_coordinates_englandwales.csv')
         geo_hierarchy = _load_geo_file(unit_hierarchy_file)
         smallest_unit_coord = _load_area_coords(smallest_unit_coords_file)
         
-        if filter_key not None:
+        if filter_key is not None:
             geo_hierarchy = _filtering(geo_hierarchy, filter_key)
 
         # At the moment we only support data at the UK OA & MSOA level.
         geo_hierarchy = geo_hierarchy[["MSOA", "OA"]]
-        return Geography(geo_hierarchy)
+        return Geography(geo_hierarchy, smallest_unit_coord)
 
 
 def _load_geo_file(
         names_path: str
-) -> pd.pd.DataFrame:
+) -> pd.DataFrame:
     """
     """
-    usecols = [1 ,3, 4]
+    usecols = [1,3, 4]
     column_names = ["OA", "MSOA", "LAD"]
     return pd.read_csv(
-        unit_hierarchy_file,
+        names_path,
         names=column_names,
         usecols=usecols,
     )
@@ -213,7 +223,7 @@ def _load_geo_file(
 
 def _load_area_coords(
         coords_path: str
-) -> pd.pd.DataFrame:
+) -> pd.DataFrame:
     """
     """
     return pd.read_csv(
@@ -223,12 +233,12 @@ def _load_area_coords(
 
 def _filtering(
         data: pd.DataFrame,
-        filter_key: Optional[Dict[str, list]] = None
-) -> pd.pd.DataFrame:
+        filter_key: Dict[str, list],
+) -> pd.DataFrame:
     """
     Filter DataFrame for given geo-unit and it's listed names
     """
-    return data[ data[filter_key["unit"]].isin(filter_key["names"]) ]
+    return data[ data[list(filter_key.keys())[0]].isin(list(filter_key.values())[0]) ]
 
 
 def _sorting_and_grouping(
@@ -245,8 +255,8 @@ def _sorting_and_grouping(
         and the column the smallest geographical unit.
     """
     # sorting
-    nr_unique_units = [len(dic[unit].unique())
-        for unit in dic.columns.values
+    nr_unique_units = [len(hierarchy[unit].unique())
+        for unit in hierarchy.columns.values
     ]
     idx = np.argsort(np.array(nr_unique_units))
     sorted_unit_labels = list(hierarchy.columns.values[idx])
