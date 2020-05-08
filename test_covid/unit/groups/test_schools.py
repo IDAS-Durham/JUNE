285dc8031cb5bbe04e9557d54b811ca532e7ede5
Richard <richardhayes777@gmail.com>
2020-05-08 14:02:34 +0100

    fixed test

--- test_covid/unit/groups/test_schools.py
+++ test_covid/unit/groups/test_schools.py
@@ -44,7 +44,7 @@ def test__all_kids_mandatory_school(world_ne):
     for area in world_ne.areas.members:
         for person in area.subgroups[0]._people:
             if (person.age >= KIDS_LOW) and (
-                person.age <= KIDS_UP
+                    person.age <= KIDS_UP
             ):
                 if person.school is None:
                     lost_kids += 1
@@ -77,13 +77,17 @@ def test__age_range_schools(world_ne):
                 n_outside_range += 1
     assert n_outside_range == 0
 
+
 def test__non_mandatory_dont_go_if_school_full(world_ne):
     non_mandatory_added = 0
     mandatory_age_range = world_ne.schools.mandatory_age_range
     for school in world_ne.schools.members:
         if school.n_pupils > school.n_pupils_max:
             ages = np.array(
-                [person.age for person in list(school.people)[int(school.n_pupils_max):]]
+                [person.age for person in list(sorted(
+                    school.people,
+                    key=lambda person: person.age
+                ))[int(school.n_pupils_max):]]
             )
             older_kids_when_full = np.sum(
                 ages > mandatory_age_range[1]
