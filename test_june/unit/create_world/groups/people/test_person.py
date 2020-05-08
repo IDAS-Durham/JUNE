4f716132a6b018857bbe3003295c85ce7cadf643
Christoph Becker <christoph.becker@durham.ac.uk>
2020-05-08 20:55:10 +0100

    final repo structure

--- test_june/unit/create_world/groups/people/test_person.py
+++
@@ -1,9 +0,0 @@
-# import pytest
-# import os
-#
-# from june.groups.people import person as p
-#
-# test_data_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "..", "..", "data", "census_data", "commute.csv")
-#
-# class TestHealthInformation:
-#
