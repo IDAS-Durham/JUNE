2f95d373ee422e1757bf85b1aa76de6dbeaf278b
florpi <florpisoteadadejardin@gmail.com>
2020-05-10 00:10:44 +0100

    I like :lipstick: :lipstick: :lipstick:

--- test_june/unit/simulator/test_seed.py
+++ test_june/unit/simulator/test_seed.py
@@ -1,24 +1,26 @@
-
 from june.simulator import Simulator
+
 
 def test__n_infected(simulator_box):
     n_infections = 50
-    simulator_box.seed(simulator_box.world.boxes.members[0],
-            n_infections=n_infections)
+    simulator_box.seed(simulator_box.world.boxes.members[0], n_infections=n_infections)
     assert len(simulator_box.world.people.infected) == n_infections
 
 
 def test__n_infected(world_ne, interaction, infection_constant):
-    simulator = Simulator.from_file(world_ne, interaction, infection_constant,
-            config_filename = test_directory / "config_ne.yaml")
-    n_infections = 2 
+    simulator = Simulator.from_file(
+        world_ne,
+        interaction,
+        infection_constant,
+        config_filename=test_directory / "config_ne.yaml",
+    )
+    n_infections = 2
     counter = 0
     household = simulator.world.households.members[counter]
     while household.size < 4:
         counter += 1
         household = simulator.world.households.members[counter]
-    simulator.seed(household,
-            n_infections=n_infections)
+    simulator.seed(household, n_infections=n_infections)
 
     assert len(household.infected) == n_infections
 
@@ -27,7 +29,3 @@ def test__n_infected(world_ne, interaction, infection_constant):
             print(person.health_information.infected)
 
     assert len(simulator.world.people.infected) == n_infections
-
-
-
-
