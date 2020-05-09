479d8d3ec6b2b1449ef3dd5e7353575aca22900f
florpi <florpisoteadadejardin@gmail.com>
2020-05-09 12:44:00 +0100

    hopefully this solves all my problems :lipstick: :lipstick:

--- june/groups/group/subgroup.py
+++ june/groups/group/subgroup.py
@@ -49,32 +49,7 @@ class Subgroup(AbstractGroup):
     def people(self):
         return self._people
 
-    def update_status_lists(self, time: int, delta_time: int):
-        """
-        Assign people in this group to sets based on their health status.
-        """
-        self._susceptible.clear()
-        self._infected.clear()
-        self._recovered.clear()
-        self._in_hospital.clear()
-        self._dead.clear()
-
-        for person in self.people:
-            # TODO: These two lines should be removed once health information update has been added to world
-            health_information = person.health_information
-            health_information.update_health_status(time, delta_time)
-            if health_information.susceptible:
-                self._susceptible.add(person)
-            elif health_information.infected_at_home:
-                self._infected.add(person)
-            elif health_information.in_hospital:
-                self._in_hospital.add(person)
-            elif health_information.recovered:
-                self._recovered.add(person)
-            elif person.health_information.dead:
-                self._dead.add(person)
-
-    def append(self, person: Person):
+   def append(self, person: Person):
         """
         Add a person to this group
         """
