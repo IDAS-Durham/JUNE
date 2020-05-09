09e17c1a1cd9699ebdfd686d0a304bcb5be61bbb
florpi <florpisoteadadejardin@gmail.com>
2020-05-09 10:59:51 +0100

    test seed in both box and non box

--- test_covid/unit/simulator/test_seed.py
+++ test_covid/unit/simulator/test_seed.py
@@ -7,3 +7,13 @@ def test__n_infected(simulator_box):
             n_infections=n_infections)
     assert len(simulator_box.world.people.infected) == n_infections
 
+
+def test__n_infected(simulator):
+    n_infections = 2 
+    simulator.seed(simulator.world.schools.members[0],
+            n_infections=n_infections)
+    assert len(simulator.world.people.infected) == n_infections
+
+
+
+
