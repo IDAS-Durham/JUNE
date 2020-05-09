254776cc94271b898cbd2973462a227859237b66
florpi <florpisoteadadejardin@gmail.com>
2020-05-09 23:59:19 +0100

    added scripts to process data

---
+++ june/data_formatting/census_data/care_homes.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import os
+
+raw_path = (
+    Path(os.path.abspath(__file__)).parent.parent.parent.parent
+    / "data/census_data/output_area/"
+)
+processed_path = (
+    Path(os.path.abspath(__file__)).parent.parent.parent.parent
+    / "data/processed/census_data/output_area/"
+)
+
+carehome_df = pd.read_csv(raw_path / "communal_people.csv")
+carehome_df.set_index(carehome_df["geography"], inplace=True)
+
+
+carehome_df = carehome_df[[col for col in carehome_df.columns if "Care home" in col]]
+all_care_homes = carehome_df.sum(axis=1)
+print(all_care_homes)
+assert len(all_care_homes) == 181408
+all_care_homes.to_csv(processed_path / "carehomes.csv")
