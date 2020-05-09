a4ae9a603709c6d5b5c8b4132865ec370c740570
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-05-09 17:12:42 +0100

    groups/__init__.py merge conflict resolve

--- june/groups/__init__.py
+++ june/groups/__init__.py
@@ -9,6 +9,5 @@ from .hospital import Hospital, Hospitals
 from .household import Household, Households
 from .pub import Pub, Pubs, PubFiller
 from .school import School, Schools
-from .boundary_condition import Boundary
 from .commute import CommuteCity, CommuteCities, CommuteCityDistributor, CommuteHub, CommuteHubs, CommuteHubDistributor, CommuteUnit, CommuteUnits, CommuteUnitDistributor
 from .test_group import TestGroup, TestGroups
