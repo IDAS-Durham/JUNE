21345f2b6339ab7145f3c61fc24fbc59bbd77384
florpi <florpisoteadadejardin@gmail.com>
2020-05-09 11:52:19 +0100

    there is no conservation of people because interaction doesnt take active into account

--- covid/interaction/default_interaction.py
+++ covid/interaction/default_interaction.py
@@ -63,7 +63,7 @@ class DefaultInteraction(Interaction):
         norm   = 1./max(1, group.size)
         for grouping in group.groupings:
             summed = 0.
-            for person in grouping.infected:
+            for person in grouping.infected_active:
                 individual = (
                     person.health_information.infection.transmission.probability
                 )
--- test_covid/unit/test_fullrun.py
+++ test_covid/unit/test_fullrun.py
@@ -6,6 +6,13 @@ but at least we can use it in the meantime to make sure the code runs before pus
 import os
 
 from covid import World
+from covid.groups.people.health_index import HealthIndex
+from covid.interaction import DefaultInteraction 
+from covid.infection import Infection
+from covid.infection.symptoms import SymptomsConstant
+from covid.infection.transmission import TransmissionConstant
+from covid.simulator import Simulator
+
 
 
 def test_full_run(world_ne):
@@ -22,6 +29,19 @@ if __name__ == "__main__":
             "config_ne.yaml"
     )
     world = World(config_path, box_mode=False)
-    world.group_dynamics()
-    world.logger.plot_infection_curves_per_day()
+
+    reference_health_index = HealthIndex().get_index_for_age(40)
+    symptoms = SymptomsConstant(health_index=reference_health_index, recovery_rate=0.05)
+    transmission = TransmissionConstant(probability=0.3) 
+    infection = Infection(transmission, symptoms)
+    interaction = DefaultInteraction()
+
+
+    simulator = Simulator.from_file(world, interaction, infection, config_filename = config_path)
+    for household in simulator.world.households.members[:20]:
+        simulator.seed(household, n_infections=1)
+    simulator.run()
+    simulator.logger.plot_infection_curves_per_day()
     plt.show()
+
+
