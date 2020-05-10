4090b3531f3d4398e8e7c215aab5cff4c2e6958c
Richard <richardhayes777@gmail.com>
2020-05-10 15:57:32 +0100

    autogenerating ids and names for groups

--- june/groups/household.py
+++ june/groups/household.py
@@ -1,9 +1,8 @@
-from itertools import count
+from enum import IntEnum
 
 import numpy as np
 
 from june.groups.group import Group
-from enum import IntEnum
 
 
 class Household(Group):
@@ -18,7 +17,7 @@ class Household(Group):
     """
     __slots__ = "area", "household_composition", "communal", "max_size"
 
-    _id = count()
+    spec = "household"
 
     class GroupType(IntEnum):
         kids = 0
@@ -27,8 +26,7 @@ class Household(Group):
         old_adults = 3
 
     def __init__(self, composition=None, communal=False, area=None, max_size=np.inf):
-        house_id = next(self._id)
-        super().__init__(f"Household_{house_id}", "household")
+        super().__init__()
         self.area = area
         self.household_composition = composition
         self.communal = communal
