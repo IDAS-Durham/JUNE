d5afcefe8e14203357f4428c5c9f2b5179ffc6d9
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-05-10 19:02:32 +0200

    school distributors tests working without world

--- test_june/unit/distributors/test_schools_distributor.py
+++ test_june/unit/distributors/test_schools_distributor.py
@@ -8,6 +8,9 @@ import pytest
 from june.geography import Geography
 from june.geography import Area
 from june.groups import *
+from june.geography import Geography
+from june.groups.school import Schools, School
+from june.distributors.school_distributor import SchoolDistributor
 
 default_data_filename = Path(os.path.abspath(__file__)).parent.parent.parent.parent / \
     "data/processed/school_data/england_schools_data.csv"
@@ -23,15 +26,29 @@ default_config = {
 
 default_mandatory_age_range = (5, 18)
 
-def test__all_kids_mandatory_school(world_ne):
+@pytest.fixture(name="geography", scope="module")
+def create_geography():
+    geography = Geography.from_file({"msoa" : ["E02004935"]})
+    return geography
+
+@pytest.fixture(name="schools", scope="module")
+def make_and_populate_schools(geography):
+    schools = Schools.for_geography(geography)
+    school_distributor = SchoolDistributor(schools)
+    school_distributor.distribute_kids_to_school(geography.areas)
+    return schools
+
+
+
+def test__all_kids_mandatory_school(schools, geography):
     """
     Check that all kids in mandatory school ages are assigned a school 
     """
     KIDS_LOW = default_mandatory_age_range[0]
     KIDS_UP = default_mandatory_age_range[1]
     lost_kids = 0
-    for area in world_ne.areas.members:
-        for person in area.subgroups[0]._people:
+    for area in geography.areas.members:
+        for person in area.people:
             if (person.age >= KIDS_LOW) and (
                     person.age <= KIDS_UP
             ):
@@ -40,11 +57,11 @@ def test__all_kids_mandatory_school(world_ne):
     assert lost_kids == 0
 
 
-def test__only_kids_school(world_ne):
+def test__only_kids_school(geography, schools):
     ADULTS_LOW = 20
     schooled_adults = 0
-    for area in world_ne.areas.members:
-        for person in area.subgroups[0]._people:
+    for area in geography.areas:
+        for person in area.people:
             if person.age >= ADULTS_LOW:
                 if person.school is not None:
                     schooled_adults += 1
@@ -52,25 +69,25 @@ def test__only_kids_school(world_ne):
     assert schooled_adults == 0
 
 
-def test__n_pupils_counter(world_ne):
-    for school in world_ne.schools.members:
+def test__n_pupils_counter(schools):
+    for school in schools.members:
         n_pupils = np.sum([len(grouping.people) for grouping in school.subgroups])
         assert n_pupils == school.n_pupils
 
 
-def test__age_range_schools(world_ne):
+def test__age_range_schools(schools):
     n_outside_range = 0
-    for school in world_ne.schools.members:
+    for school in schools.members:
         for person in school.people:
             if person.age < school.age_min or person.age > school.age_max:
                 n_outside_range += 1
     assert n_outside_range == 0
 
 
-def test__non_mandatory_dont_go_if_school_full(world_ne):
+def test__non_mandatory_dont_go_if_school_full(schools):
     non_mandatory_added = 0
     mandatory_age_range = default_mandatory_age_range
-    for school in world_ne.schools.members:
+    for school in schools.members:
         if school.n_pupils > school.n_pupils_max:
             ages = np.array(
                 [person.age for person in list(sorted(
