4d2a145424dd5f4e5bc22d88dd2613550117b28a
Christoph Becker <christoph.becker@durham.ac.uk>
2020-05-12 10:17:58 +0100

    no warnings, more tests

--- test_june/unit/distributors/test_workers_distributor.py
+++ test_june/unit/distributors/test_workers_distributor.py
@@ -1,9 +1,11 @@
 import os
+import yaml
 from pathlib import Path
 
 import numpy as np
 import pandas as pd
 import pytest
+import unittest
 
 from june.geography import Geography
 from june.demography import Demography
@@ -11,25 +13,29 @@ from june.demography import Person, Population
 from june.distributors import WorkerDistributor
 
 
-default_data_path = Path(os.path.abspath(__file__)).parent.parent.parent / "data/"
-default_workflow_file = default_data_path / \
-        "processed/flow_in_msoa_wu01ew_2011.csv"
-default_sex_per_sector_per_superarea_file = default_data_path / \
-        "processed/census_data/company_data/companysector_by_sex_cleaned.csv"
-default_education_sector_file = default_data_path / \
-        "processed/census_data/company_data/education_by_sex_2011.csv"
-default_healthcare_sector_file = default_data_path / \
-        "processed/census_data/company_data/healthcare_by_sex_2011.csv"
-default_areas_map_path = default_data_path / \
-        "processed/geographical_data/oa_msoa_region.csv"
-#TODO put in config
-working_age_min = 18
-working_age_min = 65
-default_key_compsec_id = ["P", "Q"]
+default_base_path = Path(os.path.abspath(__file__)).parent.parent.parent.parent
+default_workflow_file = default_base_path / \
+        "data/processed/flow_in_msoa_wu01ew_2011.csv"
+default_sex_per_sector_per_superarea_file = default_base_path / \
+        "data/processed/census_data/company_data/companysector_by_sex_cleaned.csv"
+default_areas_map_path = default_base_path / \
+        "data/processed/geographical_data/oa_msoa_region.csv"
+default_config_file = default_base_path / \
+        "configs/defaults/distributors/worker_distributor.yaml"
+
+@pytest.fixture(name="config", scope="session")
+def load_config():
+    with open(default_config_file) as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    return config
+
+@pytest.fixture(name="super_areas", scope="session")
+def use_super_areas():
+    return ["E02002559", "E02002560", "E02002561"]
 
 @pytest.fixture(name="geography", scope="session")
-def create_geography():
-    return Geography.from_file(filter_key={"msoa" : ["E02002559", "E02002560", "E02002561"]})
+def create_geography(super_areas):
+    return Geography.from_file(filter_key={"msoa" : super_areas})
 
 
 @pytest.fixture(name="population", scope="session")
@@ -42,6 +48,20 @@ def test__workers_distribution_for_geography(geography, population):
     workers_distributor = WorkerDistributor.for_geography(geography)
     return workers_distributor.distribute(geography, population)
 
+def test__workers_stay_in_geography(geography, population, super_areas, config):
+    case = unittest.TestCase()
+    workers_distributor = WorkerDistributor.for_geography(geography)
+    workers_distributor.distribute(geography, population)
+    work_super_area_name = np.array([
+        person.work_super_area.name
+        for person in population.people
+        if config["age_range"][0] <= person.age <= config["age_range"][1]
+    ])
+    work_super_area_name = list(np.unique(work_super_area_name))
+    print(work_super_area_name)
+    print(super_areas)
+    case.assertCountEqual(work_super_area_name, super_areas)
+
 # work in progress:
 #def test__sub_sector_ratio_for_geography(geography, population):
 #    workers_distributor = WorkerDistributor.for_geography(geography)
@@ -53,6 +73,4 @@ def test__workers_distribution_for_geography(geography, population):
 #    idx = np.where(sector == "P")[0]
 #    print(idx)
 #    ratio
-
-
 
