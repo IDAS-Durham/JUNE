30b8cdcfa0de875badd2e420c65d0f088c619aa8
florpi <florpisoteadadejardin@gmail.com>
2020-05-12 13:34:34 +0100

    getting seed data in

--- june/data_formatting/seed/reformat_seed.py
+++ june/data_formatting/seed/reformat_seed.py
@@ -13,8 +13,9 @@ processed_path = (
 
 
 seed_df = pd.read_csv(raw_path / "Seeding_March_first_Try.csv", )
+print(seed_df)
 
 n_cases_region = seed_df.groupby('Region').sum()
 
-n_cases_region[' N_cases_1_march'].to_csv(processed_path / "n_cases_region.csv")
-n_cases_region[' N_deaths_24_march'].to_csv(processed_path / "n_deaths_region.csv")
+n_cases_region['N_cases_1_march'].to_csv(processed_path / "n_cases_region.csv")
+n_cases_region['N_deaths_24_march'].to_csv(processed_path / "n_deaths_region.csv")
