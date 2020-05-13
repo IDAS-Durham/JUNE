29e9a8d0c3ff9e3f53a78422fd40fb85f92c0c01
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-05-13 23:42:05 +0200

    reworked company and company distributor

--- june/distributors/company_distributor.py
+++ june/distributors/company_distributor.py
@@ -12,44 +12,36 @@ according to census data.
 class CompanyDistributor:
     """
     Distributes workers that are not yet working in key company sectors
-    (e.g. such as schools and hospitals) to companies.
+    (e.g. such as schools and hospitals) to companies. This assumes that
+    the WorkerDistributor has already been run to allocate workers in
+    a super_area
     """
 
-    def __init__(self, companies, super_area, config):
+    def __init__(self):
         """Get all companies within SuperArea"""
-        self.msoarea = super_area
-        self.companies = companies
-        self.config = config
 
-    def distribute_adults_to_companies(self):
+    def distribute_adults_to_companies_in_super_area(self, super_area):
         """
+        Looks for all workers and companies in the super area and matches
+        them
         """
-        STUDENT_THRESHOLD = self.config["people"]["student_age_group"]
-        ADULT_THRESHOLD = self.config["people"]["adult_threshold"]
-        OLD_THRESHOLD = self.config["people"]["old_threshold"]
-
-        for person in self.msoarea.work_people:
-
-            comp_choice = np.random.choice(
-                len(self.msoarea.companies), len(self.msoarea.companies), replace=False
-            )
-
-            for idx in comp_choice:
-                company = self.msoarea.companies[idx]
-
-                if (
-                    person.industry == company.industry
-                    and company.n_employees < company.n_employees_max
-                ):
-                    company.n_employees += 1
-                    company.n_woman     += person.sex  # remember: woman=1;man=0
-                    company.add(person,"worker")
-                    break
-                # TODO: Take care if cases where people did not find any
-                # company at all
+        # shuffle companies
+        for person in super_area.workers:
+            compatible_companies = []
+            for company in super_area.companies:
+                if person.sector == company.sector:
+                    compatible_companies.append(company)
+                    if company.n_workers < company.n_workers_max:
+                        company.add(person)
+                        break
+                    
+            # allocate randomly if no place for him/her
+            if len(compatible_companies) == 0:
+                company = np.random.choice(super_area.companies)
+            else:
+                company = np.random.choice(compatible_companies)
 
         # remove companies with no employees
-        for company in self.msoarea.companies:
-            if company.n_employees == 0:
-                self.msoarea.companies.remove(company)
-                self.companies.members.remove(company)
+        for company in super_area.companies:
+            if company.n_workers == 0:
+                super_area.companies.remove(company)
