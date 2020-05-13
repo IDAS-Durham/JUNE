6d386cae29ec7a882ab0463247d933e09a623d12
florpi <florpisoteadadejardin@gmail.com>
2020-05-13 11:50:17 +0100

    seed works but we need to figure out how to split data in the right way

--- june/data_formatting/seed/reformat_seed.py
+++ june/data_formatting/seed/reformat_seed.py
@@ -15,10 +15,13 @@ processed_path = (
 seed_df = pd.read_csv(raw_path / "Seeding_March_first_Try.csv", 
         index_col=0)
 
+print(seed_df)
+print(seed_df['Region'].unique())
 seed_df = seed_df[['Region', 'N_cases_1_march', 'N_deaths_24_march']]
 seed_df.columns = ['region', 'n_cases', 'n_deaths']
 
 n_cases_region = seed_df.groupby('region').sum()
+print(n_cases_region)
 
 n_cases_region['n_cases'].to_csv(processed_path / "n_cases_region.csv")
 n_cases_region['n_deaths'].to_csv(processed_path / "n_deaths_region.csv")
