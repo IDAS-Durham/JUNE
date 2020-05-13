56bfd7f20a96e11a1fa186018e15c3a496e97162
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-05-13 11:22:51 +0100

    adding commuting unit tests

--- test_june/unit/groups/test_commuting.py
+++ test_june/unit/groups/test_commuting.py
@@ -8,7 +8,7 @@ import pandas as pd
 from june.geography import Geography
 from june.geography import Area
 from june.demography import Person
-from june.groups import CommuteCity, CommuteCities
+from june.groups import CommuteCity, CommuteCities, CommuteHub, CommuteHubs
 
 @pytest.fixture(name="super_area_commute", scope="module")
 def super_area_name():
@@ -34,3 +34,46 @@ class TestCommuteCity:
 
     def test__city_grouptype(self, city):
         assert len(city.GroupType.passengers) == 0
+        assert len(city.GroupType.commutehubs) == 0
+        assert len(city.GroupType.commute_internal) == 0
+        assert len(city.GroupType.commutecityunits) == 0
+
+class TestCommuteHub:
+    @pytest.fixture(name="hub")
+    def create_hub(self):
+        return CommuteHub(
+            commutehub_id = 0,
+            city = 'Manchester',
+            lat_lon = [-2,52.],
+        )
+
+    def test__hub_grouptype(self, hub):
+        assert len(hub.GroupType.passengers) == 0
+        assert len(hub.GroupType.commuteunits) == 0
+
+class TestCommuteHubUnit:
+    @pytest.fixture(name="unit")
+    def create_hub(self):
+        return CommuteHubUnit(
+            commutehubunit_id = 0,
+            city = 'Manchester',
+            commutehub_id = 0,
+            is_peak = False,
+        )
+
+    def test__unit_grouptype(self, unit):
+        assert len(unit.GroupType.passengers) == 0
+        assert unit.GroupType.max_passengers != 0
+
+class TestCommuteCityUnit:
+    @pytest.fixture(name="unit")
+    def create_hub(self):
+        return CommuteCityUnit(
+            commutecityunit_id = 0,
+            city = 'Manchester',
+            is_peak = False,
+        )
+
+    def test__unit_grouptype(self, unit):
+        assert len(unit.GroupType.passengers) == 0
+        assert unit.GroupType.max_passengers != 0
