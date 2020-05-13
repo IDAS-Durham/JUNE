759aa057511275b517cb217726e857217aeb3c8e
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-05-14 00:15:49 +0200

    minor bug

--- june/world.py
+++ june/world.py
@@ -9,6 +9,7 @@ from june.distributors import (
     HouseholdDistributor,
     CareHomeDistributor,
     WorkerDistributor,
+    CompanyDistributor,
 )
 from june.geography import Geography
 from june.groups import Hospitals
@@ -33,11 +34,11 @@ class World:
     """
 
     def __init__(
-            self,
-            geography: Geography,
-            demography: Demography,
-            include_households: bool = True,
-            box_mode=False
+        self,
+        geography: Geography,
+        demography: Demography,
+        include_households: bool = True,
+        box_mode=False,
     ):
         """
         Initializes a world given a geography and a demography. For now, households are
@@ -57,20 +58,14 @@ class World:
         self.box_mode = box_mode
         if self.box_mode:
             self.hospitals = Hospitals.for_box_mode()
-            self.people = _populate_areas(
-                geography,
-                demography
-            )
+            self.people = _populate_areas(geography, demography)
             self.boxes = Boxes([Box()])
             self.boxes.members[0].set_population(self.people)
             return
         self.areas = geography.areas
         self.super_areas = geography.super_areas
         print("populating the world's geography with the specified demography...")
-        self.people = _populate_areas(
-            geography,
-            demography
-        )
+        self.people = _populate_areas(geography, demography)
 
         if hasattr(geography, "carehomes"):
             self.carehomes = geography.carehomes
@@ -81,9 +76,9 @@ class World:
                 self.areas
             )
         if (
-                hasattr(geography, "companies")
-                or hasattr(geography, "hospitals")
-                or hasattr(geography, "schools")
+            hasattr(geography, "companies")
+            or hasattr(geography, "hospitals")
+            or hasattr(geography, "schools")
         ):
             worker_distr = WorkerDistributor.for_geography(
                 geography
@@ -94,10 +89,16 @@ class World:
             self.schools = geography.schools
             school_distributor = SchoolDistributor(geography.schools)
             school_distributor.distribute_kids_to_school(self.areas)
-            school_distributor.distribute_teachers_to_schools_in_super_areas(self.super_areas)
+            school_distributor.distribute_teachers_to_schools_in_super_areas(
+                self.super_areas
+            )
 
         if hasattr(geography, "companies"):
             self.companies = geography.companies
+            company_distributor = CompanyDistributor()
+            company_distributor.distribute_adults_to_companies_in_super_areas(
+                geography.super_areas
+            )
 
         if hasattr(geography, "hospitals"):
             self.hospitals = geography.hospitals
