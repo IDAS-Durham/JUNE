3c23e707b8690162586f5c3d28a1b1a13e320e30
Richard <richardhayes777@gmail.com>
2020-05-14 11:21:32 +0100

    cleaning

--- june/interaction/interaction.py
+++ june/interaction/interaction.py
@@ -1,14 +1,11 @@
-import random
 from abc import ABC, abstractmethod
 
-import numpy as np
 
 class Interaction(ABC):
     def time_step(self, time, health_index_generator, delta_time, group):
-       if group.contains_people:
-            self.single_time_step_for_group(group, health_index_generator,time, delta_time)
-                    
-    @abstractmethod
-    def single_time_step_for_group(self):
-        pass
+        if group.contains_people:
+            self.single_time_step_for_group(group, health_index_generator, time, delta_time)
 
+    @abstractmethod
+    def single_time_step_for_group(self, group, health_index_generator, time, delta_time):
+        pass
