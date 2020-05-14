d1d5720d7defb72cb68d0a37ffa5696d7682b6fb
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-05-14 14:30:49 +0100

    commuteunits and distributor working

--- june/groups/commute/commutehub_distributor.py
+++ june/groups/commute/commutehub_distributor.py
@@ -4,6 +4,7 @@ from enum import IntEnum
 from pathlib import Path
 from typing import List, Dict, Optional
 
+import csv
 import numpy as np
 import pandas as pd
 from scipy import spatial
@@ -29,23 +30,48 @@ class CommuteHubDistributor:
         #self.msoa_oa_coordinates = msoa_oa_coordinates
         self.commutecities = commutecities
 
-    def _get_msoa_oa(self,oa):
-        'Get MSOA for a give OA'
-        
-        msoa = self.msoa_oa_coordinates['MSOA11CD'][self.msoa_oa_coordinates['OA11CD'] == oa]
+    #def _get_msoa_oa(self,oa):
+    #    'Get MSOA for a give OA'
+    #    
+    #    msoa = self.msoa_oa_coordinates['MSOA11CD'][self.msoa_oa_coordinates['OA11CD'] == oa]
+    #
+    #     return msoa
 
-        return msoa
+    def _get_msoa_oa(self, oa):
+         'Get MSOA for a given OA'
+         return self.coordinates_dict[
+             oa
+         ]["MSOA11CD"]
 
     def _get_area_lat_lon(self, oa):
         'Get lat/lon for  a given OA'
-        lat = float(self.msoa_oa_coordinates['Y'][self.msoa_oa_coordinates['OA11CD'] == oa])
-        lon = float(self.msoa_oa_coordinates['X'][self.msoa_oa_coordinates['OA11CD'] == oa])
+        #lat = float(self.msoa_oa_coordinates['Y'][self.msoa_oa_coordinates['OA11CD'] == oa])
+        #lon = float(self.msoa_oa_coordinates['X'][self.msoa_oa_coordinates['OA11CD'] == oa])
 
-        return [lat,lon]
+        area_dict = self.coordinates_dict[
+             oa
+         ]
+        return area_dict["Y"], area_dict["X"]
 
     def from_file(self):
 
-        self.msoa_oa_coordinates = pd.read_csv(default_msoa_oa_coordinates)
+        #self.msoa_oa_coordinates = pd.read_csv(default_msoa_oa_coordinates)
+
+        coordinates_dict = dict()
+        with open(default_msoa_oa_coordinates) as f:
+            reader = csv.reader(f)
+            headers = next(reader)
+            key_index = headers.index("OA11CD")
+            for row in reader:
+                row_dict = dict(zip(
+                    headers,
+                    row
+                ))
+                row_dict["X"] = float(row_dict["X"])
+                row_dict["Y"] = float(row_dict["Y"])
+                coordinates_dict[row[key_index]] = row_dict
+                
+        self.coordinates_dict = coordinates_dict
 
     def distribute_people(self):
 
@@ -57,7 +83,8 @@ class CommuteHubDistributor:
             to_commute_in = []
             to_commute_out = []
             for work_person in work_people:
-                msoa = list(self._get_msoa_oa(work_person.area.name))[0]
+                #msoa = list(self._get_msoa_oa(work_person.area.name))[0]
+                msoa = self._get_msoa_oa(work_person.area.name)
                 # check if live AND work in metropolitan area
                 if msoa in commutecity.metro_msoas:
                     to_commute_in.append(work_person)
