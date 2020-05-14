d11f8f1cba58af8cba930ba1fdc96a70be78bfac
Richard <richardhayes777@gmail.com>
2020-05-14 15:19:20 +0100

    warning

--- june/paths.py
+++ june/paths.py
@@ -1,13 +1,18 @@
+import logging
 import os
 from pathlib import Path
 from sys import argv
+
+logger = logging.getLogger(
+    __name__
+)
 
 directory = Path(
     os.path.abspath(__file__)
 ).parent
 
 
-def path_for_flag(flag: str) -> Path:
+def path_for_name(name: str) -> Path:
     """
     Get a path input using a flag when the program is run.
 
@@ -19,20 +24,24 @@ def path_for_flag(flag: str) -> Path:
 
     Parameters
     ----------
-    flag
+    name
         A string such as "data" which corresponds to the flag --data
 
     Returns
     -------
     A path
     """
+    flag = f"--{name}"
     try:
-        path = argv[argv.index(f"--{flag}") + 1]
+        path = argv[argv.index(flag) + 1]
     except (IndexError, ValueError):
-        path = str(directory.parent / flag)
+        path = str(directory.parent / name)
+        logger.warning(
+            f"No {flag} argument given. Defaulting to {path}"
+        )
 
     return Path(path)
 
 
-data_path = path_for_flag("data")
-configs_path = path_for_flag("configs")
+data_path = path_for_name("data")
+configs_path = path_for_name("configs")
