4ebf37eb0e304fba2a3e303be80d985dd7b26801
Richard <richardhayes777@gmail.com>
2020-05-14 15:17:27 +0100

    schools and docs

--- june/paths.py
+++ june/paths.py
@@ -7,7 +7,25 @@ directory = Path(
 ).parent
 
 
-def path_for_flag(flag):
+def path_for_flag(flag: str) -> Path:
+    """
+    Get a path input using a flag when the program is run.
+
+    If no such argument is given default to the directory above
+    the june with the name of the flag appended.
+
+    e.g. --data indicates where the data folder is and defaults
+    to june/../data
+
+    Parameters
+    ----------
+    flag
+        A string such as "data" which corresponds to the flag --data
+
+    Returns
+    -------
+    A path
+    """
     try:
         path = argv[argv.index(f"--{flag}") + 1]
     except (IndexError, ValueError):
