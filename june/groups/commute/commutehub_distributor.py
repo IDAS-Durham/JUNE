9853c88cb9bcbb7d7854e9023c8c9cb4ff39c1d0
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-05-14 14:00:58 +0100

    commutehub integrated

--- june/groups/commute/commutehub_distributor.py
+++ june/groups/commute/commutehub_distributor.py
@@ -10,7 +10,7 @@ from scipy import spatial
 
 
 default_data_path = (
-    Path(os.path.abspath(__file__)).parent.parent.parent
+    Path(os.path.abspath(__file__)).parent.parent.parent.parent
     / "data/"
 )
 
