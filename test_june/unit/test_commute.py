c16e14f04a8a22f735155e75aa3d314029786be4
Matthew Townson <matthew.townson@durham.ac.uk>
2020-05-15 10:03:00 +0100

    Experimenting with saving data and code separately

--- test_june/unit/test_commute.py
+++ test_june/unit/test_commute.py
@@ -8,9 +8,7 @@ from june.groups import CommuteHubDistributor
 from june.groups.commute import default_geographical_data_directory
 
 test_data_filename = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    "..",
-    "..",
+    "~",
     "data",
     "census_data",
     "commute.csv"
