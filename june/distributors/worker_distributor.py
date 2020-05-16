9c6fce25e256ca15c58a1da9a37f0a8576155b4e
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-05-16 21:18:02 +0200

    readded argument to workerdistributor init

--- june/distributors/worker_distributor.py
+++ june/distributors/worker_distributor.py
@@ -39,7 +39,7 @@ class WorkerDistributor:
         age_range: List[int],
         sub_sector_ratio: dict,
         sub_sector_distr: dict,
-
+        non_geographical_work_location: dict,
     ):
         """
         Parameters
