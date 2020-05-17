805802bc28c9fff813f27b0c353580d27b07d56d
florpi <florpisoteadadejardin@gmail.com>
2020-05-18 00:20:57 +0100

    people do like commuting

--- june/groups/commute/commutehub_distributor.py
+++ june/groups/commute/commutehub_distributor.py
@@ -113,7 +113,7 @@ class CommuteHubDistributor:
                 _, hub_index = commutehub_tree.query(live_lat_lon,1) 
                 
                 commutehub_in_city[hub_index].add(work_person,
-                        activity_type=None,
+                        activity_type=work_person.ActivityType.commute,
                         subgroup_type=commutehub_in_city[hub_index].SubgroupType.default
                         )
 
