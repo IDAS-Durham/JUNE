827e007c744e94b0cdcddfabd927f14384b405e0
florpi <florpisoteadadejardin@gmail.com>
2020-05-17 11:07:42 +0100

    trying to activate failed

--- june/groups/commute/commutehub_distributor.py
+++ june/groups/commute/commutehub_distributor.py
@@ -112,7 +112,9 @@ class CommuteHubDistributor:
 
                 _, hub_index = commutehub_tree.query(live_lat_lon,1) 
                 
-                commutehub_in_city[hub_index].add(work_person)
+                commutehub_in_city[hub_index].add(work_person,
+                        activity_type=work_person.ActivityType.commute,
+                        subgroup_type=commutehub_in_city[hub_index])
 
             for work_person in to_commute_in:
                 commutecity.commute_internal.append(work_person)
