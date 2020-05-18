4516c08e2ef987dcee2cf13a2b9369f1b0701f8f
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-05-18 11:59:12 +0200

    save/write in chunks working

--- june/demography/demography.py
+++ june/demography/demography.py
@@ -149,74 +149,120 @@ class Population:
     def to_hdf5(self, file_path: str):
         n_people = len(self.people)
         dt = h5py.vlen_dtype(np.dtype("int32"))
-        ids = []
-        ages = []
-        sexes = []
-        ethns = []
-        areas = []
-        group_ids = []
-        group_specs = []
-        subgroup_types = []
-        housemates = []
-        for person in self.people:
-            ids.append(person.id)
-            ages.append(person.age)
-            sexes.append(person.sex.encode("ascii", "ignore"))
-            ethns.append(person.ethnicity.encode("ascii", "ignore"))
-            if person.area is not None:
-                areas.append(person.area.id)
-            else:
-                areas.append(nan_integer)
-            gids = []
-            stypes = []
-            specs = []
-            for subgroup in person.subgroups:
-                if subgroup is None:
-                    gids.append(nan_integer)
-                    stypes.append(nan_integer)
-                    specs.append(" ".encode("ascii", "ignore"))
-                else:
-                    gids.append(subgroup.group.id)
-                    stypes.append(subgroup.subgroup_type)
-                    specs.append(subgroup.group.spec.encode("ascii", "ignore"))
-            group_specs.append(np.array(specs, dtype="S10"))
-            group_ids.append(np.array(gids, dtype=np.int))
-            subgroup_types.append(np.array(stypes, dtype=np.int))
-            hmates = [mate.id for mate in person.housemates]
-            if len(hmates) == 0:
-                housemates.append(np.array([nan_integer], dtype=np.int))
-            else:
-                housemates.append(np.array(hmates, dtype=np.int))
-
-        ids = np.array(ids, dtype=np.int)
-        ages = np.array(ages, dtype=np.int)
-        sexes = np.array(sexes, dtype="S10")
-        ethns = np.array(ethns, dtype="S10")
-        areas = np.array(areas, dtype=np.int)
-        group_ids = np.array(group_ids, dtype=np.int)
-        subgroup_types = np.array(subgroup_types, dtype=np.int)
-        group_specs = np.array(group_specs, dtype="S10")
-
+        chunk_size = 1000000  # 1M people at a time to avoid memory overload
+        n_chunks = int(np.ceil(n_people / chunk_size))
         with h5py.File(file_path, "a", libver="latest") as f:
             people_dset = f.create_group("population")
-            people_dset.attrs["n_people"] = n_people
-            people_dset.create_dataset("id", data=ids)
-            people_dset.create_dataset("age", data=ages)
-            people_dset.create_dataset("sex", data=sexes)
-            people_dset.create_dataset("ethnicity", data=ethns)
-            people_dset.create_dataset("group_ids", data=group_ids)
-            people_dset.create_dataset("group_specs", data=group_specs)
-            people_dset.create_dataset("subgroup_types", data=subgroup_types)
-            people_dset.create_dataset("area", data=areas)
-            people_dset.create_dataset("housemates", data=housemates, dtype=dt)
+            for chunk in range(n_chunks):
+                idx1 = chunk * chunk_size
+                idx2 = min((chunk + 1) * chunk_size, n_people)
+                print(idx1, idx2)
+                ids = []
+                ages = []
+                sexes = []
+                ethns = []
+                areas = []
+                group_ids = []
+                group_specs = []
+                subgroup_types = []
+                housemates = []
+                for person in self.people[idx1:idx2]:
+                    ids.append(person.id)
+                    ages.append(person.age)
+                    sexes.append(person.sex.encode("ascii", "ignore"))
+                    ethns.append(person.ethnicity.encode("ascii", "ignore"))
+                    if person.area is not None:
+                        areas.append(person.area.id)
+                    else:
+                        areas.append(nan_integer)
+                    gids = []
+                    stypes = []
+                    specs = []
+                    for subgroup in person.subgroups:
+                        if subgroup is None:
+                            gids.append(nan_integer)
+                            stypes.append(nan_integer)
+                            specs.append(" ".encode("ascii", "ignore"))
+                        else:
+                            gids.append(subgroup.group.id)
+                            stypes.append(subgroup.subgroup_type)
+                            specs.append(subgroup.group.spec.encode("ascii", "ignore"))
+                    group_specs.append(np.array(specs, dtype="S10"))
+                    group_ids.append(np.array(gids, dtype=np.int))
+                    subgroup_types.append(np.array(stypes, dtype=np.int))
+                    hmates = [mate.id for mate in person.housemates]
+                    if len(hmates) == 0:
+                        housemates.append(np.array([nan_integer], dtype=np.int))
+                    else:
+                        housemates.append(np.array(hmates, dtype=np.int))
+
+                ids = np.array(ids, dtype=np.int)
+                ages = np.array(ages, dtype=np.int)
+                sexes = np.array(sexes, dtype="S10")
+                ethns = np.array(ethns, dtype="S10")
+                areas = np.array(areas, dtype=np.int)
+                group_ids = np.array(group_ids, dtype=np.int)
+                subgroup_types = np.array(subgroup_types, dtype=np.int)
+                group_specs = np.array(group_specs, dtype="S10")
+
+                if chunk == 0:
+                    people_dset.attrs["n_people"] = n_people
+                    people_dset.create_dataset("id", data=ids, maxshape=(n_people,))
+                    people_dset.create_dataset("age", data=ages, maxshape=(n_people,))
+                    people_dset.create_dataset("sex", data=sexes, maxshape=(n_people,))
+                    people_dset.create_dataset(
+                        "ethnicity", data=ethns, maxshape=(n_people,)
+                    )
+                    people_dset.create_dataset(
+                        "group_ids",
+                        data=group_ids,
+                        maxshape=(n_people, group_ids.shape[1]),
+                    )
+                    people_dset.create_dataset(
+                        "group_specs",
+                        data=group_specs,
+                        maxshape=(n_people, group_specs.shape[1]),
+                    )
+                    people_dset.create_dataset(
+                        "subgroup_types",
+                        data=subgroup_types,
+                        maxshape=(n_people, subgroup_types.shape[1]),
+                    )
+                    people_dset.create_dataset("area", data=areas, maxshape=(n_people,))
+                    people_dset.create_dataset(
+                        "housemates",
+                        data=housemates,
+                        dtype=dt,
+                        maxshape=(n_people,),
+                    )
+                else:
+                    newshape = (people_dset["id"].shape[0] + ids.shape[0], )
+                    people_dset["id"].resize(newshape)
+                    people_dset["id"][idx1:idx2] = ids
+                    people_dset["age"].resize(newshape)
+                    people_dset["age"][idx1:idx2] = ages
+                    people_dset["sex"].resize(newshape)
+                    people_dset["sex"][idx1:idx2] = sexes
+                    people_dset["ethnicity"].resize(newshape)
+                    people_dset["ethnicity"][idx1:idx2] = ethns
+                    people_dset["area"].resize(newshape)
+                    people_dset["area"][idx1:idx2] = areas
+                    people_dset["group_ids"].resize(newshape[0], axis=0)
+                    people_dset["group_ids"][idx1:idx2] = group_ids
+                    people_dset["group_specs"].resize(newshape[0], axis=0)
+                    people_dset["group_specs"][idx1:idx2] = group_specs
+                    people_dset["subgroup_types"].resize(newshape[0], axis=0)
+                    people_dset["subgroup_types"][idx1:idx2] = subgroup_types
+                    people_dset["housemates"].resize(newshape[0], axis=0)
+                    people_dset["housemates"][idx1:idx2] = housemates
 
     @classmethod
     def from_hdf5(cls, file_path: str):
         with h5py.File(file_path, "r") as f:
             people = list()
             population = f["population"]
-            # read in chunks of 100,000 people
-            chunk_size = 50000
+            # read in chunks of 1M people
+            chunk_size = 1000000
             n_people = population.attrs["n_people"]
             n_chunks = int(np.ceil(n_people / chunk_size))
             for chunk in range(n_chunks):
