983c3124e8e1c3d0de47788b0daad31b397ac88b
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-05-19 14:55:32 +0100

    adding policy description

--- june/policy.py
+++ june/policy.py
@@ -2,6 +2,25 @@
 
 
 class Policy:
+    '''
+    Implement certain policy decisions into the simulartor
+    Policies should be implemented in a modular fashion applying one after the other
+
+    Assumptions:
+    - If alpha and beta values are changed this is done by social distancing
+      This means that this function must be called first
+    - Any functions making edits to the active subgroups of a person assume that there is an attribute person.policy_subgroups
+      This attribute is all the groups which will be accessed under and policy implementations
+      To undo a policy this attribute has to be reset and the fraction implemented again
+      This can be done by running the open_all() function
+
+      e.g. full school closure:
+           school_closure(person, years = None, full_closure = True)
+           now reopen only certain schools:
+           open_all()
+           school_closure(person, years = [...], full_closure = False)
+
+    '''
 
     # want to make this modular so that you could apply policies in combinations - Keras stype
 
@@ -25,6 +44,10 @@ class Policy:
         '''
         Set people back to moving as before
         This is done by resetting all their subgroups to what they had before
+        
+        -----------
+        Parameters:
+        person: a member of the Persons class
         '''
 
         person.policy_subgroup = person.subgroups
@@ -39,6 +62,13 @@ class Policy:
         pass
 
     def school_closure(self, person, years, full_closure):
+        '''
+        Implement school closure by year group
+        
+        ----------
+        Parametes:
+
+        '''
 
         # uses adherence
         
@@ -51,8 +81,6 @@ class Policy:
         # This will currently not work and is more like pseudocode
         if person.age is in years:
             person.policy_subgroup.pop('school')
-        
-        pass
     
     def company_closure(self, sectors, full_closure):
 
@@ -68,7 +96,7 @@ class Policy:
         '''
         Implement social distancing policy
         
-        ----------------
+        -----------
         Parameters:
         alphas: e.g. (float) from DefaultInteraction, e.g. DefaultInteraction.from_file(selector=selector).alpha
         betas: e.g. (dict) from DefaultInteraction, e.g. DefaultInteraction.from_file(selector=selector).beta
