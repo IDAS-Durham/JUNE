a82a57e74c3c555eedb400208aefd70c4fd0aefe
Richard <richardhayes777@gmail.com>
2020-05-20 16:08:11 +0100

    stages as stages

--- june/infection/symptoms_trajectory.py
+++ june/infection/symptoms_trajectory.py
@@ -1,27 +1,16 @@
 import numpy as np
 
-from june.infection.trajectory_maker import TrajectoryMaker
 from june.infection.symptoms import Symptoms, SymptomTags
-
-
-class MissingTrajectoryMakerError(BaseException):
-    pass
-
-
-class MissingTrajectoryError(BaseException):
-    pass
-
-
-class MissingPatientError(BaseException):
-    pass
+from june.infection.trajectory_maker import TrajectoryMaker
 
 
 class SymptomsTrajectory(Symptoms):
     def __init__(self, health_index=None):
         super().__init__(health_index=health_index)
-        self.make_trajectory()
+        self.trajectory = None
+        self.update_trajectory()
 
-    def make_trajectory(self):
+    def update_trajectory(self):
         trajectory_maker = TrajectoryMaker()
         maxtag = self.max_tag()
         self.trajectory = trajectory_maker[maxtag]
@@ -32,10 +21,6 @@ class SymptomsTrajectory(Symptoms):
 
     def update_severity_from_delta_time(self, delta_time):
         self.tag = SymptomTags.healthy
-        if self.trajectory == []:
-            raise MissingTrajectoryMakerError(
-                f"SymptomsTrajectory has no trajectory"
-            )
         for stage in self.trajectory:
             if delta_time > stage[0]:
                 self.tag = stage[1]
