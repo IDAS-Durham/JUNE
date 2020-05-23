e430706b4cffdf7f5e16086dd96c4cda652e12e2
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-05-23 13:44:57 +0100

    updating travelunit to new Group structure

--- june/groups/travel/travelunit.py
+++ june/groups/travel/travelunit.py
@@ -1,16 +1,24 @@
 import numpy as np
+from june.groups.group import Group, Supergroup
 
-class TravelUnit:
+class TravelUnit(Group):
 
-    def __init__(self, travelunit_id, city):
-
-        self.travelunit_id = travelunit_id
+    def __init__(self, city):
+        """
+        city: (string) City unit belongs to
+        people: (Group structure) passengers travelingin the travel unit
+        max_passengers: (int) capacity of the travel unit
+        """
+        super().__init__()
+        
         self.city = city
         self.passengers = []
         self.max_passengers = 50 # assume all units are of equal size but this could be made more granular later
 
-class TravelUnits:
+class TravelUnits(SuperGroup):
 
     def __init__(self):
 
+        super().__init__()
+        
         self.members = []
