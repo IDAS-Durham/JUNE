2873a07d0d2e78274f2c3abc0dcabdda46439d86
Richard <richardhayes777@gmail.com>
2020-05-27 14:36:09 +0100

    all trajectories config in one place, exponential transition to hospital, bodged tests

--- test_june/unit/conftest.py
+++ test_june/unit/conftest.py
@@ -45,9 +45,7 @@ def create_symptoms_healthy():
 
 @pytest.fixture(name="trajectories", scope="session")
 def create_trajectories():
-    return tmaker.TrajectoryMakers.from_file(
-        paths.configs_path / "config.test.yaml"
-    )
+    return tmaker.TrajectoryMakers.from_file()
 
 
 @pytest.fixture(name="symptoms_trajectories", scope="session")
