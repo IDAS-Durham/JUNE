1f7a48390bd0e83fe8f229210039785c9dfe87da
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-06-02 01:07:57 +0200

    double list compreh is faster :rocket:

--- scripts/create_world.py
+++ scripts/create_world.py
@@ -106,7 +106,7 @@ msoaslist = [
 t1 = time.time()
 
 # we have two options, we can take the list of areas above and select a few:
-geography = Geography.from_file({"msoa" : msoaslist})
+geography = Geography.from_file({"msoa" : msoaslist[:10]})
 # or select an entire region:
 #geography = Geography.from_file({"region" : ["North East"]})
 
