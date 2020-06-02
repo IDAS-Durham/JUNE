ae02605fcc915d9c0499b1e44850444e4671c1ce
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-06-02 23:59:30 +0200

    Update cemetery.py

--- june/groups/cemetery.py
+++ june/groups/cemetery.py
@@ -6,17 +6,6 @@ from enum import IntEnum
 class Cemetery:
     def __init__(self):
         self.people = []
-    #class GroupType(IntEnum):
-    #    default = 0
-
-    #def must_timestep(self):
-    #    return False
-
-    #def update_status_lists(self, time=1, delta_time=1):
-    #    pass
-
-    #def set_active_members(self):
-    #    pass
 
     def add(self, person):
         self.people.append(person)
