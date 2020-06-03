89b1c2076396bde76d0a438df92e2d0b5013fdc7
florpi <florpisoteadadejardin@gmail.com>
2020-06-03 12:15:40 +0100

    changes from rewiew

--- get_data.sh
+++ get_data.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-wget --user=access --password=d0wn10@d$ http://cosma-web1.cosma.dur.ac.uk/downloads/dc-quer1/data.zip
-unzip data.zip
+
+#https://drive.google.com/open?id=1i-HkNRbBwZojT5FuXIOJArEV2Q4lRCWa
+fileId=1i-HkNRbBwZojT5FuXIOJArEV2Q4lRCWa
+fileName=data.zip
+curl -sc cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
+code="$(awk '/_warning_/ {print $NF}' cookie)"  
+curl -Lb cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
+
+unzip ${fileName} || tar -zxvf ${fileName}
+rm cookie
 rm data.zip
