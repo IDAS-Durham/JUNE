9f4d902dd47a494a8d065f2635e2a97d8a7fd1fd
florpi <florpisoteadadejardin@gmail.com>
2020-06-04 16:33:27 +0100

    old data comes back

--- get_data.sh
+++ get_data.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
-
-#https://drive.google.com/open?id=1i-HkNRbBwZojT5FuXIOJArEV2Q4lRCWa
-#https://drive.google.com/file/d/1OPpupTh1uCnWi3OvxF_GPdiw5lXfEFMp/view?usp=sharing
-fileId=1OPpupTh1uCnWi3OvxF_GPdiw5lXfEFMp
-fileName=data.zip
-curl -sc cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
-code="$(awk '/_warning_/ {print $NF}' cookie)"  
-curl -Lb cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
-
-unzip ${fileName} || tar -zxvf ${fileName}
-rm cookie
-rm data.zip
+wget --user=access --password=d0wn10@d$ http://cosma-web1.cosma.dur.ac.uk/downloads/dc-quer1/data.zip
+unzip data.zip
