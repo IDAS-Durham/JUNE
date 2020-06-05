75641dbf64afcf20718c300a18870338952d25e5
florpi <florpisoteadadejardin@gmail.com>
2020-06-05 18:20:22 +0100

    added pre processing files for data frames

---
+++ june/data_formatting/time_series/process_confirmed_cases.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from june import paths
+from datetime import datetime
+import os
+
+raw_path = paths.data_path / "time_series/"
+processed_path = paths.data_path / "processed/time_series/"
+
+confirmed_cases_df = pd.read_csv(raw_path / "coronavirus-cases_latest.csv",
+        index_col=0)
+mask = confirmed_cases_df['Area type'] == 'Region'
+confirmed_cases_df = confirmed_cases_df[mask]
+confirmed_cases_df = confirmed_cases_df[['Specimen date','Daily lab-confirmed cases']]
+confirmed_cases_df.reset_index(inplace=True)
+confirmed_cases_df = confirmed_cases_df.set_index('Specimen date')
+confirmed_cases_df = confirmed_cases_df.pivot(columns='Area name', values='Daily lab-confirmed cases')
+confirmed_cases_df.to_csv(processed_path / "n_confirmed_cases.csv")
+
+
