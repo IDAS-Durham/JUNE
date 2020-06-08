3e3edf025d9373b92da9812d4695d583f0833a1b
florpi <florpisoteadadejardin@gmail.com>
2020-06-08 22:22:10 +0100

    making logger lighter

--- june/logger/read_logger.py
+++ june/logger/read_logger.py
@@ -12,7 +12,7 @@ from june import paths
 
 class ReadLogger:
     def __init__(
-        self, output_path: str = "results", output_file_name: str = "logger.hdf5"
+            self, output_path: str = "results", output_file_name: str = "logger.hdf5", light_logger: bool =True
     ):
         """
         Read hdf5 file saved by the logger, and produce useful data frames
@@ -26,24 +26,26 @@ class ReadLogger:
         """
         self.output_path = output_path
         self.file_path = Path(self.output_path) / output_file_name
-        self.load_population_data()
+        self.light_logger = light_logger
+        self.load_population_data(self.light_logger)
         self.load_infected_data()
         self.load_infection_location()
         self.start_date = min(self.infections_df.index)
         self.end_date = max(self.infections_df.index)
         self.load_real_time_series()
 
-    def load_population_data(self):
+    def load_population_data(self, light_logger):
         """
         Load data related to population (age, sex, ...)
         """
         with h5py.File(self.file_path, "r", libver="latest", swmr=True) as f:
             population = f["population"]
             self.n_people = population.attrs["n_people"]
-            self.ids = population["id"][:]
-            self.ages = population["age"][:]
-            self.sexes = population["sex"][:]
-            self.super_areas = population["super_area"][:].astype("U13")
+            if not light_logger:
+                self.ids = population["id"][:]
+                self.ages = population["age"][:]
+                self.sexes = population["sex"][:]
+                self.super_areas = population["super_area"][:].astype("U13")
 
     def load_infected_data(self,):
         """
