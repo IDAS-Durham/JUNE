a1a6cef94c8f67c439960110019c63b8a55aeae1
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-06-08 15:42:36 +0200

    modified get data

--- get_data.sh
+++ get_data.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 wget --user=access --password=d0wn10@d$ http://cosma-web1.cosma.dur.ac.uk/downloads/dc-quer1/data.zip
 unzip data.zip
+# camp data
+wget --user=access --password=d0wn10@d$ http://cosma-web1.cosma.dur.ac.uk/downloads/dc-quer1/camp_data.zip
+unzip camp_data.zip
