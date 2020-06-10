abf83cef7f03b36c2e3cfbfa5899dfcb1eaf2d86
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-06-10 14:21:59 +0100

    adding female comunal spaces to leisure

--- june/groups/leisure/__init__.py
+++ june/groups/leisure/__init__.py
@@ -6,6 +6,7 @@ from .grocery import Groceries, Grocery, GroceryDistributor
 from .pump_latrine import PumpLatrine, PumpLatrines, PumpLatrineDistributor
 from .distribution_center import DistributionCenter, DistributionCenters, DistributionCenterDistributor
 from .communal import Communal, Communals, CommunalDistributor
+from .female_communal import FemaleCommunal, FemaleCommunals, FemaleCommunalDistributor
 from .care_home_visits import CareHomeVisitsDistributor
 from .household_visits import HouseholdVisitsDistributor 
 from .leisure import Leisure, generate_leisure_for_world
