413dbf28b7559378001ec966ce9e094883438cfc
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-06-10 10:22:33 +0100

    updating leisure and init

--- june/groups/leisure/__init__.py
+++ june/groups/leisure/__init__.py
@@ -3,6 +3,7 @@ from .social_venue_distributor import SocialVenueDistributor
 from .pub import Pub, Pubs, PubDistributor
 from .cinema import Cinema, Cinemas, CinemaDistributor
 from .grocery import Groceries, Grocery, GroceryDistributor
+from .pump_latrine import PumpLatrine, PumpLatrines, PumpLatrineDistributor
 from .care_home_visits import CareHomeVisitsDistributor
 from .household_visits import HouseholdVisitsDistributor 
 from .leisure import Leisure, generate_leisure_for_world
