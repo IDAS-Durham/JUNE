1adf2eae65b5dae950f6cb1e77c930f485154161
Arnau <arnau.quera-bofarull@durham.ac.uk>
2020-06-11 15:09:05 +0200

    created camp module

---
+++ camps/groups/__init__.py
@@ -0,0 +1,4 @@
+from .distribution_center import DistributionCenter, DistributionCenters, DistributionCenterDistributor
+from .female_communal import FemaleCommunal, FemaleCommunals, FemaleCommunalDistributor
+from .pump_latrine import PumpLatrine, PumpLatrines, PumpLatrineDistributor
+from .communal import Communal, Communals, CommunalDistributor
