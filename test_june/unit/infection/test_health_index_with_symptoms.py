7b3493f0bad4900f9cc6a3f5dfd8140133beb518
florpi <florpisoteadadejardin@gmail.com>
2020-06-15 23:26:09 +0100

    added trusts

--- test_june/unit/infection/test_health_index_with_symptoms.py
+++ test_june/unit/infection/test_health_index_with_symptoms.py
@@ -1,27 +1,37 @@
 import numpy as np
+from collections import defaultdict
 from june.demography.person import Person
 from june.infection.symptoms import SymptomsStep
 from june.infection.health_index import HealthIndexGenerator
 from june.infection.symptoms import SymptomTag
 
+
 def test__right_frequency_in_health_index():
-    N_samples = 1000
-    health_index = HealthIndexGenerator.from_file()(Person(age=27, sex='m'))
+    N_samples = 10000
+    health_index = HealthIndexGenerator.from_file()(Person(age=60, sex="m"))
 
-    frequencies = np.zeros(8)
-    for i in range(N_samples):
-        symptoms = SymptomsStep(health_index = health_index, time_offset=0.)
+    frequencies = defaultdict(int)
+    for _ in range(N_samples):
+        symptoms = SymptomsStep(health_index=health_index, time_offset=0.0)
         symptoms.update_severity_from_delta_time(0.01)
-        # check their symptoms matches the frequency in health index 
-        if symptoms.tag != 'healthy':
-            frequencies[SymptomTag(symptoms.tag)] += 1
+        # check their symptoms matches the frequency in health index
+        frequencies[symptoms.tag.name] += 1
 
-    np.testing.assert_allclose(frequencies[0]/N_samples, health_index[0], atol=0.05)
-    np.testing.assert_allclose(frequencies[1]/N_samples, health_index[1]-health_index[0],
-                               atol=0.05)
-    np.testing.assert_allclose(frequencies[2]/N_samples, health_index[2]-health_index[1],
-                               atol=0.05)
-    np.testing.assert_allclose(frequencies[3]/N_samples, health_index[3]-health_index[2],
-                               atol=0.05)
-    np.testing.assert_allclose(frequencies[4]/N_samples, health_index[4]-health_index[3],
-                               atol=0.05)
+    np.testing.assert_allclose(
+        frequencies["asymptomatic"] / N_samples, health_index[0], atol=0.01
+    )
+    np.testing.assert_allclose(
+        frequencies["influenza"] / N_samples,
+        health_index[1] - health_index[0],
+        atol=0.01,
+    )
+    np.testing.assert_allclose(
+        frequencies["pneumonia"] / N_samples, health_index[2] - health_index[1], atol=0.01
+    )
+    np.testing.assert_allclose(
+        frequencies["hospitalised"] / N_samples, health_index[3] - health_index[2], atol=0.01
+    )
+    np.testing.assert_allclose(
+        frequencies["intensive_care"] / N_samples, health_index[4] - health_index[3], atol=0.01
+    )
+    np.testing.assert_allclose(frequencies["dead"] / N_samples, 1 - health_index[4], atol=0.01)
