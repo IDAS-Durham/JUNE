4df5c510617f5bbc21e935dade04c4ef5d546b48
florpi <florpisoteadadejardin@gmail.com>
2020-06-17 14:51:26 +0100

    do not encode trusts

--- june/demography/__init__.py
+++ june/demography/__init__.py
@@ -1,4 +1,4 @@
-from .person import Person
+from .person import Person, Activities
 from .person_distributor import PersonDistributor
 from .demography import Demography, Population, AgeSexGenerator
 from . import geography
