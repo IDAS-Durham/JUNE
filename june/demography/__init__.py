fc0c953daa60c634c9e261b544aa71064c387021
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-06-18 15:22:13 +0200

    removed legacy code

--- june/demography/__init__.py
+++ june/demography/__init__.py
@@ -1,5 +1,4 @@
 from .person import Person, Activities
-from .person_distributor import PersonDistributor
 from .demography import Demography, Population, AgeSexGenerator
 from . import geography
 from .geography import Geography
