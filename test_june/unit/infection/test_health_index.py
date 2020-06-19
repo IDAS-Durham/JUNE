270347f593637c52c650215a51f4fb14fc72359e
Miguel Icaza <miguel.a.de-icaza-lizaola@durham.ac.uk>
2020-06-19 08:31:10 +0100

    new changes

--- test_june/unit/infection/test_health_index.py
+++ test_june/unit/infection/test_health_index.py
@@ -8,9 +8,12 @@ def test__smaller_than_one():
     for i in range(len(index_list.Prob_lists[0])):
         index_m=index_list(Person.from_attributes(age=i,sex='m'))
         index_w=index_list(Person.from_attributes(age=i,sex='w'))
-        bool_m=np.sum(index_m<1)
-        bool_w=np.sum(index_w<1)
-        increasing_count+=1 if bool_m+bool_w==10 else increasing_count==increasing_count
+        bool_m=np.sum(np.round(index_m,7)<=1)
+        bool_w=np.sum(np.round(index_w,7)<=1)
+        if bool_m+bool_w==14:
+           increasing_count+=1
+        else:
+           increasing_count==increasing_count
     assert increasing_count ==121
 
 def test__growing_index():
@@ -23,6 +26,9 @@ def test__growing_index():
                                          index_list.Prob_lists[0][0][1:])) 
         bool_w=all(i < j for i, j in zip(index_list.Prob_lists[0][0],
                                          index_list.Prob_lists[0][0][1:]))
-        increasing_count+=1 if bool_m+bool_w==2 else increasing_count==increasing_count
+        if bool_m+bool_w==2:
+             increasing_count+=1  
+        else:
+               increasing_count==increasing_count
     assert increasing_count ==121
 
