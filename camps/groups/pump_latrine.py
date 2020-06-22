06ff9816e3a237b0002084e623bcbd29b68798f1
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-06-22 10:45:35 +0100

    file renaming and group update

--- camps/groups/pump_latrine.py
+++ camps/groups/pump_latrine.py
@@ -8,7 +8,7 @@ from june.groups.leisure.social_venue_distributor import SocialVenueDistributor
 from camps.paths import camp_configs_path
 from june.demography.geography import SuperArea, Area
 
-default_config_filename = camp_configs_path / "defaults/groups/pumplatrines.yaml"
+default_config_filename = camp_configs_path / "defaults/groups/pump_latrine.yaml"
 
 class PumpLatrine(SocialVenue):
     def __init__(self, max_size=10):
