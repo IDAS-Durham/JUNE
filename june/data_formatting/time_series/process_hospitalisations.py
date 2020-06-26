2c92c5b0a6495c5382811bb7fba88157341f4685
florpi <florpisoteadadejardin@gmail.com>
2020-06-27 00:05:21 +0100

    added test for observed cases

---
+++ june/data_formatting/time_series/process_hospitalisations.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from june import paths
+from datetime import datetime
+import os
+
+raw_path = paths.data_path / "time_series/"
+processed_path = paths.data_path / "processed/time_series/"
+
+hosp_df = pd.read_csv(raw_path / "COVID_output_pivot_v3.csv", sep=",", skiprows=1)
+
+hosp_df.set_index("ReportingPeriod", inplace=True)
+hosp_df.index = pd.to_datetime(hosp_df.index)
+hosp_df["covid_admissions"] = hosp_df[
+    ["SIT008_Total", "SIT009_Total", "SIT009_Suspected"]
+].sum(axis=1)
+hosp_df = hosp_df.groupby([hosp_df.index, "Region_Name"]).sum()
+hosp_df = hosp_df[["covid_admissions"]].reset_index()
+hosp_df = hosp_df.pivot(
+    index="ReportingPeriod", columns="Region_Name", values="covid_admissions"
+)
+hosp_df.to_csv(processed_path / "hospital_admissions_region.csv")
