d636ef4e1e71fa3da6af00f5c9b62351a20418ed
florpi <florpisoteadadejardin@gmail.com>
2020-07-10 14:25:58 +0100

    added docstring

--- june/infection/infection.py
+++ june/infection/infection.py
@@ -12,90 +12,161 @@ from june.infection.transmission import TransmissionConstant
 from june.infection.transmission_xnexp import TransmissionXNExp
 
 default_config_filename = (
-        paths.configs_path
-        / "defaults/infection/InfectionTrajectoriesXNExp.yaml"
+    paths.configs_path / "defaults/infection/InfectionTrajectoriesXNExp.yaml"
 )
 
 
 class SymptomsType(IntEnum):
-    constant = 0,
-    gaussian = 1,
-    step = 2,
+    constant = (0,)
+    gaussian = (1,)
+    step = (2,)
     trajectories = 3
 
 
 class InfectionSelector:
-    def __init__(self, transmission_type: str, asymptomatic_ratio:float=0.3):
+    def __init__(self, transmission_type: str, asymptomatic_ratio: float = 0.3):
+        """
+        Selects the type of infection a person is given
+
+        Parameters
+        ----------
+        transmission_type:
+            either constant or xnexp, controls the person's infectiousness profile over time
+        asymptomatic_ratio:
+            proportion of infected people that are asymptomatic
+        """
         self.transmission_type = transmission_type
         self.trajectory_maker = TrajectoryMakers.from_file()
-        self.health_index_generator = HealthIndexGenerator.from_file(asymptomatic_ratio=asymptomatic_ratio)
+        self.health_index_generator = HealthIndexGenerator.from_file(
+            asymptomatic_ratio=asymptomatic_ratio
+        )
 
     @classmethod
     def from_file(
-            cls,
-            asymptomatic_ratio:float =0.43,
-            config_filename: str = default_config_filename,
+        cls,
+        asymptomatic_ratio: float = 0.3,
+        config_filename: str = default_config_filename,
     ) -> "InfectionSelector":
+        """
+        Generate infection selector from default config file
+        
+        Parameters
+        ----------
+        asymptomatic_ratio:
+            proportion of infected people that are asymptomatic
+        config_filename: 
+            path to config file 
+        """
         with open(config_filename) as f:
             config = yaml.load(f, Loader=yaml.FullLoader)
-        return InfectionSelector(config['transmission_type'], asymptomatic_ratio)
+        return InfectionSelector(config["transmission_type"], asymptomatic_ratio)
 
-    def infect_person_at_time(self, person, time):
+    def infect_person_at_time(self, person: "Person", time: float):
+        """
+        Infects a person at a given time
+
+        Parameters
+        ----------
+        person:
+            person that will be infected
+        time:
+            time at which infection happens
+        """
         infection = self.make_infection(person, time)
         person.health_information = HealthInformation()
         person.health_information.set_infection(infection=infection)
 
-    def make_infection(self, person, time):
+    def make_infection(self, person: "Person", time: float):
+        """
+        Generates the symptoms and infectiousness of the person being infected
+
+        Parameters
+        ----------
+        person:
+            person that will be infected
+        time:
+            time at which infection happens
+        """
+
         symptoms = self.select_symptoms(person)
         incubation_period = symptoms.time_exposed()
         transmission = self.select_transmission(person, incubation_period)
-        return Infection(transmission=transmission,
-                         symptoms=symptoms,
-                         start_time=time)
+        return Infection(transmission=transmission, symptoms=symptoms, start_time=time)
 
-    def select_transmission(self, person, incubation_period):
-        if self.transmission_type == 'xnexp': 
-            start_transmission = incubation_period - np.random.normal(2., 0.5)
-            peak_position = incubation_period - np.random.normal(0.7,0.1)  - start_transmission
-            alpha = 1.
-            N = peak_position/alpha
-            print('peak position = ', peak_position)
+    def select_transmission(self, person: "Person", incubation_period: float)->"Transmission":
+        """
+        Selects the transmission type specified by the user in the init, 
+        and links its parameters to the symptom onset for the person (incubation
+        period)
+
+        Parameters
+        ----------
+        person:
+            person that will be infected
+        incubation_period:
+            time of symptoms onset for person
+        """
+        if self.transmission_type == "xnexp":
+            start_transmission = incubation_period - np.random.normal(2.0, 0.5)
+            peak_position = (
+                incubation_period - np.random.normal(0.7, 0.4) - start_transmission
+            )
+            alpha = 1.5
+            N = peak_position / alpha
             return TransmissionXNExp.from_file(
-                                     start_transmission=start_transmission,
-                                     N=N,
-                                     alpha=alpha,
-                                     )
-        elif self.transmission_type ==  'constant':
+                start_transmission=start_transmission, N=N, alpha=alpha,
+            )
+        elif self.transmission_type == "constant":
             return TransmissionConstant.from_file()
         else:
-            raise NotImplementedError('This transmission type has not been implemented')
+            raise NotImplementedError("This transmission type has not been implemented")
 
-    def select_symptoms(self, person):
+    def select_symptoms(self, person: "Person")->"Symptoms":
+        """
+        Select the symptoms that a given person has, and how they will evolve
+        in the future
+
+        Parameters
+        ----------
+        person:
+            person that will be infected
+        """
         health_index = self.health_index_generator(person)
         return Symptoms(health_index=health_index)
 
 
 class Infection:
     """
-    The description of the infection, with two time dependent characteristics,
-    which may vary by individual:
-    - transmission probability, Ptransmission.
-    - symptom severity, Severity
-    Either of them will be a numer between 0 (low) and 1 (high, strong sypmotoms),
-    and for both we will have some thresholds.
-    Another important part for the infection is their begin, starttime, which must
-    be given in the constructor.  Transmission probability and symptom severity
-    can be added/modified a posteriori.
+    The infection class combines the transmission (infectiousness profile) of the infected
+    person, and their symptoms trajectory.
     """
 
-    def __init__(self, transmission, symptoms, start_time=-1):
+    def __init__(self, transmission: "Transmission", symptoms: "Symptoms", start_time: float=-1):
+        """
+        Parameters
+        ----------
+        transmission:
+            instance of the class that controls the infectiousness profile
+        symptoms:
+            instance of the class that controls the symptoms' evolution
+        start_time:
+            time at which the person is infected
+        """
         self.start_time = start_time
         self.last_time_updated = start_time
         self.transmission = transmission
         self.symptoms = symptoms
         self.infection_probability = 0.0
 
-    def update_at_time(self, time):
+    def update_at_time(self, time: float):
+        """
+        Updates symptoms and infectousness 
+
+        Parameters
+        ----------
+        time:
+            time elapsed from time of infection
+        """
         if self.last_time_updated <= time:
             delta_time = time - self.start_time
             self.last_time_updated = time
@@ -106,5 +177,3 @@ class Infection:
     @property
     def still_infected(self):
         return True
-    # self.symptoms.tag!=SymptomTags:recovered and
-    # self.symptoms.tag!=SymptomTags:dead
