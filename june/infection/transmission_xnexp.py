223f60274975e35592ac7521e48f285fea1631c7
florpi <florpisoteadadejardin@gmail.com>
2020-07-10 11:48:16 +0100

    adapting infectiousness

--- june/infection/transmission_xnexp.py
+++ june/infection/transmission_xnexp.py
@@ -1,17 +1,20 @@
 from june.infection.transmission import Transmission
+from june.infection.trajectory_maker import CompletionTime
+from june import paths
+import yaml
 import numpy as np
 import numba as nb
 
-
+default_config_path = paths.configs_path / "defaults/transmission/XNExp.yaml"
 @nb.jit(nopython=True)
 def f(t, n, alpha):
     return t ** n * np.exp(-t / alpha)
 
 
 @nb.jit(nopython=True)
-def update_probability(time, incubation_time, norm, norm_time, alpha, n):
-    if time > incubation_time:
-        delta_tau = (time - incubation_time) / norm_time
+def update_probability(time, start_transmission, norm, norm_time, alpha, n):
+    if time > start_transmission:
+        delta_tau = (time - start_transmission) / norm_time
         return norm * delta_tau ** n * np.exp(-delta_tau / alpha)
     else:
         return 0.0
@@ -19,10 +22,10 @@ def update_probability(time, incubation_time, norm, norm_time, alpha, n):
 
 class TransmissionXNExp(Transmission):
     def __init__(
-        self, max_probability=1.0, incubation_time=2.6, norm_time=1.0, N=1.0, alpha=5.0
+        self, max_probability=1.0, start_transmission=2.6, norm_time=1.0, N=1.0, alpha=5.0
     ):
         self.max_probability = max_probability
-        self.incubation_time = incubation_time
+        self.start_transmission = start_transmission
         self.norm_time = norm_time
         self.N = N
         self.alpha = alpha
@@ -32,10 +35,23 @@ class TransmissionXNExp(Transmission):
         )  # self.f(max_time/self.norm_time)  #/self.norm_time)
         self.probability = 0.0
 
+    @classmethod
+    def from_file(cls, start_transmission, N, alpha, config_path: str  = default_config_path) -> "TransmissionXNExp":
+        with open(config_path) as f:
+            config = yaml.safe_load(f)
+        max_probability = CompletionTime.from_dict(config['max_probability'])()
+        norm_time = CompletionTime.from_dict(config['norm_time'])()
+        return TransmissionXNExp(max_probability=max_probability,
+                start_transmission=start_transmission,
+                norm_time=norm_time,
+                N=N,
+                alpha=alpha)
+
+
     def update_probability_from_delta_time(self, delta_time):
         self.probability = update_probability(
             delta_time,
-            self.incubation_time,
+            self.start_transmission,
             self.norm,
             self.norm_time,
             self.alpha,
