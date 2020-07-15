b8d0ff256ac15988d61074c49d39f89e6ecb8a6e
Richard <richardhayes777@gmail.com>
2020-07-15 14:35:54 +0100

    moved activity package into groups

--- june/__init__.py
+++ june/__init__.py
@@ -14,7 +14,6 @@ from . import infection
 from . import interaction
 from . import simulator
 from . import policy
-from . import activity
 from .demography import Person
 from .demography import geography
 from .exc import GroupException
