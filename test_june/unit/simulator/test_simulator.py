b822d78736883ce6baa9af71c7844178213df2a5
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-07-28 14:48:40 +0200

    sorted people ids ins sim'

--- test_june/unit/simulator/test_simulator.py
+++ test_june/unit/simulator/test_simulator.py
@@ -70,32 +70,12 @@ def setup_sim(dummy_world, selector):
     leisure_instance = leisure.generate_leisure_for_world(
         world=world, list_of_leisure_groups=["pubs", "cinemas", "groceries"]
     )
-    # world.initialise_commuting()
-    city = CommuteCity()
-    hub = CommuteHub(None, None)
-    commuter = Person.from_attributes(sex="m", age=30)
-    world.households[0].add(commuter)
-    commuter.mode_of_transport = ModeOfTransport(description="bus", is_public=True)
-    commuter.mode_of_transport = "public"
-    world.people.people.append(commuter)
-    city.commutehubs = [hub]
-    world.commutehubs = CommuteHubs([city])
-    world.commutehubs.members = [hub]
-    world.commutecities = CommuteCities()
-    world.commutecities.members = [city]
-    world.commutehubs[0].add(commuter)
-    world.commuteunits = CommuteUnits(world.commutehubs.members)
-    world.commuteunits.init_units()
-    world.commutecityunits = CommuteCityUnits(world.commutecities)
-    world.cemeteries = Cemeteries()
-    care_home = CareHome()
-    world.care_homes = CareHomes([care_home])
     leisure_instance.distribute_social_venues_to_households(world.households)
     interaction = ContactAveraging.from_file()
-    interaction.selector = selector
     policies = Policies.from_file()
     sim = Simulator.from_file(
         world=world,
+        infection_selector = selector,
         interaction=interaction,
         config_filename=test_config,
         leisure=leisure_instance,
@@ -105,134 +85,12 @@ def setup_sim(dummy_world, selector):
     return sim
 
 
-# @pytest.fixture(name="sim", scope="module")
-# def create_simulator(selector):
-#    super_area = SuperArea(name="E02002559", coordinates=[1,1])
-#    area = Area(name="E00062207", super_area=super_area, coordinates=[1,1])
-#    areas = Areas([area])
-#    super_area.areas = areas
-#    super_areas = SuperAreas([super_area])
-#    company = Company(super_area=super_area, n_workers_max=100, sector="Q")
-#    school = School(
-#        coordinates=super_area.coordinates,
-#        n_pupils_max=100,
-#        age_min=4,
-#        age_max=10,
-#        sector="primary",
-#    )
-#    household = Household()
-#    household.area = super_area.areas[0]
-#    care_home = CareHome()
-#    hospital = Hospital(
-#        n_beds=40,
-#        n_icu_beds=5,
-#        super_area=super_area.name,
-#        coordinates=super_area.coordinates,
-#    )
-#    worker = Person.from_attributes(age=40)
-#    worker.area = super_area
-#    household.add(worker, subgroup_type=household.SubgroupType.adults)
-#    worker.sector = "Q"
-#    company.add(worker)
-#
-#    pupil = Person.from_attributes(age=6)
-#    pupil.area = super_area
-#    household.add(pupil, subgroup_type=household.SubgroupType.kids)
-#    household.area = super_area
-#    school.add(pupil)
-#
-#
-#    world = World()
-#    world.areas = areas
-#    world.super_areas = super_areas
-#    world.schools = Schools([school])
-#    world.hospitals = Hospitals([hospital])
-#    world.households = Households([household])
-#    world.universities = Universities([])
-#    world.companies = Companies([company])
-#    world.care_homes = CareHomes([CareHome()])
-#    commuter = Person.from_attributes(sex='m', age=30)
-#    world.people = Population([worker, pupil, commuter])
-#    cinema = Cinema()
-#    cinema.coordinates = super_area.coordinates
-#    world.cinemas = Cinemas([cinema])
-#    pub = Pub()
-#    pub.coordinates = super_area.coordinates
-#    world.pubs = Pubs([pub])
-#    grocery = Grocery()
-#    grocery.coordinates = super_area.coordinates
-#    world.groceries = Groceries([grocery])
-#    leisure_instance = leisure.generate_leisure_for_world(
-#        world=world, list_of_leisure_groups=["pubs", "cinemas", "groceries"]
-#    )
-#    world.initialise_commuting()
-#    world.commutehubs[0].add(commuter)
-#    world.cemeteries = Cemeteries()
-#    leisure_instance.distribute_social_venues_to_households(world.households)
-#    interaction = ContactAveraging.from_file()
-#    interaction.selector = selector
-#    policies = Policies.from_file()
-#    sim = Simulator.from_file(
-#        world=world,
-#        interaction=interaction,
-#        config_filename=test_config,
-#        leisure=leisure_instance,
-#        policies=policies,
-#    )
-#    sim.activity_manager.leisure.generate_leisure_probabilities_for_timestep(3, False)
-#    return sim
-
-# def create_simulator(selector):
-#    geography = Geography.from_file(
-#        {
-#            "super_area": [
-#                "E02003282",
-#                "E02001720",
-#                "E00088544",
-#                "E02002560",
-#                "E02002559",
-#                "E02004314",
-#            ]
-#        }
-#    )
-#    geography.hospitals = Hospitals.for_geography(geography)
-#    geography.care_homes = CareHomes.for_geography(geography)
-#    geography.schools = Schools.for_geography(geography)
-#    geography.universities = Universities.for_super_areas(geography.super_areas)
-#    geography.companies = Companies.for_geography(geography)
-#    world = generate_world_from_geography(
-#        geography=geography, include_commute=True, include_households=True
-#    )
-#    world.to_hdf5("simulator_tests.hdf5")
-#    world = generate_world_from_hdf5("simulator_tests.hdf5")
-#    world.cinemas = Cinemas.for_areas(world.areas)
-#    world.pubs = Pubs.for_areas(world.areas)
-#    world.groceries = Groceries.for_geography(geography)
-#    leisure_instance = leisure.generate_leisure_for_config(
-#        world=world, config_filename=test_config
-#    )
-#    leisure_instance.distribute_social_venues_to_households(world.households)
-#    interaction = ContactAveraging.from_file()
-#    interaction.selector = selector
-#    policies = Policies.from_file()
-#    sim = Simulator.from_file(
-#        world=world,
-#        interaction=interaction,
-#        config_filename=test_config,
-#        leisure=leisure_instance,
-#        policies=policies,
-#    )
-#    sim.activity_manager.leisure.generate_leisure_probabilities_for_timestep(3, False)
-#    return sim
-
-
 @pytest.fixture(name="health_index")
 def create_health_index():
     def dummy_health_index(age, sex):
         return [0.1, 0.3, 0.5, 0.7, 0.9]
 
     return dummy_health_index
-
 
 def test__everyone_has_an_activity(sim: Simulator):
     for person in sim.world.people.members:
