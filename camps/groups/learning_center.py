d096ebc2857372110bafe60a4f1b4f3b0ace8843
florpi <florpisoteadadejardin@gmail.com>
2020-08-09 21:05:05 +0200

    added tree to learning centers

--- camps/groups/learning_center.py
+++ camps/groups/learning_center.py
@@ -1,34 +1,102 @@
-from typing import List
+from typing import List, Tuple, Optional
+import numpy as np
 import collections
+from enum import IntEnum
+from sklearn.neighbors import BallTree
 from june.groups import Group, Supergroup
 from june.demography import Person
 
 
 class LearningCenter(Group):
-    def __init__(self):
+    class SubgroupType(IntEnum):
+        teachers = 0
+        students = 1
+
+    def __init__(
+        self, coordinates: Tuple[float, float], n_pupils_max: int,
+    ):
         super().__init__()
+        self.coordinates = coordinates
+        self.n_pupils_max = n_pupils_max
         self.active_shift = 0
         self.has_shifts = True
         self.ids_per_shift = collections.defaultdict(list)
-    
-    def add(self, person: Person, shift: int):
-        super().add(person=person, activity="primary_activity", subgroup_type=0)
+
+    def add(self, person: Person, shift: int, subgroup_type=SubgroupType.students):
+        super().add(
+            person=person, activity="primary_activity", subgroup_type=subgroup_type
+        )
         self.ids_per_shift[shift].append(person.id)
 
+    @property
+    def n_pupils(self):
+        return len(self.students)
+
+    @property
+    def n_teachers(self):
+        return len(self.teachers)
+
+    @property
+    def teachers(self):
+        return self.subgroups[self.SubgroupType.teachers]
+
+    @property
+    def students(self):
+        return self.subgroups[self.SubgroupType.students]
+
+
 class LearningCenters(Supergroup):
-    def __init__(self, learning_centers: List[LearningCenter]):
+    def __init__(
+        self, learning_centers: List[LearningCenter], learning_centers_tree: Optional[BallTree] = None
+    ):
         super().__init__()
-        self.members = learning_centers 
+        self.members = learning_centers
+        self.learning_centers_tree = learning_centers_tree
         self.has_shifts = True
 
-    def get_closest(self):
-        return self[0]
-    
+    @staticmethod
+    def _create_learning_center_tree(
+        learning_centers_coordinates: np.ndarray,
+    ) -> BallTree:
+        """
+
+        Parameters
+        ----------
+        learning centers coordinates: 
+            array with coordinates
+
+        Returns
+        -------
+        Tree to query nearby learning centers 
+
+        """
+        return BallTree(np.deg2rad(schools_coordinates), metric="haversine")
+
+    def get_closest(self, coordinates: Tuple[float, float], k: int) -> int:
+        """
+        Get the k-th closest learning center to a given coordinate
+
+        Parameters
+        ----------
+        coordinates: 
+            latitude and longitude
+        k:
+            k-th neighbour
+
+        Returns
+        -------
+        ID of the k-th closest learning center
+
+        """
+        coordinates_rad = np.deg2rad(coordinates).reshape(1, -1)
+        k = min(k, len(list(self.learning_centers_tree.data)))
+        distances, neighbours = self.learning_centers_tree.query(
+            coordinates_rad, k=k, sort_results=True,
+        )
+        return neighbours[0]
+
     def activate_next_shift(self, n_shifts):
         for learning_center in self.members:
             learning_center.active_shift += 1
             if learning_center.active_shift == n_shifts:
                 learning_center.active_shift = 0
-
-
-
--- test_camps/test_learning_centers.py
+++ test_camps/test_learning_centers.py
@@ -1,3 +1,6 @@
+import numpy as np
+from sklearn.neighbors import BallTree
+
 from june.demography import Person, Population
 from june.time import Timer
 from june.world import World
@@ -7,11 +10,12 @@ from june.policy import Policies
 from camps.groups import LearningCenter, LearningCenters
 from camps.activity import CampActivityManager
 
+
 def make_dummy_world():
-    pupil_shift_1 = Person.from_attributes(age=12, sex='f')
-    pupil_shift_2 = Person.from_attributes(age=5, sex='m')
-    pupil_shift_3 = Person.from_attributes(age=11, sex='f')
-    learning_center = LearningCenter()
+    pupil_shift_1 = Person.from_attributes(age=12, sex="f")
+    pupil_shift_2 = Person.from_attributes(age=5, sex="m")
+    pupil_shift_3 = Person.from_attributes(age=11, sex="f")
+    learning_center = LearningCenter(coordinates=None, n_pupils_max=None)
     household = Household()
     household.add(person=pupil_shift_1)
     household.add(person=pupil_shift_2)
@@ -20,39 +24,60 @@ def make_dummy_world():
     learning_center.add(person=pupil_shift_2, shift=1)
     learning_center.add(person=pupil_shift_3, shift=2)
     world = World()
-    world.learning_centers = LearningCenters([learning_center])
+    world.learning_centers = LearningCenters([learning_center],)
     world.households = Households([household])
     world.people = Population([pupil_shift_1, pupil_shift_2, pupil_shift_3])
     for person in world.people.members:
         person.busy = False
     learning_center.clear()
     household.clear()
-    return pupil_shift_1, pupil_shift_2, pupil_shift_3, learning_center, household, world
+    return (
+        pupil_shift_1,
+        pupil_shift_2,
+        pupil_shift_3,
+        learning_center,
+        household,
+        world,
+    )
+
 
 def make_dummy_activity_manager(world):
     timer = Timer(
-            total_days=10,
-            weekday_step_duration=[8,8,8],
-            weekend_step_duration=[8,8,8],
-            weekday_activities=[['primary_activity', 'residence'],
-                ['primary_activity', 'residence']],
-            weekend_activities=[['primary_activity', 'residence'],
-                ['primary_activity', 'residence']]
-            )
+        total_days=10,
+        weekday_step_duration=[8, 8, 8],
+        weekend_step_duration=[8, 8, 8],
+        weekday_activities=[
+            ["primary_activity", "residence"],
+            ["primary_activity", "residence"],
+        ],
+        weekend_activities=[
+            ["primary_activity", "residence"],
+            ["primary_activity", "residence"],
+        ],
+    )
     activity_manager = CampActivityManager(
-            world=world,
-            policies=Policies.from_file(),
-            timer=timer,
-            all_activities=['primary_activity', 'residence'],
-            activity_to_groups={'primary_activity': ['learning_centers'],
-                'residence': ['households']}
-            )
+        world=world,
+        policies=Policies.from_file(),
+        timer=timer,
+        all_activities=["primary_activity", "residence"],
+        activity_to_groups={
+            "primary_activity": ["learning_centers"],
+            "residence": ["households"],
+        },
+    )
     return activity_manager
 
 
 def test__activate_next_shift():
 
-    pupil_shift_1, pupil_shift_2, pupil_shift_3, learning_center, household, world = make_dummy_world()
+    (
+        pupil_shift_1,
+        pupil_shift_2,
+        pupil_shift_3,
+        learning_center,
+        household,
+        world,
+    ) = make_dummy_world()
     activity_manager = make_dummy_activity_manager(world)
     assert learning_center.active_shift == 0
     activity_manager.activate_next_shift()
@@ -62,15 +87,23 @@ def test__activate_next_shift():
     activity_manager.activate_next_shift()
     assert learning_center.active_shift == 0
 
+
 def test__shift_manager_moving_people():
-    pupil_shift_1, pupil_shift_2, pupil_shift_3, learning_center, household, world = make_dummy_world()
+    (
+        pupil_shift_1,
+        pupil_shift_2,
+        pupil_shift_3,
+        learning_center,
+        _,
+        world,
+    ) = make_dummy_world()
     activity_manager = make_dummy_activity_manager(world)
     assert pupil_shift_1.id in learning_center.ids_per_shift[0]
     assert pupil_shift_2.id in learning_center.ids_per_shift[1]
     assert pupil_shift_3.id in learning_center.ids_per_shift[2]
     activity_manager.do_timestep()
 
-    assert pupil_shift_1  in learning_center.people
+    assert pupil_shift_1 in learning_center.people
     assert pupil_shift_2 not in learning_center.people
     assert pupil_shift_3 not in learning_center.people
 
@@ -100,3 +133,19 @@ def test__shift_manager_moving_people():
     assert pupil_shift_3 not in learning_center.people
 
 
+def test__get_closest_learning_center():
+
+    coordinates_1 = (12.3, 15.6)
+    learning_center_1 = LearningCenter(coordinates=coordinates_1, n_pupils_max=20)
+    coordinates_2 = (120.3, 150.6)
+    learning_center_2 = LearningCenter(coordinates=coordinates_2, n_pupils_max=20)
+    coordinates = np.vstack((np.array(coordinates_1), np.array(coordinates_2))).T
+    tree = BallTree(np.deg2rad(coordinates), metric="haversine")
+    learning_centers = LearningCenters(
+        learning_centers=[learning_center_1, learning_center_2],
+        learning_centers_tree=tree,
+    )
+
+    closest = learning_centers.get_closest(coordinates=(121.5, 130.2), k=1)
+
+    assert learning_centers.members[closest[0]] == learning_center_2
