235252c0f9468dfc982ce3bb4741cfb8d4cb6159
florpi <florpisoteadadejardin@gmail.com>
2020-08-10 11:59:43 +0200

    everyone goes to learning centers

--- test_camps/test_learning_centers_distributor.py
+++ test_camps/test_learning_centers_distributor.py
@@ -6,28 +6,31 @@ from june.demography.geography import Area, Areas
 from camps.distributors import LearningCenterDistributor
 from camps.groups import LearningCenter, LearningCenters
 
+
 def test__age_distribution():
-    dummy_area = Area(name='dummy', super_area=None, coordinates = (12., 15.))
+    dummy_area = Area(name="dummy", super_area=None, coordinates=(12.0, 15.0))
     dummy_areas = Areas(areas=[dummy_area],)
-    people = [Person.from_attributes(sex='f', age=age) for age in range(100)]
+    people = [Person.from_attributes(sex="f", age=age) for age in range(100)]
     for person in people:
         person.area = dummy_area
     dummy_area.people = people
-    male_enrollment_rates = {'0-4': 0.,
-                    '4-6': 0.5,
-                    '6-12': 0.3,
-                    '12-16': 0.2,
-                    '16-21': 0.1,
-                    '21-100': 0.,}
-                     
-    female_enrollment_rates = { 
-                    '0-4': 0.0,
-                    '4-6': 0.0,
-                    '6-12': 1.,
-                    '12-16': 0.0,
-                    '16-21': 0.0,
-                    '21-100': 0.,
-                    }
+    male_enrollment_rates = {
+        "0-4": 0.0,
+        "4-6": 0.5,
+        "6-12": 0.3,
+        "12-16": 0.2,
+        "16-21": 0.1,
+        "21-100": 0.0,
+    }
+
+    female_enrollment_rates = {
+        "0-4": 0.0,
+        "4-6": 0.0,
+        "6-12": 1.0,
+        "12-16": 0.0,
+        "16-21": 0.0,
+        "21-100": 0.0,
+    }
 
     coordinates_1 = (12.3, 15.6)
     learning_center_1 = LearningCenter(coordinates=coordinates_1, n_pupils_max=20)
@@ -36,43 +39,49 @@ def test__age_distribution():
     coordinates = np.vstack((np.array(coordinates_1), np.array(coordinates_2))).T
     learning_centers_tree = BallTree(np.deg2rad(coordinates), metric="haversine")
 
-    learning_centers = LearningCenters(learning_centers=[learning_center_1, learning_center_2],
-        learning_centers_tree = learning_centers_tree)
+    learning_centers = LearningCenters(
+        learning_centers=[learning_center_1, learning_center_2],
+        learning_centers_tree=learning_centers_tree,
+    )
 
-    learning_center_distributor = LearningCenterDistributor(learning_centers=learning_centers,
-        female_enrollment_rates = female_enrollment_rates,
-        male_enrollment_rates=male_enrollment_rates)
+    learning_center_distributor = LearningCenterDistributor(
+        learning_centers=learning_centers,
+        female_enrollment_rates=female_enrollment_rates,
+        male_enrollment_rates=male_enrollment_rates,
+    )
     learning_center_distributor.distribute_kids_to_learning_centers(areas=dummy_areas)
 
     for kid in people:
-        if kid.age < 6 or kid.age>=12:
+        if kid.age < 6 or kid.age >= 12:
             assert kid.primary_activity is None
         else:
-            assert kid.primary_activity.group.spec == 'learning_center' 
+            assert kid.primary_activity.group.spec == "learning_center"
 
 
 def test__distribute_teachers():
-    dummy_area = Area(name='dummy', super_area=None, coordinates = (12., 15.))
+    dummy_area = Area(name="dummy", super_area=None, coordinates=(12.0, 15.0))
     dummy_areas = Areas(areas=[dummy_area],)
-    people = [Person.from_attributes(sex='f', age=age) for age in range(100)]
+    people = [Person.from_attributes(sex="f", age=age) for age in range(100)]
     for person in people:
         person.area = dummy_area
     dummy_area.people = people
-    male_enrollment_rates = {'0-4': 0.,
-                    '4-6': 0.5,
-                    '6-12': 0.3,
-                    '12-16': 0.2,
-                    '16-21': 0.1,
-                    '21-100': 0.,}
-                     
-    female_enrollment_rates = { 
-                    '0-4': 0.0,
-                    '4-6': 0.0,
-                    '6-12': 1.,
-                    '12-16': 0.0,
-                    '16-21': 0.0,
-                    '21-100': 0.,
-                    }
+    male_enrollment_rates = {
+        "0-4": 0.0,
+        "4-6": 0.5,
+        "6-12": 0.3,
+        "12-16": 0.2,
+        "16-21": 0.1,
+        "21-100": 0.0,
+    }
+
+    female_enrollment_rates = {
+        "0-4": 0.0,
+        "4-6": 0.0,
+        "6-12": 1.0,
+        "12-16": 0.0,
+        "16-21": 0.0,
+        "21-100": 0.0,
+    }
 
     coordinates_1 = (12.3, 15.6)
     learning_center_1 = LearningCenter(coordinates=coordinates_1, n_pupils_max=20)
@@ -81,13 +90,19 @@ def test__distribute_teachers():
     coordinates = np.vstack((np.array(coordinates_1), np.array(coordinates_2))).T
     learning_centers_tree = BallTree(np.deg2rad(coordinates), metric="haversine")
 
-    learning_centers = LearningCenters(learning_centers=[learning_center_1, learning_center_2],
-        learning_centers_tree = learning_centers_tree)
+    learning_centers = LearningCenters(
+        learning_centers=[learning_center_1, learning_center_2],
+        learning_centers_tree=learning_centers_tree,
+    )
 
-    learning_center_distributor = LearningCenterDistributor(learning_centers=learning_centers,
-        female_enrollment_rates = female_enrollment_rates,
-        male_enrollment_rates=male_enrollment_rates)
-    learning_center_distributor.distribute_teachers_to_learning_centers(areas=dummy_areas)
+    learning_center_distributor = LearningCenterDistributor(
+        learning_centers=learning_centers,
+        female_enrollment_rates=female_enrollment_rates,
+        male_enrollment_rates=male_enrollment_rates,
+    )
+    learning_center_distributor.distribute_teachers_to_learning_centers(
+        areas=dummy_areas
+    )
 
     for learning_center in learning_centers:
         assert len(learning_center.teachers) == 3
@@ -95,6 +110,3 @@ def test__distribute_teachers():
         assert learning_center.ids_per_shift[0] == learning_center.ids_per_shift[1]
         assert learning_center.ids_per_shift[1] == learning_center.ids_per_shift[2]
         assert learning_center.teachers[0].age >= 21
-
-
-
