e571f229109ab3e668f8df10cd8b15a74c71954e
florpi <florpisoteadadejardin@gmail.com>
2020-08-10 11:59:43 +0200

    everyone goes to learning centers

--- test_camps/test_learning_centers.py
+++ test_camps/test_learning_centers.py
@@ -12,26 +12,38 @@ from camps.activity import CampActivityManager
 
 
 def make_dummy_world():
+    teacher = Person.from_attributes(age=100, sex="f")
     pupil_shift_1 = Person.from_attributes(age=12, sex="f")
     pupil_shift_2 = Person.from_attributes(age=5, sex="m")
     pupil_shift_3 = Person.from_attributes(age=11, sex="f")
     learning_center = LearningCenter(coordinates=None, n_pupils_max=None)
     household = Household()
+    household.add(person=teacher)
     household.add(person=pupil_shift_1)
     household.add(person=pupil_shift_2)
     household.add(person=pupil_shift_3)
+    learning_center.add(
+        person=teacher, shift=0, subgroup_type=learning_center.SubgroupType.teachers
+    )
+    learning_center.add(
+        person=teacher, shift=1, subgroup_type=learning_center.SubgroupType.teachers
+    )
+    learning_center.add(
+        person=teacher, shift=2, subgroup_type=learning_center.SubgroupType.teachers
+    )
     learning_center.add(person=pupil_shift_1, shift=0)
     learning_center.add(person=pupil_shift_2, shift=1)
     learning_center.add(person=pupil_shift_3, shift=2)
     world = World()
     world.learning_centers = LearningCenters([learning_center],)
     world.households = Households([household])
-    world.people = Population([pupil_shift_1, pupil_shift_2, pupil_shift_3])
+    world.people = Population([teacher,pupil_shift_1, pupil_shift_2, pupil_shift_3])
     for person in world.people.members:
         person.busy = False
     learning_center.clear()
     household.clear()
     return (
+        teacher,
         pupil_shift_1,
         pupil_shift_2,
         pupil_shift_3,
@@ -71,6 +83,7 @@ def make_dummy_activity_manager(world):
 def test__activate_next_shift():
 
     (
+        teacher,
         pupil_shift_1,
         pupil_shift_2,
         pupil_shift_3,
@@ -90,6 +103,7 @@ def test__activate_next_shift():
 
 def test__shift_manager_moving_people():
     (
+        teacher,
         pupil_shift_1,
         pupil_shift_2,
         pupil_shift_3,
@@ -98,11 +112,15 @@ def test__shift_manager_moving_people():
         world,
     ) = make_dummy_world()
     activity_manager = make_dummy_activity_manager(world)
+    assert teacher.id in learning_center.ids_per_shift[0]
+    assert teacher.id in learning_center.ids_per_shift[1]
+    assert teacher.id in learning_center.ids_per_shift[2]
     assert pupil_shift_1.id in learning_center.ids_per_shift[0]
     assert pupil_shift_2.id in learning_center.ids_per_shift[1]
     assert pupil_shift_3.id in learning_center.ids_per_shift[2]
     activity_manager.do_timestep()
 
+    assert teacher in learning_center.people
     assert pupil_shift_1 in learning_center.people
     assert pupil_shift_2 not in learning_center.people
     assert pupil_shift_3 not in learning_center.people
@@ -112,6 +130,7 @@ def test__shift_manager_moving_people():
         person.busy = False
     activity_manager.do_timestep()
 
+    assert teacher in learning_center.people
     assert pupil_shift_1 not in learning_center.people
     assert pupil_shift_2 in learning_center.people
     assert pupil_shift_3 not in learning_center.people
@@ -120,6 +139,7 @@ def test__shift_manager_moving_people():
     for person in world.people.members:
         person.busy = False
     activity_manager.do_timestep()
+    assert teacher in learning_center.people
     assert pupil_shift_1 not in learning_center.people
     assert pupil_shift_2 not in learning_center.people
     assert pupil_shift_3 in learning_center.people
@@ -128,6 +148,7 @@ def test__shift_manager_moving_people():
     for person in world.people.members:
         person.busy = False
     activity_manager.do_timestep()
+    assert teacher in learning_center.people
     assert pupil_shift_1 in learning_center.people
     assert pupil_shift_2 not in learning_center.people
     assert pupil_shift_3 not in learning_center.people
