11838ffde141491fea3f8f4104152885449bfba9
Valeriu Predoi <valeriu.predoi@gmail.com>
2020-08-11 09:55:45 +0100

    changed household complacency default arg to 1 as per Arnau suggestion

--- june/groups/household.py
+++ june/groups/household.py
@@ -42,7 +42,7 @@ class Household(Group):
         adults = 2
         old_adults = 3
 
-    def __init__(self, type=None, area=None, max_size=np.inf, household_complacency=random()):
+    def __init__(self, type=None, area=None, max_size=np.inf, household_complacency=1.0):
         """
         Type should be on of ["family", "student", "young_adults", "old", "other", "nokids", "ya_parents", "communal"].
         Relatives is a list of people that are related to the family living in the household
