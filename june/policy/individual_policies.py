a8204a03028df3fb2c1f55b8eee895886860ad04
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-08-12 09:26:58 +0100

    alter policies for new setup with compant closure

--- june/policy/individual_policies.py
+++ june/policy/individual_policies.py
@@ -22,7 +22,7 @@ class IndividualPolicies(PolicyCollection):
     policy_type = "individual"
     min_age_home_alone = 15
 
-    def apply(self, person: Person, days_from_start: float, activities: List[str], furlough_ratio: float, key_ratio: float):
+    def apply(self, person: Person, days_from_start: float, activities: List[str], furlough_ratio=None, key_ratio=None, random_ratio=None):
         """
         Applies all active individual policies to the person. Stay home policies are applied first,
         since if the person stays home we don't need to check for the others.
