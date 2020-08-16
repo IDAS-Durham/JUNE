b1adb0c895f1f9ae02dd061ddef75bec01a698c9
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-08-16 23:35:16 +0100

    full run parse help working for masks

--- camp_scripts/full_run_parse.py
+++ camp_scripts/full_run_parse.py
@@ -61,9 +61,9 @@ parser.add_argument('-p', '--parameters', help="Parameter file", required=False,
 parser.add_argument('-u', '--isolation_units', help="True to include isolation units", required=False, default="False")
 parser.add_argument('-t', '--isolation_testing', help="Model weights in HDF5 format", required=False, default=3)
 parser.add_argument('-i', '--isolation_time', help="Ouput file name", required=False, default=7)
-parser.add_argument('-a', '--isolation_compliance', help="Isolation unit self reporting compliance", required=False, default=0.6)
+parser.add_argument('-ic', '--isolation_compliance', help="Isolation unit self reporting compliance", required=False, default=0.6)
 parser.add_argument('-m', '--mask_wearing', help="True to include mask wearing", required=False, default="False")
-parser.add_argument('-m', '--mask_compliance', help="Mask wearing compliance", required=False, default="False")
+parser.add_argument('-mc', '--mask_compliance', help="Mask wearing compliance", required=False, default="False")
 parser.add_argument('-inf', '--infectiousness_path', help="path to infectiousness parameter file", required=False, default='nature')
 parser.add_argument('-s', '--save_path', help="Path of where to save logger", required=False, default="results")
 parser.add_argument('-lc', '--learning_centers' ,help="Add learning centers", required=False, default=False)
@@ -199,7 +199,7 @@ if args.isolation_units:
     policies.policies[3].testing_mean_time = args.isolation_testing
     policies.policies[3].compliance = args.isolation_compliance
 
-elif: args.mask_wearing:
+elif args.mask_wearing:
     policies = Policies.from_file(
         camp_configs_path / "defaults/policy/mask_wearing.yaml",
         base_policy_modules=("june.policy", "camps.policy"),
