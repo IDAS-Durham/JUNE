1c94334d07a08d77d10271670e4af75bc52637c1
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-08-20 16:03:15 +0200

    reworked symptoms class

--- june/infection/symptoms.py
+++ june/infection/symptoms.py
@@ -6,16 +6,21 @@ from june.infection.trajectory_maker import TrajectoryMakers
 
 
 class Symptoms:
+    """
+    Class to represent the symptoms of a person. The symptoms class composes the
+    ``Infection`` class alongside with the ``Transmission`` class. Once infected,
+    a person is assigned a symptoms trajectory according to a health index generated
+    by the ``HealthIndexGenerator``. A trajectory is a collection of symptom tags with
+    characteristic timings.
+    """
     def __init__(self, health_index=None):
-        self.health_index = [] if health_index is None else health_index
-        self.tag = SymptomTag.exposed
         self.max_severity = random()
-        self.trajectory = None
-        self.update_trajectory()
+        self.trajectory = self._make_symptom_trajectory(health_index)
         self.stage = 0
         self.tag = self.trajectory[self.stage][1]
+        self.time_of_symptoms_onset = self._time_from_infection_to_symptoms()
 
-    def time_from_infection_to_symptoms(self):
+    def _compute_time_from_infection_to_symptoms(self):
         symptoms_onset = 0
         for completion_time, tag in self.trajectory:
             symptoms_onset += completion_time
@@ -25,22 +30,36 @@ class Symptoms:
                 return None
         return symptoms_onset
 
+    def _make_symptom_trajectory(self, health_index):
+        if health_index is None:
+            return SymptomTag(0)
+        trajectory_maker = TrajectoryMakers.from_file()
+        index_max_symptoms_tag = np.searchsorted(self.health_index, self.max_severity)
+        max_symptoms_tag = SymptomTag(index_max_symptoms_tag)
+        return trajectory_maker[max_symptoms_tag]
+
+    @property
     def time_exposed(self):
         return self.trajectory[1][0]
 
+    @property
     def is_recovered(self):
         return self.tag == SymptomTag.recovered
 
-    def update_trajectory(self):
-        trajectory_maker = TrajectoryMakers.from_file()
-        maxtag = self.max_tag()
-        self.trajectory = trajectory_maker[maxtag]
-
+    @property
     def max_tag(self):
-        index = np.searchsorted(self.health_index, self.max_severity)
-        return SymptomTag(index)
+        self.trajectory[-1][1]
 
-    def update_severity_from_delta_time(self, time_from_infection):
+    def update_trajectory_stage(self, time_from_infection):
+        """
+        Updates the current symptom tag from the symptoms trajectory,
+        given how much time has passed since the person was infected.
+
+        Parameters
+        ----------
+        time_from_infection: float
+            Time in days since the person got infected.
+        """
         if time_from_infection > self.trajectory[self.stage + 1][0]:
             self.stage += 1
             self.tag = self.trajectory[self.stage][1]
