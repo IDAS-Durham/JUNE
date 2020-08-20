3d6a8a84e2cb5f87fd7771e9ee83c47f62d13022
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-08-20 16:03:15 +0200

    reworked symptoms class

--- june/infection/health_information.py
+++ june/infection/health_information.py
@@ -4,44 +4,18 @@ dead_tags = (SymptomTag.dead_home, SymptomTag.dead_hospital, SymptomTag.dead_icu
 
 class HealthInformation:
     __slots__ = (
-        "susceptibility",
-        "susceptible",
-        "infected",
         "infection",
-        "recovered",
-        "dead",
         "number_of_infected",
-        "maximal_symptoms",
-        "maximal_symptoms_time",
-        "maximal_symptoms_tag",
-        "time_of_infection",
-        "time_of_symptoms_onset",
-        "length_of_infection",
-        "infecter",
+        "time_of_symptoms_onset"
     )
 
     def __init__(self):
-        self.susceptibility = 1.0
-        self.susceptible = True
-        self.infected = False
         self.infection = None
-        self.recovered = False
-        self.dead = False
         self.number_of_infected = 0
-        self.maximal_symptoms = 0
-        self.maximal_symptoms_time = -1
-        self.maximal_symptoms_tag = None
-        self.time_of_infection = -1
-        self.length_of_infection = -1
-        self.infecter = None
 
     def set_infection(self, infection):
         self.infection = infection
-        self.infected = True
-        self.susceptible = False
-        self.susceptibility = 0.0
-        self.time_of_infection = infection.start_time
-        time_to_symptoms = infection.symptoms.time_from_infection_to_symptoms()
+        time_to_symptoms = infection.symptoms.
         if time_to_symptoms is None:
             self.time_of_symptoms_onset = None
         else:
@@ -52,6 +26,13 @@ class HealthInformation:
         if self.infection is not None:
             return self.infection.symptoms.tag
         return None
+
+    @property
+    def time_of_infection(self):
+        try:
+            return self.infection.start_time
+        except:
+            return None
 
     @property
     def should_be_in_hospital(self) -> bool:
@@ -74,7 +55,6 @@ class HealthInformation:
         self.recovered = True
         self.infected = False
         self.susceptible = False
-        self.susceptibility = 0.0
         self.set_length_of_infection(time)
         self.infection = None
 
@@ -82,7 +62,6 @@ class HealthInformation:
         self.dead = True
         self.infected = False
         self.susceptible = False
-        self.susceptibility = 0.0
         self.set_length_of_infection(time)
         self.infection = None
 
