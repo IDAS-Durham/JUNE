a72f0790938e7ebd0223c0cdced1eb720c7022ce
florpi <florpisoteadadejardin@gmail.com>
2020-09-02 13:55:49 +0200

    added regional summaries to legacy, optimize current read logger

--- june/logger/read_logger_legacy.py
+++ june/logger/read_logger_legacy.py
@@ -213,6 +213,41 @@ class ReadLoggerLegacy:
             areas_df.append(area_df)
         return pd.concat(areas_df)
 
+    def super_areas_to_region_mapping(self, super_areas, super_area_region_path=paths.data_path / 'input/geography/area_super_area_region.csv'):
+        super_area_region = pd.read_csv(super_area_region_path)
+        super_area_region = super_area_region.drop(columns='area').drop_duplicates()
+        super_area_region.set_index('super_area', inplace=True)
+        return super_area_region.loc[super_areas]['region'].values
+
+    def region_summary(self) -> pd.DataFrame:
+        """ 
+        Generate a summary for regions, on how many people are recovered, dead, infected,
+        susceptible, hospitalised or in intensive care, per time step.
+
+        Returns
+        -------
+        A data frame whose index is the date recorded, and columns are regions, number of recovered,
+        dead, infected...
+        """
+        regions = self.super_areas_to_region_mapping(self.super_areas)
+        self.infections_df["regions"] = self.infections_df.apply(
+          lambda x: regions[x.infected_id], axis=1
+        )
+        regions_df = []
+        for region in np.unique(regions):
+            region_df = pd.DataFrame()
+            n_people_in_region = np.sum(regions == region)
+            region_df["symptoms"] = self.infections_df.apply(
+              lambda x: x.symptoms[x.regions == region], axis=1
+            )
+            region_df["infected_id"] = self.infections_df.apply(
+              lambda x: x.infected_id[x.regions == region], axis=1
+            )
+            region_df = self.process_symptoms(region_df, n_people_in_region)
+            region_df["region"] = region
+            regions_df.append(region_df)
+        return pd.concat(regions_df)
+
     def age_summary(self, age_ranges: List[int]) -> pd.DataFrame:
         """
         Generate a summary per age range, on how many people are recovered, dead, infected,
