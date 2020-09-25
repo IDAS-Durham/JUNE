e874ded1cc6bf55e2264308873ec0dd83e029787
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-09-25 15:26:25 +0100

    drafting of master plotting and school reopening

--- plots/scripts/master_plotter.py
+++ plots/scripts/master_plotter.py
@@ -3,16 +3,16 @@ import time
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 
+from june.hdf5_savers import generate_world_from_hdf5
 from policy import PolicyPlots
 
-default_save_dir = ''
+default_world_filename = 'world.hdf5'
 
 class Plotting:
 
     def __init__(
             self,
             world = default_world,
-            save_dir = default_save_dir,
     ):
 
         self.world = world
@@ -22,17 +22,23 @@ class Plotting:
             cls,
             world_filename: str = default_world_filename,
     ):
-        [LOAD WORLD]
+        world = generate_world_from_hdf5(world_filename)
 
         return Plotting(world)
         
-    def plot_policies(self):
+    def plot_policies(
+            self,
+            save_dir: str = '../plots/policy/'
+    ):
 
         policy_plots = PolicyPlots(self.world)
 
         school_reopening_plot = policy_plots.plot_school_reopening()
+        school_reopening_plot.savefig(save_dir + 'school_reopening.png', dpi=150, bbox_inches='tight')
 
-        school_reopening_plot.savefig()
-        
+    
+    def plot_all(self):
+
+        self.plot_policies()
 
     
