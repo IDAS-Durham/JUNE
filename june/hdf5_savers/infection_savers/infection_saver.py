178ecbf44b90fe0db2dd72fbe60fb3b49675bdac
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-09-25 10:54:53 +0200

    infection saver works

---
+++ june/hdf5_savers/infection_savers/infection_saver.py
@@ -0,0 +1,112 @@
+import numpy as np
+import h5py
+from collections import defaultdict
+from typing import List
+
+from june.hdf5_savers.utils import read_dataset, write_dataset
+from june.infection import Infection
+from .symptoms_saver import save_symptoms_to_hdf5, load_symptoms_from_hdf5
+from .transmission_saver import save_transmissions_to_hdf5, load_transmissions_from_hdf5
+
+int_vlen_type = h5py.vlen_dtype(np.dtype("int64"))
+float_vlen_type = h5py.vlen_dtype(np.dtype("float64"))
+
+
+def save_infections_to_hdf5(
+    hdf5_file_path: str, infections: List[Infection], chunk_size: int = 50000,
+):
+    """
+    Saves infections data to hdf5.
+
+    Parameters
+    ----------
+    attributes_to_save
+        attributes to save from each symptom
+    hdf5_file_path
+        hdf5 path to save symptoms
+    symptoms
+        list of symptom objects
+    chunk_size
+        number of hdf5 chunks to use while saving
+    """
+    with h5py.File(hdf5_file_path, "a") as f:
+        f.create_group("infections")
+        n_infections = len(infections)
+        f["infections"].attrs["n_infections"] = n_infections
+        symptoms_list = [infection.symptoms for infection in infections]
+        transmission_list = [infection.transmission for infection in infections]
+        save_symptoms_to_hdf5(
+            symptoms_list=symptoms_list,
+            hdf5_file_path=hdf5_file_path,
+            chunk_size=chunk_size,
+        )
+        save_transmissions_to_hdf5(
+            transmissions=transmission_list,
+            hdf5_file_path=hdf5_file_path,
+            chunk_size=chunk_size,
+        )
+        attributes_to_save = ["start_time", "number_of_infected"]
+        n_chunks = int(np.ceil(n_infections / chunk_size))
+        for chunk in range(n_chunks):
+            idx1 = chunk * chunk_size
+            idx2 = min((chunk + 1) * chunk_size, n_infections)
+            attribute_dict = defaultdict(list)
+            for index in range(idx1, idx2):
+                infection = infections[index]
+                for attribute_name in attributes_to_save:
+                    attribute = getattr(infection, attribute_name)
+                    if attribute is None:
+                        attribute_dict[attribute_name].append(np.nan)
+                    else:
+                        attribute_dict[attribute_name].append(attribute)
+            for attribute_name in attributes_to_save:
+                data = np.array(attribute_dict[attribute_name], dtype=np.float)
+                write_dataset(
+                    group=f["infections"],
+                    dataset_name=attribute_name,
+                    data=data,
+                    index1=idx1,
+                    index2=idx2,
+                )
+
+
+def load_infections_from_hdf5(hdf5_file_path: str, chunk_size=50000):
+    """
+    Loads infections data from hdf5. 
+
+    Parameters
+    ----------
+    hdf5_file_path
+        hdf5 path to load from  
+    chunk_size
+        number of hdf5 chunks to use while loading
+    """
+    infections = []
+    with h5py.File(hdf5_file_path, "r") as f:
+        infections_group = f["infections"]
+        symptoms_list = load_symptoms_from_hdf5(
+            hdf5_file_path=hdf5_file_path, chunk_size=chunk_size
+        )
+        transmissions = load_transmissions_from_hdf5(
+            hdf5_file_path=hdf5_file_path, chunk_size=chunk_size
+        )
+        n_infections = infections_group.attrs["n_infections"]
+        n_chunks = int(np.ceil(n_infections / chunk_size))
+        for chunk in range(n_chunks):
+            idx1 = chunk * chunk_size
+            idx2 = min((chunk + 1) * chunk_size, n_infections)
+            for index in range(idx1, idx2):
+                infection = Infection(
+                    transmission=transmissions[index], symptoms=symptoms_list[index]
+                )
+                for attribute_name in infections_group.keys():
+                    if attribute_name in ["symptoms", "transmissions"]:
+                        continue
+                    attribute_value = read_dataset(
+                        infections_group[attribute_name], idx1, idx2
+                    )
+                    if attribute_value == np.nan:
+                        attribute_value = None
+                    setattr(infection, attribute_name, attribute_value)
+                infections.append(infection)
+    return infections 
--- test_june/unit/test_infection_savers.py
+++ test_june/unit/test_infection_savers.py
@@ -1,49 +1,95 @@
 import h5py
+import pytest
 import numpy as np
 
 from june.infection.transmission_xnexp import TransmissionXNExp
 from june.infection.transmission import TransmissionGamma
 from june.infection.symptoms import Symptoms, SymptomTag
+from june.infection import Infection
 from june.hdf5_savers.infection_savers import (
     save_transmissions_to_hdf5,
     load_transmissions_from_hdf5,
     save_symptoms_to_hdf5,
     load_symptoms_from_hdf5,
+    save_infections_to_hdf5,
+    load_infections_from_hdf5
 )
 
+@pytest.fixture(name="xnexp_transmissions", scope="module")
+def setup_xnexp_trans():
+    transmission1 = TransmissionXNExp(
+        max_probability=1,
+        time_first_infectious=1,
+        norm_time=2,
+        n=3,
+        alpha=4,
+        max_symptoms="asymptomatic",
+        asymptomatic_infectious_factor=5,
+        mild_infectious_factor=6,
+    )
+    transmission2 = TransmissionXNExp(
+        max_probability=7,
+        time_first_infectious=8,
+        norm_time=9,
+        n=10,
+        alpha=11,
+        max_symptoms="mild",
+        asymptomatic_infectious_factor=12,
+        mild_infectious_factor=13,
+    )
+    transmissions = [transmission1, transmission2]
+    return transmissions
+
+@pytest.fixture(name="gamma_transmissions", scope="module")
+def setup_gamma_trans():
+    transmission1 = TransmissionGamma(
+        max_infectiousness=1.0,
+        shape=2.0,
+        rate=3.0,
+        shift=-2.0,
+        max_symptoms="mild",
+        asymptomatic_infectious_factor=0.5,
+        mild_infectious_factor=0.7,
+    )
+    transmission2 = TransmissionGamma(
+        max_infectiousness=1.1,
+        shape=2.1,
+        rate=3.1,
+        shift=-2.1,
+        max_symptoms="asymptomatic",
+        asymptomatic_infectious_factor=0.2,
+        mild_infectious_factor=0.2,
+    )
+    transmissions = [transmission1, transmission2]
+    return transmissions
+
+@pytest.fixture(name="symptoms_list", scope="module")
+def setup_symptoms():
+    health_index = np.linspace(0, 1, 5)
+    symptoms1 = Symptoms(health_index=health_index)
+    symptoms2 = Symptoms(health_index=health_index)
+    symptoms = [symptoms1, symptoms2]
+    return symptoms
+
+@pytest.fixture(name="infections", scope="module")
+def setup_infections(xnexp_transmissions, symptoms_list):
+    infections = []
+    for symptoms, trans in zip(symptoms_list, xnexp_transmissions):
+        infection = Infection(transmission=trans, symptoms=symptoms, start_time=2)
+        infections.append(infection)
+    return infections
 
 class TestTransmissionSavers:
-    def test__save_xnexp(self):
+    def test__save_xnexp(self, xnexp_transmissions):
         with h5py.File("checkpoint_tests.hdf5", "w") as f:
             pass
-        transmission1 = TransmissionXNExp(
-            max_probability=1,
-            time_first_infectious=1,
-            norm_time=2,
-            n=3,
-            alpha=4,
-            max_symptoms="asymptomatic",
-            asymptomatic_infectious_factor=5,
-            mild_infectious_factor=6,
-        )
-        transmission2 = TransmissionXNExp(
-            max_probability=7,
-            time_first_infectious=8,
-            norm_time=9,
-            n=10,
-            alpha=11,
-            max_symptoms="mild",
-            asymptomatic_infectious_factor=12,
-            mild_infectious_factor=13,
-        )
-        transmissions = [transmission1, transmission2]
-        save_transmissions_to_hdf5("checkpoint_tests.hdf5", transmissions, chunk_size=1)
+        save_transmissions_to_hdf5("checkpoint_tests.hdf5", xnexp_transmissions, chunk_size=1)
         transmissions_recovered = load_transmissions_from_hdf5(
             "checkpoint_tests.hdf5", chunk_size=1
         )
-        assert len(transmissions_recovered) == len(transmissions)
-        for transmission, transmissions_recovered in zip(
-            transmissions_recovered, transmissions
+        assert len(transmissions_recovered) == len(xnexp_transmissions)
+        for transmission, transmission_recovered in zip(
+            xnexp_transmissions, transmissions_recovered 
         ):
             for attribute in [
                 "time_first_infectious",
@@ -54,59 +100,36 @@ class TestTransmissionSavers:
                 "probability",
             ]:
                 assert getattr(transmission, attribute) == getattr(
-                    transmissions_recovered, attribute
+                    transmission_recovered, attribute
                 )
 
-    def test__save_gamma(self):
+    def test__save_gamma(self, gamma_transmissions):
         with h5py.File("checkpoint_tests.hdf5", "w") as f:
             pass
-        transmission1 = TransmissionGamma(
-            max_infectiousness=1.0,
-            shape=2.0,
-            rate=3.0,
-            shift=-2.0,
-            max_symptoms="mild",
-            asymptomatic_infectious_factor=0.5,
-            mild_infectious_factor=0.7,
-        )
-        transmission2 = TransmissionGamma(
-            max_infectiousness=1.1,
-            shape=2.1,
-            rate=3.1,
-            shift=-2.1,
-            max_symptoms="asymptomatic",
-            asymptomatic_infectious_factor=0.2,
-            mild_infectious_factor=0.2,
-        )
-        transmissions = [transmission1, transmission2]
-        save_transmissions_to_hdf5("checkpoint_tests.hdf5", transmissions, chunk_size=1)
+        save_transmissions_to_hdf5("checkpoint_tests.hdf5", gamma_transmissions, chunk_size=1)
         transmissions_recovered = load_transmissions_from_hdf5(
             "checkpoint_tests.hdf5", chunk_size=1
         )
-        assert len(transmissions_recovered) == len(transmissions)
-        for transmission, transmissions_recovered in zip(
-            transmissions_recovered, transmissions
+        assert len(transmissions_recovered) == len(gamma_transmissions)
+        for transmission, transmission_recovered in zip(
+            gamma_transmissions, transmissions_recovered 
         ):
             for attribute in ["shape", "shift", "scale", "norm", "probability"]:
                 assert getattr(transmission, attribute) == getattr(
-                    transmissions_recovered, attribute
+                    transmission_recovered, attribute
                 )
 
 
 class TestSymptomSavers:
-    def test__save_symptoms(self):
+    def test__save_symptoms(self, symptoms_list):
         with h5py.File("checkpoint_tests.hdf5", "w") as f:
             pass
-        health_index = np.linspace(0, 1, 5)
-        symptoms1 = Symptoms(health_index=health_index)
-        symptoms2 = Symptoms(health_index=health_index)
-        symptoms = [symptoms1, symptoms2]
-        save_symptoms_to_hdf5("checkpoint_tests.hdf5", symptoms, chunk_size=1)
+        save_symptoms_to_hdf5("checkpoint_tests.hdf5", symptoms_list, chunk_size=1)
         symptoms_recovered = load_symptoms_from_hdf5(
             "checkpoint_tests.hdf5", chunk_size=1
         )
-        assert len(symptoms_recovered) == len(symptoms)
-        for symptom, symptom_recovered in zip(symptoms, symptoms_recovered):
+        assert len(symptoms_recovered) == len(symptoms_list)
+        for symptom, symptom_recovered in zip(symptoms_list, symptoms_recovered):
             for attribute_name in [
                 "max_tag",
                 "tag",
@@ -124,3 +147,53 @@ class TestSymptomSavers:
                 assert isinstance(stage_recovered[1], SymptomTag)
                 assert stage[0] == stage_recovered[0]
                 assert stage[1] == stage_recovered[1]
+
+class TestInfectionSavers:
+    def test__save_infection(self, infections):
+        with h5py.File("checkpoint_tests.hdf5", "w") as f:
+            pass
+        save_infections_to_hdf5("checkpoint_tests.hdf5", infections, chunk_size=1)
+        infections_recovered= load_infections_from_hdf5(
+            "checkpoint_tests.hdf5", chunk_size=1
+        )
+        assert len(infections_recovered) == len(infections)
+        for infection, infection_recovered in zip(infections, infections_recovered):
+            for attribute_name in [
+                "start_time",
+                "number_of_infected",
+            ]:
+                assert getattr(infection, attribute_name) == getattr(
+                    infection_recovered, attribute_name
+                )
+            symptoms = infection.symptoms
+            symptoms_recovered = infection_recovered.symptoms
+            for attribute_name in [
+                "max_tag",
+                "tag",
+                "max_severity",
+                "stage",
+                "time_of_symptoms_onset",
+            ]:
+                assert getattr(symptoms, attribute_name) == getattr(
+                    symptoms_recovered, attribute_name
+                )
+            trajectory = symptoms.trajectory
+            trajectory_recovered = symptoms_recovered.trajectory
+            assert len(trajectory) == len(trajectory_recovered)
+            for stage, stage_recovered in zip(trajectory, trajectory_recovered):
+                assert isinstance(stage_recovered[1], SymptomTag)
+                assert stage[0] == stage_recovered[0]
+                assert stage[1] == stage_recovered[1]
+            transmission = infection.transmission
+            transmission_recovered = infection_recovered.transmission
+            for attribute in [
+                "time_first_infectious",
+                "norm_time",
+                "n",
+                "norm",
+                "alpha",
+                "probability",
+            ]:
+                assert getattr(transmission, attribute) == getattr(
+                    transmission_recovered, attribute
+                )
