ad4d26678a90ba43d193d899ac43ae280af2ea29
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-09-30 14:08:38 +0200

    included susceptibility in interaction

--- june/interaction/interactive_group.py
+++ june/interaction/interactive_group.py
@@ -15,10 +15,11 @@ class InteractiveGroup:
     - group : group that we want to prepare for interaction.
     """
 
-    def __init__(self, group: Group, people_from_abroad = None):
+    def __init__(self, group: Group, people_from_abroad=None):
         infector_ids = []
         trans_prob = []
         susceptible_ids = []
+        susceptibilities = []
         infector_subgroup_sizes = []
         self.subgroups_infector = []
         self.subgroups_susceptible = []
@@ -30,6 +31,7 @@ class InteractiveGroup:
                 people_from_abroad is not None
                 and subgroup.subgroup_type in people_from_abroad
             ):
+                # get people from abroad data
                 people_abroad_data = people_from_abroad[subgroup.subgroup_type]
                 people_abroad_ids = list(people_abroad_data.keys())
                 self.n_foreign_people += len(people_abroad_ids)
@@ -43,61 +45,84 @@ class InteractiveGroup:
                     for id in people_abroad_ids
                     if people_abroad_data[id]["susc"] > 0.0
                 ]
-                people_abroad_infected_prob = [
-                    people_abroad_data[id]["inf_prob"]
-                    for id in people_abroad_ids
-                    if people_abroad_data[id]["inf_prob"] > 0
-                ]
                 subgroup_size = len(subgroup.people) + len(people_abroad_ids)
                 if subgroup_size == 0:
                     continue
+                # get susceptible people in subgroup
+                subgroup_susceptible = [
+                    person for person in subgroup if person.susceptible
+                ]
+                if subgroup_susceptible:
+                    # store subgroup id and susceptibilities
+                    self.has_susceptible = True
+                    self.subgroups_susceptible.append(i)
+                    sus_ids = [
+                        person.id for person in subgroup_susceptible
+                    ] + people_abroad_susceptible_ids
+                    people_abroad_susceptibilities = [
+                        people_abroad_data[id]["susc"]
+                        for id in people_abroad_ids
+                        if people_abroad_data[id]["susc"] > 0.0
+                    ]
+                    subgroup_susceptibilities = [
+                        person.susceptibility for person in subgroup_susceptible
+                    ] + people_abroad_susceptible_ids
+                    susceptible_ids.append(sus_ids)
+                    susceptibilities.append(subgroup_susceptibilities)
+
+                # get infected people in subgroup
                 subgroup_infected = [
                     person
                     for person in subgroup
                     if person.infection is not None
                     and person.infection.transmission.probability > 0
                 ]
-                sus_ids = [
-                    person.id for person in subgroup.people if person.susceptible
-                ] + people_abroad_susceptible_ids
-                if sus_ids:
-                    self.has_susceptible = True
-                    self.subgroups_susceptible.append(i)
-                    susceptible_ids.append(sus_ids)
-
-                inf_ids = [
-                    person.id for person in subgroup_infected
-                ] + people_abroad_infected_ids
-                if inf_ids:
+                if subgroup_infected:
+                    # has infected
+                    self.has_infector = True
+                    self.subgroups_infector.append(i)
+                    inf_ids = [
+                        person.id for person in subgroup_infected
+                    ] + people_abroad_infected_ids
+                    people_abroad_infected_prob = [
+                        people_abroad_data[id]["inf_prob"]
+                        for id in people_abroad_ids
+                        if people_abroad_data[id]["inf_prob"] > 0
+                    ]
                     tprob = sum(
                         person.infection.transmission.probability
                         for person in subgroup_infected
                     ) + sum(people_abroad_infected_prob)
-                    self.has_infector = True
-                    self.subgroups_infector.append(i)
                     trans_prob.append(tprob)
                     infector_ids.append(inf_ids)
                     infector_subgroup_sizes.append(subgroup_size)
             else:
+                # no foreign people
                 subgroup_size = len(subgroup.people)
                 if subgroup_size == 0:
                     continue
+                # get susceptible people
+                subgroup_susceptible = [
+                    person for person in subgroup if person.susceptible
+                ]
+                if subgroup_susceptible:
+                    self.has_susceptible = True
+                    self.subgroups_susceptible.append(i)
+                    sus_ids = [
+                        person.id for person in subgroup_susceptible
+                    ]
+                    subgroup_susceptibilities = [person.susceptibility for person in subgroup_susceptible]
+                    susceptible_ids.append(sus_ids)
+                    susceptibilities.append(subgroup_susceptibilities)
+                # get infected people
                 subgroup_infected = [
                     person
                     for person in subgroup
                     if person.infection is not None
                     and person.infection.transmission.probability > 0
                 ]
-                sus_ids = [
-                    person.id for person in subgroup.people if person.susceptible
-                ]
-                if sus_ids:
-                    self.has_susceptible = True
-                    self.subgroups_susceptible.append(i)
-                    susceptible_ids.append(sus_ids)
-
-                inf_ids = [person.id for person in subgroup_infected]
-                if inf_ids:
+                if subgroup_infected:
+                    inf_ids = [person.id for person in subgroup_infected]
                     tprob = sum(
                         person.infection.transmission.probability
                         for person in subgroup_infected
@@ -116,6 +141,7 @@ class InteractiveGroup:
         self.infector_ids = infector_ids
         self.transmission_probabilities = trans_prob
         self.susceptible_ids = susceptible_ids
+        self.susceptibilities = susceptibilities
         # self.subgroups_susceptible = self.subgroups_susceptible
         # self.subgroups_infector = self.subgroups_infector
         self.infector_subgroup_sizes = infector_subgroup_sizes
