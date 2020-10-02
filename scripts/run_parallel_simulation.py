d228228cc233ec916b1ac4d6a5070096ee83c61e
florpi <florpisoteadadejardin@gmail.com>
2020-10-02 19:47:05 +0100

    remove after combined

--- scripts/run_parallel_simulation.py
+++ scripts/run_parallel_simulation.py
@@ -42,6 +42,7 @@ def set_random_seed(seed=999):
 class MockHealthIndexGenerator:
     def __init__(self, desired_symptoms=SymptomTag.hospitalised):
         self.index = desired_symptoms
+        self.asymptomatic_ratio = 0.2
 
     def __call__(self, person):
         hi = np.ones(8)
