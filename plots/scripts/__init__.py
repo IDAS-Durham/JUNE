dabacfcb6e8704e5b71f7a5eaccd56df472d1c8b
Henry Truong <dc-truo1@login5b.pri.cosma7.alces.network>
2020-10-04 00:52:40 +0100

    added contact_matrix plots

--- plots/scripts/__init__.py
+++ plots/scripts/__init__.py
@@ -2,3 +2,5 @@ from .policy import PolicyPlots
 from .leisure import LeisurePlots
 from .companies import CompanyPlots
 from .care_homes import CareHomePlots 
+from .households import HouseholdPlots
+from .contact_matrix import ContactMatrixPlots
