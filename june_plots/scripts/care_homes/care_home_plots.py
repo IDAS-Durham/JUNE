b629e59c81f371bacd40bfa205a34f5a3de28c88
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-10-06 11:44:27 +0100

    plot updates

--- june_plots/scripts/care_homes/care_home_plots.py
+++ june_plots/scripts/care_homes/care_home_plots.py
@@ -89,8 +89,8 @@ class CareHomePlots:
             )
 
         f, ax = plt.subplots()
-        self.percent["Persons"].plot.bar(ax=ax, label="NOMIS")
-        june_percent["Persons"].plot.bar(ax=ax, alpha=0.5, label="JUNE")
+        self.percent["Persons"].plot.bar(ax=ax, label="NOMIS", color='blue', alpha=0.7)
+        june_percent["Persons"].plot.bar(ax=ax, label="JUNE", color='green', alpha=0.7)
         ax.set_ylabel("\% of population in care homes")
         ax.legend()
         return ax
