0cfc1387fc0f901505078452ad08f48565fa0bd0
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-10-07 23:04:07 +0200

    new leisure plots

--- june_plots/scripts/leisure/leisure_plots.py
+++ june_plots/scripts/leisure/leisure_plots.py
@@ -6,188 +6,167 @@ from glob import glob
 from scipy import stats
 from random import random
 from tqdm import tqdm
+from itertools import chain
 import matplotlib.pyplot as plt
 
 from june.utils import parse_age_probabilities
+from june.activity import ActivityManager
+from june.policy import Policies
+from june.groups.leisure import Leisure, generate_leisure_for_config
+from june.time import Timer
+from june.groups.travel import Travel
+from june.simulator import Simulator
 from june import paths
 
 config_files_leisure = (paths.configs_path / "defaults/groups/leisure").glob("*.yaml")
+simulation_config = paths.configs_path / "config_example.yaml"
+
+# source: private comm with Aoife
+time_survey = {
+    "residence": 0.635 + 0.002,
+    "work": 0.08,
+    "household visits": 0.034,
+    "groceries": 0.013,
+    "pubs": 0.012,
+    "commute": 0.003 + 0.001 + 0.00064 + 0.0004 + 0.0002 + 0.000157 + 0.000032,
+}
+time_survey["other"] = 1 - sum(time_survey.values())
 
 class LeisurePlots:
-
     def __init__(self, world):
         self.world = world
+        self.leisure = generate_leisure_for_config(
+            world=self.world, config_filename=simulation_config
+        )
+        self.simulator = self.setup_simulator()
+        self.timer = self.simulator.timer
+        self.activity_manager = self.simulator.activity_manager
 
-    def run_poisson_process(
-            self,
-    ):
-        probabilities_dict = parse_multiple_config_files()
-        self.time_spent_in_leisure, self.ret_child, self.ret_adult, self.ret_retired = simulate_leisure_week(probabilities_dict)
+    def setup_simulator(self):
+        simulator = Simulator.from_file(
+            world=self.world,
+            policies=Policies([]),
+            interaction=None,
+            leisure=self.leisure,
+            travel=Travel(),
+            infection_selector=None,
+            infection_seed=None,
+            config_filename=simulation_config,
+            record=None,
+        )
+        return simulator
 
-    def plot_week_probabilities(
-            self,
-    ):
-        "Plotting probabaility of doing different activities in a week"
+    def simulate_weekend_and_weekday(self, one_time = False):
+        time_in_place = {
+            "residence": 0,
+            "work": 0,
+            "household visits": 0,
+            "groceries": 0,
+            "pubs": 0,
+            "commute": 0,
+            "other": 0,
+        }
+        self.timer.reset()
+        total_duration = 0
+        while str(self.timer.date.date()) in ["2020-03-01", "2020-03-02"]:
+            self.simulator.clear_world()
+            self.activity_manager.do_timestep()
+            duration = self.timer.duration
+            for household in self.world.households:
+                residents = 0
+                visitors = 0
+                for person in household.people:
+                    if person.residence.group == household:
+                        residents += 1
+                    elif person.leisure.group == household:
+                        visitors += 1
+                    else:
+                        raise ValueError
+                time_in_place["residence"] += duration * residents
+                time_in_place["household visits"] += duration * visitors
 
-        activities = []
-        new_activities = []
-        child_probability = []
-        adult_probability = []
-        retired_probability = []
-        for activity in self.ret_adult:
-            activities.append(activity)
-            new_activities.append(activity.replace("_", " "))
-            adult_probability.append(self.ret_adult[activity])
-            try:
-                child_probability.append(self.ret_child[activity])
-            except:
-                child_probability.append(0.)
-            retired_probability.append(self.ret_retired[activity])
+            for care_home in self.world.care_homes:
+                time_in_place["residence"] += duration * len(care_home.residents)
+                time_in_place["work"] += duration * len(care_home.workers)
+                time_in_place["other"] += duration * len(care_home.visitors)
 
-        x = np.arange(len(activities))  # the label locations
-        width = 0.35  # the width of the bars
+            for company in self.world.companies:
+                time_in_place["work"] += duration * len(company.people)
 
-        f, ax = plt.subplots()
-        ax.bar(x - width/2, child_probability, width/2, label = 'Children')
-        ax.bar(x, adult_probability, width/2, label = 'Adults')
-        ax.bar(x + width/2, retired_probability, width/2, label = 'Retired')
-        ax.set_ylabel('Probability of doing activity in a week')
-        ax.set_xticks(x)
-        ax.set_xticklabels(new_activities)
-        ax.legend()
-        plt.xticks(rotation=45)
+            for pub in self.world.pubs:
+                time_in_place["pubs"] += duration * len(pub.people)
 
-        return ax
+            for grocery in self.world.groceries:
+                time_in_place["groceries"] += duration * len(grocery.people)
 
-    def plot_leisure_time_spent(
-            self
-    ):
-        "Plotting time spent in leisure by age"
-        
-        f, ax = plt.subplots()
-        ax.bar(self.time_spent_in_leisure.keys(), self.time_spent_in_leisure.values())
-        ax.axvline(65, color = "red", linestyle=":")
-        ax.set_ylabel("Hours of leisure a week")
-        ax.set_xlabel("Age")
+            for cinema in self.world.cinemas:
+                time_in_place["other"] += duration * len(cinema.people)
 
-        return ax
+            for station in self.world.stations:
+                time_in_place["commute"] += duration * sum(
+                    len(inter_city_transport.people)
+                    for inter_city_transport in station.inter_city_transports
+                )
 
-def parse_config_file(config_file_path):
-    with open(config_file_path) as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    male_age_probabilities = parse_age_probabilities(config["male_age_probabilities"])
-    female_age_probabilities = parse_age_probabilities(
-        config["female_age_probabilities"]
-    )
-    return male_age_probabilities, female_age_probabilities
+            for city in self.world.cities:
+                time_in_place["commute"] += duration * sum(
+                    len(city_transport.people)
+                    for city_transport in city.city_transports
+                )
 
-def parse_config_file(config_file_path):
-    with open(config_file_path) as f:
-        config = yaml.load(f, Loader=yaml.FullLoader)
-    male_age_probabilities = parse_age_probabilities(config["male_age_probabilities"])
-    female_age_probabilities = parse_age_probabilities(
-        config["female_age_probabilities"]
-    )
-    return male_age_probabilities, female_age_probabilities
+            for university in self.world.universities:
+                time_in_place["work"] += duration * len(university.people)
 
+            for hospital in self.world.hospitals:
+                time_in_place["work"] += duration * len(hospital[0].people)
 
-def parse_multiple_config_files(config_files=config_files_leisure):
-
-    probabilities_dict = OrderedDict()
-    for config_path in config_files:
-        name = config_path.name.split("/")[-1].split(".")[0]
-        probabilities_dict[name] = {}
-        (
-            probabilities_dict[name]["m"],
-            probabilities_dict[name]["f"],
-        ) = parse_config_file(config_path)
-    return probabilities_dict
-
-
-def simulate_poisson_process(
-    probabilities_dict, leisure_delta_t
-):
-    activities = list(probabilities_dict.keys())
-    does_activity_age = {}
-    for age in np.arange(0, 100):
-        poisson_parameters = [
-            probabilities_dict[activity]["m"][age] for activity in activities
-        ]
-        total_poisson = sum(poisson_parameters)
-        does_activity = random() < (1 - np.exp(-total_poisson * leisure_delta_t))
-        if does_activity:
-            which_activity = np.random.choice(
-                activities, p=np.array(poisson_parameters) / np.sum(poisson_parameters)
+            for school in self.world.schools:
+                time_in_place["work"] += duration * len(school.people)
+            total_duration += duration
+            assert np.isclose(
+                total_duration * len(self.world.people),
+                sum(time_in_place.values()),
+                rtol=0.01,
             )
-            does_activity_age[age] = which_activity
-        else:
-            does_activity_age[age] = None
-    return does_activity_age
+            next(self.timer)
+            if one_time:
+                break
+        return time_in_place
 
-def simulate_leisure_week(probabilities_dict):
-    weekday_leisure_workers = [3]
-    weekday_leisure_retired = [8, 3]
-    weekend_leisure = [4,4,4]
-    time_spent_in_leisure = defaultdict(list)
-    child_activities = defaultdict(list)
-    adult_activities = defaultdict(list)
-    retired_activities = defaultdict(list)
-    
-    ages = np.arange(0,100)
-    for i in tqdm(range(50)):
-        time_spent_in_leisure_week = defaultdict(int)
-        child_activities_week = defaultdict(int)
-        adult_activities_week = defaultdict(int)
-        retired_activities_week = defaultdict(int)
-        for _ in range(5):
-            for age in ages:
-                if age < 65:
-                    weekday_leisure = weekday_leisure_workers
-                else:
-                    weekday_leisure = weekday_leisure_retired
-                for dt in weekday_leisure:
-                    activities_age = simulate_poisson_process(probabilities_dict, dt / 24)
-                    if activities_age[age] is not None:
-                        time_spent_in_leisure_week[age] += dt
-                        if age < 19:
-                            child_activities_week[activities_age[age]] += 1
-                        elif age >= 19 and age < 65:
-                            adult_activities_week[activities_age[age]] += 1
-                        else:
-                            retired_activities_week[activities_age[age]] += 1
+    def _normalise_times(self, times):
+        times_normed = {key: value for key,value in times.items() if key != "other"}
+        n = sum(times_normed.values())
+        return  {key : value / n * 100 for key, value in times_normed.items()}
 
-        for _ in range(2):
-            for dt in weekend_leisure:
-                activities_age = simulate_poisson_process(probabilities_dict, dt / 24)
-                for age in ages:
-                    if activities_age[age] is not None:
-                        time_spent_in_leisure_week[age] += dt
-                        if age < 19:
-                            child_activities_week[activities_age[age]] += 1
-                        elif age >= 19 and age < 65:
-                            adult_activities_week[activities_age[age]] += 1
-                        else:
-                            retired_activities_week[activities_age[age]] += 1
-                        
-        for age in activities_age:
-            time_spent_in_leisure[age].append(time_spent_in_leisure_week[age])
-        for activity in child_activities_week:
-            child_activities[activity].append(child_activities_week[activity]/(7*19))
-        for activity in adult_activities_week:
-            adult_activities[activity].append(adult_activities_week[activity]/(7*47))
-        for activity in retired_activities_week:
-            retired_activities[activity].append(retired_activities_week[activity]/(7*36))
-        
-    ret = defaultdict(float)
-    for age in activities_age:
-        ret[age] = np.mean(time_spent_in_leisure[age])
-    ret_child = defaultdict(float)
-    for activity in child_activities:
-        ret_child[activity] = np.mean(child_activities[activity])
-    ret_adult = defaultdict(float)
-    for activity in adult_activities:
-        ret_adult[activity] = np.mean(adult_activities[activity])
-    ret_retired = defaultdict(float)
-    for activity in retired_activities:
-        ret_retired[activity] = np.mean(retired_activities[activity])
-    return ret, ret_child, ret_adult, ret_retired
+    def plot_time_survey(self):
+        june_times = self.simulate_weekend_and_weekday()
+        june_times_normed = self._normalise_times(june_times)
+        time_survey_normed = self._normalise_times(time_survey)
+
+        fig, ax = plt.subplots()
+        ax.bar(time_survey_normed.keys(), time_survey_normed.values(), alpha=0.7, label = "Time Survey")
+        ax.bar(june_times_normed.keys(), june_times_normed.values(), alpha=0.7, label = "JUNE")
+        ax.set_ylabel("Fraction of time [\%]")
+        ax.set_xlabel("Activity")
+        fig.autofmt_xdate()
+        return ax
+
+    def plot_occupancy(self):
+        self.simulate_weekend_and_weekday(one_time=True)
+        # occupancy
+        f, ax = plt.subplots(1,3, figsize=(8,2), sharey=True) 
+        pub_sizes = [pub.size for pub in self.world.pubs]
+        grocery_sizes = [grocery.size for grocery in self.world.groceries]
+        cinema_sizes = [cinema.size for cinema in self.world.cinemas]
+        ax[0].hist(pub_sizes, density=True, color="C0", label="pubs/restaurants")
+        ax[1].hist(grocery_sizes, density=True, color="C1", label="stores")
+        ax[2].hist(cinema_sizes, density=True, label="cinemas", color="C2")
+        ax[0].legend()
+        ax[1].legend()
+        ax[2].legend()
+        ax[0].set_xlabel("Occupancy")
+        ax[1].set_xlabel("Occupancy")
+        ax[2].set_xlabel("Occupancy")
+        ax[0].set_ylabel("Frequency")
+        plt.subplots_adjust(wspace=0, hspace=0)
+        return ax
