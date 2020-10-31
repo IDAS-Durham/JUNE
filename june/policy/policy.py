4613cf3a09dcb0145a39d9a5fab253197d308a2f
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-10-31 10:32:36 +0000

    regional_compliance_is_active running correctly

--- june/policy/policy.py
+++ june/policy/policy.py
@@ -27,12 +27,34 @@ def str_to_class(classname, base_policy_modules=("june.policy",)):
             continue
     raise ValueError("Cannot find policy in paths!")
 
+def read_date(date: Union[str, datetime.datetime]) -> datetime.datetime:
+        """
+        Read date in two possible formats, either string or datetime.date, both
+        are translated into datetime.datetime to be used by the simulator
+
+        Parameters
+        ----------
+        date:
+            date to translate into datetime.datetime
+
+        Returns
+        -------
+            date in datetime format
+        """
+        if type(date) is str:
+            return datetime.datetime.strptime(date, "%Y-%m-%d")
+        elif isinstance(date, datetime.date):
+            return datetime.datetime.combine(date, datetime.datetime.min.time())
+        else:
+            raise TypeError("date must be a string or a datetime.date object")
+
 def regional_compliance_is_active(regional_compliance, date):
-    for compliance in regional_compliance:
+
+    for compliance in regional_compliance:       
         if (
-                datetime.datetime.strptime(complaince["start_time"], "%Y-%m-%d")
+                read_date(compliance["start_time"])
                 <= date
-                < datetime.datetime.strptime(complaince["start_time"], "%Y-%m-%d")
+                < read_date(compliance["end_time"])
         ):
             return compliance
 
