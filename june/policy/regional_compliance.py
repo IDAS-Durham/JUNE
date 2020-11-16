f8dd5c15cceedb4ba6c5c8d976ab8e51520bbcd4
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-11-16 13:07:00 +0100

    added regional compliance as policy

---
+++ june/policy/regional_compliance.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from .policy import Policy, PolicyCollection, Policies, read_date
+from june.geography import Regions
+from june import paths
+
+
+class RegionalCompliance(Policy):
+    policy_type = "regional_compliance"
+
+    def __init__(
+        self, start_time: str, end_time: str, compliances_per_region: dict,
+    ):
+        super().__init__(start_time=start_time, end_time=end_time)
+        self.compliances_per_region = compliances_per_region
+
+    def apply(self, date: datetime, regions: Regions):
+        date = read_date(date)
+        if self.is_active(date):
+            for region_name, regional_compliance in self.compliances_per_region.items():
+                region = regions.get_from_name(region_name)
+                region.regional_compliance = regional_compliance
+
+
+class RegionalCompliances(PolicyCollection):
+    policy_type = "regional_compliance"
+
+    def apply(self, date: datetime, regions: Regions):
+        # before applying compliances, reset all of them to 1.0
+        for region in regions:
+            region.regional_compliance = 1.0
+        for policy in self.policies:
+            policy.apply(date=date, regions=regions)
