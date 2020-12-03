6338ff316201de00035d151839553495461ad398
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-12-03 22:08:12 +0000

    initial test update

--- test_june/unit/policy/test_regional_compliance.py
+++ test_june/unit/policy/test_regional_compliance.py
@@ -23,3 +23,25 @@ class TestSetRegionCompliance:
         regional_compliances.apply(regions=regions, date = "2020-09-01")
         assert region.regional_compliance == 1.0
 
+class TestSetTiers:
+    def test__set_lockdowntiers(self):
+        tiered_lockdown = TieredLockdown(
+            start_time="2020-05-01",
+            end_time="2020-09-01",
+            tiers_per_region={"London": 2},
+        )
+        tiered_lockdowns = TieredLockdowns([tiered_lockdown])
+        region = Region(name="London")
+        regions = Regions([region])
+        tiered_lockdowns.apply(regions=regions, date = "2020-05-05")
+        assert region.policy["lockdown_tier"] == 2
+        tiered_lockdowns.apply(regions=regions, date = "2020-05-01")
+        assert region.policy["lockdown_tier"] == 2
+        tiered_lockdowns.apply(regions=regions, date = "2020-01-05")
+        assert region.policy["lockdown_tier"] == None
+        tiered_lockdowns.apply(regions=regions, date = "2021-01-05")
+        assert region.policy["lockdown_tier"] == None
+        tiered_lockdowns.apply(regions=regions, date = "2020-09-01")
+        assert region.policy["lockdown_tier"] == None
+        
+
