373e4bfec0091ff1a37604bc0f48872ed1b1f363
florpi <florpisoteadadejardin@gmail.com>
2020-12-06 17:23:58 +0000

    working piecewise function

--- june/policy/vaccine_policy.py
+++ june/policy/vaccine_policy.py
@@ -1,10 +1,62 @@
 import operator
 import random
 import numpy as np
-import datetime 
+import datetime
 from june.demography.person import Person
 from .policy import Policy, PolicyCollection, Policies, read_date
 from june import paths
+
+
+class VaccinePlan:
+    def __init__(
+        self,
+        first_dose_date,
+        second_dose_date,
+        first_dose_effective_days,
+        second_dose_effective_days,
+        first_dose_susceptibility,
+        second_dose_susceptibility,
+        original_susceptibility,
+    ):
+        self.first_dose_date = first_dose_date
+        self.second_dose_date = second_dose_date
+        self.first_dose_effective_days = first_dose_effective_days
+        self.second_dose_effective_days = second_dose_effective_days
+        self.first_dose_susceptibility = first_dose_susceptibility
+        self.second_dose_susceptibility = second_dose_susceptibility
+        self.first_dose_effective_date = self.first_dose_date + datetime.timedelta(
+            days=self.first_dose_effective_days
+        )
+        self.second_dose_effective_date = self.second_dose_date + datetime.timedelta(
+            days=self.second_dose_effective_days
+        )
+        self.original_susceptibility = original_susceptibility
+
+    def straight_line(self, n_days, p0, p1):
+        m = (p1[1] - p0[1]) / (p1[0] - p0[0])
+        c = p1[1] - (m * p1[0])
+        return m * n_days + c
+
+    def susceptibility(self, date):
+        if date < self.first_dose_effective_date:
+            n_days = (date - self.first_dose_date).days
+            return self.straight_line(
+                n_days,
+                p0=(0, self.original_susceptibility),
+                p1=(self.first_dose_effective_days, self.first_dose_susceptibility),
+            )
+        elif self.first_dose_effective_date <= date < self.second_dose_date:
+            return self.first_dose_susceptibility
+        elif date < self.second_dose_effective_date:
+            n_days = (date - self.second_dose_date).days
+            return self.straight_line(
+                n_days,
+                p0=(0, self.first_dose_susceptibility),
+                p1=(self.second_dose_effective_days, self.second_dose_susceptibility),
+            )
+        else:
+            return self.second_dose_susceptibility
+
 
 class VaccineDistribution(Policy):
     policy_type = "vaccine_distribuion"
@@ -12,14 +64,14 @@ class VaccineDistribution(Policy):
     def __init__(
         self,
         start_time: str = "1900-01-01",
-        end_time: str="2100-01-01",
-        group_description: dict={'by': 'residence', 'group': 'care_home'},
-        group_coverage: float=0.4,
-        efficacy: float=1.,
-        second_dose_compliance: float=1.,
-        first_rollout_days: int=100,
-        mean_time_delay: int=1,
-        std_time_delay: int=1,
+        end_time: str = "2100-01-01",
+        group_description: dict = {"by": "residence", "group": "care_home"},
+        group_coverage: float = 0.4,
+        efficacy: float = 1.0,
+        second_dose_compliance: float = 1.0,
+        first_rollout_days: int = 100,
+        mean_time_delay: int = 1,
+        std_time_delay: int = 1,
     ):
         """
         Policy to apply a vaccinated tag to people based on certain attributes with a given probability
@@ -45,15 +97,17 @@ class VaccineDistribution(Policy):
           with mean mean_time_delay and std std_time_delay
         - The vaccine is immediatrly effective after the second dose - this can be changes as needed later
         """
-        
+
         super().__init__(start_time=start_time, end_time=end_time)
-        self.group_attribute, self.group_value = self.process_group_description(group_description)
+        self.group_attribute, self.group_value = self.process_group_description(
+            group_description
+        )
         self.group_coverage = group_coverage
         self.efficacy = efficacy
         self.second_dose_compliance = second_dose_compliance
         self.total_days = (self.end_time - self.start_time).days
-        #self.probabilities = self.calculate_probabilities()
-        self.final_susceptibilty = 1 - efficacy
+        # self.probabilities = self.calculate_probabilities()
+        self.final_susceptibility = 1 - efficacy
         self.vaccinated_ids = set()
 
     def process_group_description(self, group_description):
@@ -84,44 +138,45 @@ class VaccineDistribution(Policy):
     def calculate_probabilities(self):
         values = np.zeros(total_days)
         for i in range(days):
-            vals = np.random.normal(loc=mean+i,scale=std,size=len(values))
+            vals = np.random.normal(loc=mean + i, scale=std, size=len(values))
             val_ints = np.round(vals)
             for j in val_ints:
                 values[int(j)] += 1
-        probs = values/np.sum(values)
+        probs = values / np.sum(values)
 
         return probs
-        
+
     def apply(self, person: Person, date: datetime):
-        if person.susceptibility == 1. and self.is_target_group(person):
-            if random.random() < 1.: # TODO: fill in this 1. number
+        if person.susceptibility == 1.0 and self.is_target_group(person):
+            if random.random() < 1.0:  # TODO: fill in this 1. number
                 self.vaccinate(person=person, date=date)
 
     def vaccinate(self, person, date):
-        person.vaccine_date = date
-        person.effective_vaccine_date = date + datetime.timedelta(
-            days=int(np.random.normal(loc=25, scale=10))
-            ) # TODO: change this to second dose + add more necessary numbers on target sus
+        person.vaccine_plan = VaccinePlan(
+            first_dose_date=date,
+            first_dose_effective_days=10,
+            first_dose_susceptibility=0.5*(person.susceptibility-self.final_susceptibility),
+            second_dose_date=date + datetime.timedelta(days=20),
+            second_dose_effective_days=10,
+            second_dose_susceptibility=self.final_susceptibility,
+            original_susceptibility=person.susceptibility,
+        )
         self.vaccinated_ids.add(person.id)
 
-    def susceptibility(self, time_from_vaccine, time_effective_from_vaccine):
-        return 1 - time_from_vaccine/time_effective_from_vaccine
-
-    def update_susceptibility(self, person, date):
-        time_effective_from_vaccine = (person.effective_vaccine_date - person.vaccine_date).days
-        person.susceptibility = self.susceptibility(
-            time_from_vaccine=(date-person.vaccine_date).days,
-            time_effective_from_vaccine=time_effective_from_vaccine
-        )
+    def update_susceptibility(self, person,date):
+        person.susceptibility = person.vaccine_plan.susceptibility(date=date)
 
     def update_susceptibility_of_vaccinated(self, people, date):
         if self.vaccinated_ids:
             for pid in self.vaccinated_ids:
                 person = people.get_from_id(pid)
-                if person.suscepbility == self.final_susceptibilty:
+                if person.suscepbility == self.person.vaccine_plan.final_susceptibility:
+                    #TODO: will need changing if add compliance second dose
                     self.vaccinated_ids.remove(person)
+                    person.vaccine_plan = None
                 else:
-                    self.update_susceptibility(person, date)
+                    self.update_susceptibility(person=person,date=date)
+
 
 class VaccineDistributions(PolicyCollection):
     policy_type = "vaccine_distribution"
--- test_june/unit/policy/test_vaccine.py
+++ test_june/unit/policy/test_vaccine.py
@@ -14,10 +14,10 @@ class TestVaccination:
         vaccine_policy.daily_vaccine_probability = 1.0
         date = datetime.datetime(2020, 11, 15)
         vaccine_policy.apply(person=person, date=date)
-        assert person.vaccine_date == date
+        assert person.vaccine_plan.first_dose_date == date
         person = Person.from_attributes(age=50, sex="f")
         vaccine_policy.apply(person=person, date=date)
-        assert person.vaccine_date is None
+        assert person.vaccine_plan is None
 
     def test__process_target_population_care_home(self,):
         care_home = CareHome()
@@ -29,26 +29,34 @@ class TestVaccination:
         vaccine_policy.daily_vaccine_probability = 1.0
         date = datetime.datetime(2020, 11, 15)
         vaccine_policy.apply(person=person, date=date)
-        assert person.vaccine_date == date
+        assert person.vaccine_plan.first_dose_date == date
         person = Person.from_attributes(age=50, sex="f")
         vaccine_policy.apply(person=person, date=date)
-        assert person.vaccine_date is None
+        assert person.vaccine_plan is None
 
 
     def test__susceptibility(self,):
         person = Person.from_attributes(age=30, sex="f")
-        vaccine_policy = VaccineDistribution()
+        date = datetime.datetime(2020, 11, 15)
+        vaccine_policy = VaccineDistribution(
+            group_description={"by": "age", "group": [20, 40], "total_group_size": 100}
+        )
+        vaccine_policy.apply(person=person, date=date)
         assert person.susceptibility == 1.0
-        person.vaccine_date = datetime.datetime(2020, 11, 5)
-        person.effective_vaccine_date = person.vaccine_date + datetime.timedelta(
-            days=10
-        )
+        person.vaccine_plan.first_dose_date = date
+        person.vaccine_plan.first_dose_effective_days = 10 
+        person.vaccine_plan.second_dose_date = date + datetime.timedelta(days=20)
         vaccine_policy.update_susceptibility(
-            person=person, date=datetime.datetime(2020, 11, 9)
+            person=person, date=datetime.datetime(2020, 11, 20)
         )
-        assert 0.0 < person.susceptibility < 1.0
+        assert 0.5 < person.susceptibility < 1.0
         vaccine_policy.update_susceptibility(
-            person=person, date=datetime.datetime(2020, 11, 15)
+            person=person, date=datetime.datetime(2020, 11, 26)
+        )
+        assert person.susceptibility == 0.5
+
+        vaccine_policy.update_susceptibility(
+            person=person, date=datetime.datetime(2020, 12, 25)
         )
         assert person.susceptibility == 0.0
 
