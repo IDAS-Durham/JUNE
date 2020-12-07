f5db62ce10cb09a9e938d5df0dd85a8513e759fc
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2020-12-07 17:43:59 +0000

    first test passing

--- test_june/unit/policy/test_vaccine.py
+++ test_june/unit/policy/test_vaccine.py
@@ -8,27 +8,32 @@ from june.policy import VaccineDistribution
 class TestVaccination:
     def test__process_target_population(self,):
         person = Person.from_attributes(age=30, sex="f")
+        #person.susceptibilty = 1.
         vaccine_policy = VaccineDistribution(
             start_time = "2020-12-08",
-            end_time = "2020-12-09"
+            end_time = "2020-12-09",
             group_description={"by": "age", "group": "20-40"},
+            effective_after_first_dose=1,
         )
-        date = datetime.datetime(2020, 12, 08)
+        date = datetime.datetime(2020, 12, 8)
         vaccine_policy.apply(person=person, date=date)
-        assert person.vaccine_date == date
+        print (vaccine_policy.total_days)
+        assert person.first_effective_date == date + datetime.timedelta(days=1)
         person = Person.from_attributes(age=50, sex="f")
         vaccine_policy.apply(person=person, date=date)
-        assert person.vaccine_date is None
+        assert person.first_effective_date is None
 
     def test__process_target_population_care_home(self,):
         care_home = CareHome()
         person = Person.from_attributes(age=30, sex="f")
         care_home.add(person)
         vaccine_policy = VaccineDistribution(
+            start_time = "2020-12-08",
+            end_time = "2020-12-09",
             group_description={"by": "residence", "group": 'care_home'}
+            effective_after_first_dose=1,
         )
-        vaccine_policy.daily_vaccine_probability = 1.0
-        date = datetime.datetime(2020, 11, 15)
+        date = datetime.datetime(2020, 12, 8)
         vaccine_policy.apply(person=person, date=date)
         assert person.vaccine_date == date
         person = Person.from_attributes(age=50, sex="f")
