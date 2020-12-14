1c4de0cde1c9fc333433d5f0f543e5807cda6c6f
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-12-14 18:28:02 +0100

    option to reset infections after loading checkpoint

--- june/simulator.py
+++ june/simulator.py
@@ -204,7 +204,7 @@ class Simulator:
         travel: Optional[Travel] = None,
         config_filename: str = default_config_filename,
         record: Optional[Record] = None,
-        # comment: Optional[str] = None,
+        reset_infected=False,
     ):
         from june.hdf5_savers.checkpoint_saver import generate_simulator_from_checkpoint
 
@@ -219,6 +219,7 @@ class Simulator:
             travel=travel,
             config_filename=config_filename,
             record=record,
+            reset_infected=reset_infected,
         )
 
     def clear_world(self):
