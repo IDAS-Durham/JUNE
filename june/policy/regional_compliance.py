7a5ea02d6f9e8ed27289384f1b6d2b148ad1f350
Joseph Bullock <JosephBullock95@hotmail.co.uk>
2021-01-15 18:46:25 +0000

    update general tieredlocked class

--- june/policy/regional_compliance.py
+++ june/policy/regional_compliance.py
@@ -51,7 +51,9 @@ class TieredLockdown(Policy):
                 if lockdown_tier == 2:
                     region.policy["local_closed_venues"].update("household_visits")
                 elif lockdown_tier == 3:
-                    region.policy["local_closed_venues"].update(set(("pub", "cinema")))
+                    region.policy["local_closed_venues"].update(set(("pub", "cinema", "household_visits")))
+                elif: lockdown_tier == 4.:
+                    region.policy["local_closed_venues"].update(set(("pub", "cinema", "household_visits")))
 
 class TieredLockdowns(PolicyCollection):
     policy_type = "tiered_lockdown"
