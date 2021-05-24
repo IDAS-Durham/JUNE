941565f12c9752cdfe0a9f8dceaceb288a3b76cd
florpi <florpisoteadadejardin@gmail.com>
2021-05-24 11:47:38 +0100

    old tests pass

--- test_june/unit/policy/test_vaccine.py
+++ test_june/unit/policy/test_vaccine.py
@@ -155,3 +155,35 @@ class TestVaccination:
         )
         assert young_person.id not in vaccine_policy.vaccinated_ids
         assert young_person.susceptibility == 0.0
+
+    def test_overall_multiplier_update(self,):
+        young_person = Person.from_attributes(age=30, sex="f")
+        old_person = Person.from_attributes(age=80, sex="f")
+        vaccine_policy = VaccineDistribution(
+            group_by='age',
+            group_type='20-40',
+            first_dose_sterilisation_efficacy=0.,
+            second_dose_sterilisation_efficacy=0.0,
+            first_dose_symptomatic_efficacy=0.5,
+            second_dose_symptomatic_efficacy=1.,
+
+        )
+        people = Population([young_person, old_person])
+        for person in people:
+            vaccine_policy.apply(person=person, date=datetime.datetime(2100, 1, 1))
+        vaccine_policy.update_vaccinated(
+            people=people, date=datetime.datetime(2100, 12, 3)
+        )
+
+        assert young_person.effective_multiplier == 0.0
+        assert young_person.susceptibility == 1.0
+        assert old_person.effective_multiplier == 1.0
+        assert old_person.susceptibility == 1.0
+        vaccine_policy.update_vaccinated(
+            people=people, date=datetime.datetime(2100, 12, 3)
+        )
+        assert young_person.id not in vaccine_policy.vaccinated_ids
+        assert young_person.effective_multiplier == 0.0
+        assert young_person.susceptibility == 1.0
+
+
