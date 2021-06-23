ed5c2a5a9cc4552689e6839bdb174ef14c65ba50
florpi <florpisoteadadejardin@gmail.com>
2021-06-23 11:00:06 +0100

    all files there

--- june/epidemiology/infection/immunity_setter.py
+++ june/epidemiology/infection/immunity_setter.py
@@ -5,8 +5,14 @@ from june.utils import (
     read_comorbidity_csv,
     convert_comorbidities_prevalence_to_dict,
 )
+
 from . import Covid19, B117, B16172
 
+default_susceptibility_dict = {
+    Covid19.infection_id(): {"0-13": 0.5, "13-100": 1.0},
+    B117.infection_id(): {"0-13": 0.5, "13-100": 1.0},
+    B16172.infection_id(): {"0-13": 0.5, "13-100": 1.0},
+}
 default_multiplier_dict = {
     Covid19.infection_id(): 1.0,
     B117.infection_id(): 1.5,
@@ -14,30 +20,30 @@ default_multiplier_dict = {
 }
 
 
-class EffectiveMultiplierSetter:
+
+class ImmunitySetter:
     """
-    Sets effective multipliers that increment or decrease the probability
-    of developing a severe infection. This multipliers can arise due to a person's
-    comorbidities, or due to different variants producing different outcomes.
+    Sets immnuity parameters to different viruses.
 
     Parameters
     ----------
-    multiplier_dict
-       A dictionary mapping infection_id -> multiplier.
+    susceptibility_dict
+       A dictionary mapping infection_id -> susceptibility by age.
        Example:
-        multiplier_dict = {"123" : 1.3}
+        susceptibility_dict = {"123" : {"0-50" : 0.5, "50-100" : 0.2}}
     """
-
-    # Varying due to comorbidities
-
-    # Varying due to different variants
-
-    def __init__(
-        self,
+    def __init__(self, 
+        susceptibility_dict: dict = default_susceptibility_dict,
         multiplier_dict: dict = default_multiplier_dict,
         multiplier_by_comorbidity: Optional[dict] = None,
         comorbidity_prevalence_reference_population: Optional[dict] = None,
     ):
+        if susceptibility_dict is None:
+            self.susceptibility_dict = {}
+        else:
+            self.susceptibility_dict = self._read_susceptibility_dict(
+                susceptibility_dict
+            )
         if multiplier_dict is None:
             self.multiplier_dict = {}
         else:
@@ -55,6 +61,7 @@ class EffectiveMultiplierSetter:
     @classmethod
     def from_file_with_comorbidities(
         cls,
+        susceptibility_dict: dict = default_susceptibility_dict,
         multiplier_dict: dict = default_multiplier_dict,
         comorbidity_multipliers_path: Optional[str] = None,
         male_comorbidity_reference_prevalence_path: Optional[str] = None,
@@ -144,3 +151,26 @@ class EffectiveMultiplierSetter:
                     person.immunity.effective_multiplier_dict[inf_id] += (
                         multiplier / reference_multiplier
                     ) - 1.0
+
+    def _read_susceptibility_dict(self, susceptibility_dict):
+        ret = {}
+        for inf_id in susceptibility_dict:
+            ret[inf_id] = parse_age_probabilities(
+                susceptibility_dict[inf_id], fill_value=1.0
+            )
+        return ret
+
+    def set_susceptibilities(self, population):
+        for person in population:
+            for inf_id in self.susceptibility_dict:
+                if person.age >= len(self.susceptibility_dict[inf_id]):
+                    continue
+                person.immunity.susceptibility_dict[inf_id] = self.susceptibility_dict[
+                    inf_id
+                ][person.age]
+
+    def set_immunity(self, population):
+        if self.multiplier_dict is not None:
+            self.set_multipliers(population)
+        if self.susceptibility_dict is not None:
+            self.set_susceptibilities(population)
