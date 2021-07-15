36d0da5a830564c0a2e09b7193fe8630300c4cf1
Arnau Quera-Bofarull <arnauq@protonmail.com>
2021-07-15 17:06:11 +0200

    Update data folder url

--- scripts/get_june_data.sh
+++ scripts/get_june_data.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-data_release="june_input_data_1.0.zip"
+# old release data_release="june_input_data_1.0.zip"
+# new release 
+data_release="data_private.zip"
 wget --user=access --password=d0wn10@d$  "http://virgodb.dur.ac.uk/downloads/dc-quer1/$data_release"
 unzip $data_release
 rm $data_release
