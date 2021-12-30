f8e2d7ea42a457b50d7f644bfa929d45c5238544
florpi <florpisoteadadejardin@gmail.com>
2021-12-30 13:45:48 +0100

    coverage does work

--- test_june/unit/policy/test_vaccine.py
+++ test_june/unit/policy/test_vaccine.py
@@ -17,6 +17,29 @@ from june.epidemiology.infection.infection import Delta, Omicron
 delta_id = Delta.infection_id()
 omicron_id = Omicron.infection_id()
 
+@pytest.fixture(name="population")
+def make_population():
+    people = []
+    for age in range(100):
+        for _ in range(100):
+            person = Person.from_attributes(age=age)
+            people.append(person)
+    return Population(people)
+
+@pytest.fixture(name="vax_policy")
+def make_policy():
+    return VaccineDistribution(
+        vaccine_type='Test',
+        days_to_next_dose=[0,9,16],
+        doses=[0,1,2],
+        start_time="2021-03-01",
+        end_time="2021-03-05",
+        group_by="age",
+        group_type="20-40",
+        group_coverage=0.6,
+    )
+
+
 @pytest.fixture(name="vaccine", scope="session")
 def create_vaccine():
     return Vaccine(
@@ -462,29 +485,6 @@ class TestVaccination:
 
 
 class TestVaccinationInitialization:
-    @pytest.fixture(name="vax_policy")
-    def make_policy(self, ):
-        vaccine_policy = VaccineDistribution(
-            vaccine_type='Test',
-            days_to_next_dose=[0,9,16],
-            doses=[0,1,2],
-            start_time="2021-03-01",
-            end_time="2021-04-01",
-            group_by="age",
-            group_type="20-40",
-            group_coverage=0.6,
-        )
-        return vaccine_policy
-
-    @pytest.fixture(name="population")
-    def make_population(self):
-        people = []
-        for age in range(100):
-            for _ in range(100):
-                person = Person.from_attributes(age=age)
-                people.append(person)
-        return Population(people)
-
     def test__vaccination_from_the_past(self, population, vax_policy):
         date = datetime.datetime(2021, 4, 30)
         vax_policy._apply_past_vaccinations(people=population, date=date)
@@ -542,4 +542,25 @@ class TestBooster:
 
         assert not_dosed_person.immunity.get_susceptibility(delta_id) == 1.
         assert not_dosed_person.vaccinated is None
+
+
+class TestCoverage:
+    def test__right_coverage(self, population, vax_policy):
+        date = datetime.datetime(2021, 4, 30)
+        start_time=datetime.datetime(2021,3,1)
+        n_days = 4
+        dates = [start_time+ datetime.timedelta(days=idx) for idx in range(n_days)]
+        for date in dates:
+            for person in population:
+                vax_policy.apply(person=person, date=date)
+        n_vaccinated = 0
+        for person in population:
+            if (person.age < 20) or (person.age >= 40):
+                assert person.vaccinated is None 
+            else:
+                if person.vaccinated is not None:
+                    n_vaccinated += 1
+        assert np.isclose(n_vaccinated, 60 * 20, atol=0, rtol=0.1)
+
+
 
